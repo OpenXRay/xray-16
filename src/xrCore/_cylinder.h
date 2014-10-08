8b67bb51a14dfa8b6fb05f048c8778e4b3b78bd1
Pavel Kovalenko <paul-kv@yandex.ru>
2014-10-08 19:45:26 +0400

    Add missing check in function for validating _cylinder instance.

--- src/xrCore/_cylinder.h
+++ src/xrCore/_cylinder.h
@@ -218,6 +218,6 @@ typedef _cylinder<float>	Fcylinder;
 typedef _cylinder<double>	Dcylinder;
 
 template <class T>
-BOOL	_valid			(const _cylinder<T>& c)	{ return _valid(c.m_center) && _valid(c.m_direction) && _valid(c.m_height) && _valid(c.m_height);	}
+BOOL	_valid			(const _cylinder<T>& c)	{ return _valid(c.m_center) && _valid(c.m_direction) && _valid(c.m_height) && _valid(c.m_radius);	}
 
 #endif // _DEBUG
