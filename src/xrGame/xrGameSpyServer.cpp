99de510152c3414de42b3fa07b316835e9095d25
Pavel Kovalenko <paul-kv@yandex.ru>
2014-10-10 00:08:42 +0400

    Fix potential buffer overflow in xrGameSpyServer::OnMessage.

--- src/xrGame/xrGameSpyServer.cpp
+++ src/xrGame/xrGameSpyServer.cpp
@@ -167,9 +167,18 @@ u32				xrGameSpyServer::OnMessage(NET_Packet& P, ClientID sender)			// Non-Zero 
 	{
 	case M_GAMESPY_CDKEY_VALIDATION_CHALLENGE_RESPOND:
 		{
-			string128 ResponseStr;
-			P.r_stringZ(ResponseStr);
-			
+            string128 ResponseStr = "";
+            u32 bytesRemain = P.r_elapsed();
+            if (bytesRemain == 0 || bytesRemain > sizeof(ResponseStr))
+            {
+                xr_string clientIp = CL->m_cAddress.to_string();
+                Msg("! WARNING: Validation challenge respond from client [%s] is %s. DoS attack?",
+                    clientIp.c_str(), bytesRemain == 0 ? "empty" : "too long");
+                DisconnectClient(CL, "");
+                // XXX nitrocaster: block IP address after X such attempts
+                return 0;
+            }
+            P.r_stringZ(ResponseStr);
 			if (!CL->m_bCDKeyAuth)
 			{
 #ifndef MASTER_GOLD
