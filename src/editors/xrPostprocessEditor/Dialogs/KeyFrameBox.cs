d9a14876f4e333f309dcc5e98c24741d7f5140aa
Pavel Kovalenko <paul-kv@yandex.ru>
2014-10-19 23:46:50 +0400

    Add UI for xrPostprocessEditor.

---
+++ src/editors/xrPostprocessEditor/Dialogs/KeyFrameBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace xrPostprocessEditor
+{
+    public partial class KeyFrameBox : UserControl
+    {
+        public event EventHandler SelectedIndexChanged;
+        public event EventHandler Add;
+        public event EventHandler Remove;
+        public event EventHandler Clear;
+
+        public KeyFrameBox()
+        {
+            InitializeComponent();
+        }
+        
+        private void lbKeyFrames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //public string SelectedItem { get {return lbPointList.Se} set; }
+    }
+}
