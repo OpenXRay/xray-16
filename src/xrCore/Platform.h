e05b5fd356b6554fe8850ff1859768db771ddf2f
Alexandr Akulich <akulichalexander@gmail.com>
2015-11-06 18:02:32 +0600

    xrCore: Inline control moved to platform-dependent code.

--- src/xrCore/Platform.h
+++ src/xrCore/Platform.h
@@ -19,6 +19,22 @@
 #define XR_EXPORT __declspec(dllexport)
 #define XR_IMPORT __declspec(dllimport)
 
+// inline control - redefine to use compiler's heuristics ONLY
+// it seems "IC" is misused in many places which cause code-bloat
+// ...and VC7.1 really don't miss opportunities for inline :)
+#ifdef _EDITOR
+# define __forceinline inline
+#endif
+#define _inline inline
+#define __inline inline
+#define IC inline
+#define ICF __forceinline // !!! this should be used only in critical places found by PROFILER
+#ifdef _EDITOR
+# define ICN
+#else
+# define ICN __declspec (noinline)
+#endif
+
 #ifdef __BORLANDC__
 #include <vcl.h>
 #include <mmsystem.h>
--- src/xrCore/xrCore.h
+++ src/xrCore/xrCore.h
@@ -93,22 +93,6 @@
 #endif
 #endif
 
-// inline control - redefine to use compiler's heuristics ONLY
-// it seems "IC" is misused in many places which cause code-bloat
-// ...and VC7.1 really don't miss opportunities for inline :)
-#ifdef _EDITOR
-# define __forceinline inline
-#endif
-#define _inline inline
-#define __inline inline
-#define IC inline
-#define ICF __forceinline // !!! this should be used only in critical places found by PROFILER
-#ifdef _EDITOR
-# define ICN
-#else
-# define ICN __declspec (noinline)
-#endif
-
 #ifndef DEBUG
 #pragma inline_depth ( 254 )
 #pragma inline_recursion( on )
