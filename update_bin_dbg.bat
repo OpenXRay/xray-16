6eef874f62ec642092b9e5e8f47878a2166e791d
hovan <iahovanskih@gmail.com>
2015-11-08 17:05:10 +0300

    build tools

---
+++ update_bin_dbg.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+set src="D:\Develop\xray-16-another"
+set dst="D:\Games\SCOP"
+call xrbinup.cmd %dst% %src% dbg
+endlocal
---
+++ update_bin_rel.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+set src="D:\Develop\xray-16-another"
+set dst="D:\Games\SCOP"
+call xrbinup.cmd %dst% %src% rel
+endlocal
+pause
