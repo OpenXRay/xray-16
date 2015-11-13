2ad02185eb5ddcfcde8e48a2bbc3d32c3695bed5
Alexandr Akulich <akulichalexander@gmail.com>
2015-11-14 00:47:43 +0500

    xrCore/LocatorAPI.cpp: Removed useless comment in Russian.

--- src/xrCore/LocatorAPI.cpp
+++ src/xrCore/LocatorAPI.cpp
@@ -942,7 +942,6 @@ xr_vector<char*>* CLocatorAPI::file_list_open(const char* _path, u32 flags)
 {
     R_ASSERT(_path);
     VERIFY(flags);
-    // проверить нужно ли пересканировать пути
     check_pathes();
 
     string_path N;
@@ -1004,7 +1003,6 @@ int CLocatorAPI::file_list(FS_FileSet& dest, LPCSTR path, u32 flags, LPCSTR mask
 {
     R_ASSERT(path);
     VERIFY(flags);
-    // проверить нужно ли пересканировать пути
     check_pathes();
 
     string_path N;
@@ -1323,7 +1321,6 @@ void CLocatorAPI::copy_file_to_build(T*& r, LPCSTR source_name)
 
 bool CLocatorAPI::check_for_file(LPCSTR path, LPCSTR _fname, string_path& fname, const file*& desc)
 {
-    // проверить нужно ли пересканировать пути
     check_pathes();
 
     // correct path
@@ -1452,7 +1449,6 @@ void CLocatorAPI::w_close(IWriter*& S)
 
 CLocatorAPI::files_it CLocatorAPI::file_find_it(LPCSTR fname)
 {
-    // проверить нужно ли пересканировать пути
     check_pathes();
 
     file desc_f;
@@ -1628,7 +1624,6 @@ return get_path(initial)->_update(dest,src);
 
 u32 CLocatorAPI::get_file_age(LPCSTR nm)
 {
-    // проверить нужно ли пересканировать пути
     check_pathes();
 
     files_it I = file_find_it(nm);
@@ -1637,7 +1632,6 @@ u32 CLocatorAPI::get_file_age(LPCSTR nm)
 
 void CLocatorAPI::set_file_age(LPCSTR nm, u32 age)
 {
-    // проверить нужно ли пересканировать пути
     check_pathes();
 
     // set file
