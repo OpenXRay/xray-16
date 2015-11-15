c41368c9875533dc06d03702049c82726fa0652c
Jules Blok <jules.blok@gmail.com>
2015-11-15 11:48:24 +0100

    Revert "Add reference configs, scripts and shaders."
    This reverts commit 0d00ac86a1cc7f220cc20fd492c2387f145b4beb.
    
    Conflicts:
    	res/gamedata/scripts/lua_help.script
    	res/gamedata/scripts/ui_sleep_dialog.script

--- res/gamedata/shaders/r2/c.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-del test\*.ps
-del test\*.vs
-FOR %%a IN (*.ps) DO call p %%a
-FOR %%b IN (*.vs) DO call v %%b
--- res/gamedata/shaders/r2/p1.bat
+++
@@ -1,1 +0,0 @@
-@C:\DEV\SDK\dx9se4_apr\Utilities\Bin\x86\fxc  /nologo /Tps_1_1 /Emain_ps_1_1 /Zpr /Fctest\p11a_%1 %1
