6418f7ed8b454a58e38335f83b9256f315aa5cc2
Jules Blok <jules.blok@gmail.com>
2015-11-24 22:58:23 +0100

    r2_types: Update implementation.

--- src/Layers/xrRenderPC_GL/r2_types.h
+++ src/Layers/xrRenderPC_GL/r2_types.h
@@ -89,11 +89,12 @@ const		u32					LUMINANCE_size		= 16	;
 
 // sun
 #define		SE_SUN_NEAR			0
-#define		SE_SUN_FAR			1
-#define		SE_SUN_LUMINANCE	2
-#define		SE_SUN_NEAR_MINMAX	3
+#define		SE_SUN_MIDDLE		1
+#define		SE_SUN_FAR			2
+#define		SE_SUN_LUMINANCE	3
+#define		SE_SUN_NEAR_MINMAX	4
 //	For rain R3 rendering
-#define		SE_SUN_RAIN_SMAP	3
+#define		SE_SUN_RAIN_SMAP	5
 
 extern		float	ps_r2_gloss_factor;
 IC	float	u_diffuse2s	(float x, float y, float z)	{ float	v = (x+y+z)/3.f;	return ps_r2_gloss_factor * ((v<1)?powf(v,2.f/3.f):v); }
