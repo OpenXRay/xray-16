1c9b7d954c316b7c04b2ae505af245f8e982a976
Jules Blok <jules.blok@gmail.com>
2015-11-24 22:58:08 +0100

    rgl: Uncomment sync point members.

--- src/Layers/xrRenderPC_GL/rgl.h
+++ src/Layers/xrRenderPC_GL/rgl.h
@@ -178,8 +178,8 @@ public:
 	float														o_hemi			;
 	float														o_hemi_cube[CROS_impl::NUM_FACES]	;
 	float														o_sun			;
-	//GLuint														q_sync_point[CHWCaps::MAX_GPUS];
-	//u32															q_sync_count	;
+	GLuint														q_sync_point[CHWCaps::MAX_GPUS];
+	u32															q_sync_count	;
 
 	bool														m_bMakeAsyncSS;
 	bool														m_bFirstFrameAfterReset;	// Determines weather the frame is the first after resetting device.
