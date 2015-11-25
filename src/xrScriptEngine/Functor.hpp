12c051a51f33345e2dbe3eee90fb3c919c4e2557
nitrocaster <paul-kv@yandex.ru>
2015-11-26 02:11:36 +0300

    Add default_converter and type_to_string specs for functor.

--- src/xrScriptEngine/Functor.hpp
+++ src/xrScriptEngine/Functor.hpp
@@ -2,6 +2,7 @@
 
 #include "xrScriptEngine/xrScriptEngine.hpp"
 #include <type_traits>
+#include <luabind/detail/format_signature.hpp>
 
 namespace luabind
 {
@@ -21,4 +22,32 @@ template<>
 template<typename... Args>
 void functor<void>::operator()(Args &&...args) const
 { call_function<void>(*static_cast<const adl::object *>(this), std::forward<Args>(args)...); }
+
+namespace detail
+{
+template<typename T>
+struct type_to_string<functor<T>>
+{
+    static void get(lua_State *L)
+    {
+        lua_pushstring(L, "function<");
+        type_to_string<T>::get(L);
+        lua_pushstring(L, ">");
+        lua_concat(L, 3);
+    }
+};
+}
+
+template<typename T>
+struct default_converter<functor<T>> : native_converter_base<functor<T>>
+{
+    static int compute_score(lua_State *luaState, int index)
+    { return lua_type(luaState, index)==LUA_TFUNCTION ? 0 : -1; }
+
+    functor<T> from(lua_State *luaState, int index)
+    { return object(from_stack(luaState, index)); }
+
+    void to(lua_State *luaState, const functor<T> &func)
+    { func.push(luaState); }
+};
 }
