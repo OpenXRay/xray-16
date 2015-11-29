4ee12029644d17c71ef791565a6676d190dd0e25
Jules Blok <jules.blok@gmail.com>
2015-11-29 15:54:24 +0100

    shaders: Add samplers.

--- res/gamedata/shaders/gl/common.h
+++ res/gamedata/shaders/gl/common.h
@@ -6,7 +6,7 @@
 //include "common_defines.h"
 //include "common_policies.h"
 #include "common_iostructs.h"
-//include "common_samplers.h"
+#include "common_samplers.h"
 //include "common_cbuffers.h"
 //include "common_functions.h"
 
--- res/gamedata/shaders/gl/shared/common.h
+++ res/gamedata/shaders/gl/shared/common.h
@@ -42,7 +42,6 @@ void	sincos(float x, out float s, out float c) { s = sin(x); c = cos(x); }
 #define tex2Dlod(s,t)	textureLod(s,t.xy,t.w)
 #define tex3D		texture
 #define texCUBE 	texture
-#define samplerCUBE	samplerCube
 
 // Semantics assignment, maximum 16 slots
 #define COLOR		0
