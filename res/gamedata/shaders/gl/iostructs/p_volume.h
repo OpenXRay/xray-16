a682820a1b8012e241c236b57cc3fcca56a08307
Jules Blok <jules.blok@gmail.com>
2016-03-06 18:46:34 +0100

    shaders: Add GLSL port of dynamic lighting shaders.

--- res/gamedata/shaders/gl/iostructs/p_volume.h
+++ res/gamedata/shaders/gl/iostructs/p_volume.h
@@ -1,32 +1,50 @@
 
 out vec4 SV_Target;
+#ifdef GBUFFER_OPTIMIZATION
 in vec4 gl_FragCoord;
+#endif
 #ifdef MSAA_OPTIMIZATION
 in int gl_SampleID;
 #endif
 
-layout(location = TEXCOORD0)	in 	float4 	v2p_volume_tc		; // TEXCOORD0;
+layout(location = TEXCOORD0)	in 	float4 	p_volume_tc		; // TEXCOORD0;
 #ifdef 	USE_SJITTER
-layout(location = TEXCOORD1)	in 	float4 	v2p_volume_tcJ		; // TEXCOORD1;
+layout(location = TEXCOORD1)	in 	float4 	p_volume_tcJ	; // TEXCOORD1;
 #endif
 
 #ifdef MSAA_OPTIMIZATION
-float4 _main ( v2p_volume I, uint iSample );
+#ifdef GBUFFER_OPTIMIZATION
+float4 _main( p_volume I, float4 pos2d, uint iSample );
 #else
-float4 _main ( v2p_volume I );
+float4 _main( p_volume I, uint iSample  );
+#endif
+#else
+#ifdef GBUFFER_OPTIMIZATION
+float4 _main( p_volume I, float4 pos2d );
+#else
+float4 _main( p_volume I );
+#endif
 #endif
 
 void main()
 {
-	v2p_volume	I;
-	I.tc		= v2p_volume_tc;
+	p_volume	I;
+	I.tc		= p_volume_tc;
 #ifdef 	USE_SJITTER
-	I.tcJ 		= v2p_volume_tcJ;
+	I.tcJ 		= p_volume_tcJ;
 #endif
 
 #ifdef MSAA_OPTIMIZATION
-	SV_Target	= _main ( I, gl_SampleID );
+#ifdef GBUFFER_OPTIMIZATION
+	SV_Target	= _main( I, gl_FragCoord, gl_SampleID );
 #else
-	SV_Target	= _main ( I );
+	SV_Target	= _main( I, gl_SampleID  );
+#endif
+#else
+#ifdef GBUFFER_OPTIMIZATION
+	SV_Target	= _main( I, gl_FragCoord );
+#else
+	SV_Target	= _main( I );
+#endif
 #endif
 }
