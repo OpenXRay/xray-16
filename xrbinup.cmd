c64e28cba732394f0c25214d475fe8090f0fe238
tamlin-mike <tamlin@wurmonline.com>
2016-07-13 05:50:50 +0200

    Cleaned up xrbinup.cmd a little.

--- xrbinup.cmd
+++ xrbinup.cmd
@@ -62,44 +62,54 @@ if %errorLevel% neq 0 (
   set cp_tool=cp
 )
 
-%cp_tool% "!src!\xrEngine.exe" "!dst!\xrEngine.exe"
-%cp_tool% "!src!\xrAPI.dll" "!dst!\xrAPI.dll"
-%cp_tool% "!src!\xrCore.dll" "!dst!\xrCore.dll"
-%cp_tool% "!src!\xrCDB.dll" "!dst!\xrCDB.dll"
-%cp_tool% "!src!\xrSound.dll" "!dst!\xrSound.dll"
-%cp_tool% "!src!\xrParticles.dll" "!dst!\xrParticles.dll"
-%cp_tool% "!src!\xrPhysics.dll" "!dst!\xrPhysics.dll"
-%cp_tool% "!src!\xrD3D9-Null.dll" "!dst!\xrD3D9-Null.dll"
-%cp_tool% "!src!\xrAICore.dll" "!dst!\xrAICore.dll"
-%cp_tool% "!src!\xrScriptEngine.dll" "!dst!\xrScriptEngine.dll"
-%cp_tool% "!src!\xrGame.dll" "!dst!\xrGame.dll"
-%cp_tool% "!src!\xrGameSpy.dll" "!dst!\xrGameSpy.dll"
-%cp_tool% "!src!\xrNetServer.dll" "!dst!\xrNetServer.dll"
-%cp_tool% "!src!\xrRender_R1.dll" "!dst!\xrRender_R1.dll"
-%cp_tool% "!src!\xrRender_R2.dll" "!dst!\xrRender_R2.dll"
-%cp_tool% "!src!\xrRender_R3.dll" "!dst!\xrRender_R3.dll"
-%cp_tool% "!src!\xrRender_R4.dll" "!dst!\xrRender_R4.dll"
-%cp_tool% "!src!\xrRender_GL.dll" "!dst!\xrRender_GL.dll"
+call :COPY_FILE xrEngine.exe
+call :COPY_FILE xrAPI.dll
+call :COPY_FILE xrCore.dll
+call :COPY_FILE xrCDB.dll
+call :COPY_FILE xrSound.dll
+call :COPY_FILE xrParticles.dll
+call :COPY_FILE xrPhysics.dll
+call :COPY_FILE xrD3D9-Null.dll
+call :COPY_FILE xrAICore.dll
+call :COPY_FILE xrScriptEngine.dll
+call :COPY_FILE xrGame.dll
+call :COPY_FILE xrGameSpy.dll
+call :COPY_FILE xrNetServer.dll
+call :COPY_FILE xrRender_R1.dll
+call :COPY_FILE xrRender_R2.dll
+call :COPY_FILE xrRender_R3.dll
+call :COPY_FILE xrRender_R4.dll
+call :COPY_FILE xrRender_GL.dll
 if %platform%==Win32 (
-  %cp_tool% "!src!\amd_ags_x86.dll" "!dst!\amd_ags_x86.dll"
+  call :COPY_FILE amd_ags_x86.dll
 )
 if %platform%==Win64 (
-  %cp_tool% "!src!\amd_ags_x64.dll" "!dst!\amd_ags_x64.dll"
+  call :COPY_FILE amd_ags_x64.dll
 )
 rem CxImage is compiled as DLLs only in debug configuration
 if %cfg%==Debug (
-  %cp_tool% "!src!\CxImage.dll" "!dst!\CxImage.dll"
+  call :COPY_FILE CxImage.dll
 )
-%cp_tool% "!src!\LuaJIT.dll" "!dst!\LuaJIT.dll"
-%cp_tool% "!src!\luabind.dll" "!dst!\luabind.dll"
-%cp_tool% "!src!\ODE.dll" "!dst!\ODE.dll"
-%cp_tool% "!src!\OpenAL32.dll" "!dst!\OpenAL32.dll"
+call :COPY_FILE LuaJIT.dll
+call :COPY_FILE luabind.dll
+call :COPY_FILE ODE.dll
+call :COPY_FILE OpenAL32.dll
 %cp_tool% "!src!\OpenAL32.dll" "!dst!\dedicated\OpenAL32.dll"
-%cp_tool% "!src!_Dedicated\dedicated\xrEngine.exe" "!dst!\dedicated\xrEngine.exe"
-%cp_tool% "!src!\xrWeatherEditor.dll" "!dst!\xrWeatherEditor.dll"
-%cp_tool% "!src!\xrManagedApi.dll" "!dst!\xrManagedApi.dll"
-%cp_tool% "!src!\xrSdkControls.dll" "!dst!\xrSdkControls.dll"
-%cp_tool% "!src!\xrPostprocessEditor.exe" "!dst!\xrPostprocessEditor.exe"
+if exist "!src!_Dedicated\dedicated\xrEngine.exe" (
+  %cp_tool% "!src!_Dedicated\dedicated\xrEngine.exe" "!dst!\dedicated\xrEngine.exe"
+)
+call :COPY_FILE xrWeatherEditor.dll
+call :COPY_FILE xrManagedApi.dll
+call :COPY_FILE xrSdkControls.dll
+call :COPY_FILE xrPostprocessEditor.exe
+
+goto :EOF
+
+:COPY_FILE
+if exist "!src!\%1" (
+%cp_tool% "!src!\%1" "!dst!\"
+)
+goto :EOF
 
 :ret
 endlocal
