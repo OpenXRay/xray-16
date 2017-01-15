6852d9d7cfe361217fd614426ff56d8fab031a9b
Robin <me@invokr.org>
2017-01-15 10:32:54 +0100

    Reformat: xrSound

--- src/xrSound/SoundRender_Cache.h
+++ src/xrSound/SoundRender_Cache.h
@@ -12,62 +12,66 @@
 // 9. "touch" protocol
 // 10. in case of cache-hit we have to move line to mark it used -> list
 
-struct	cache_line;
-struct	cache_ptr;
+struct cache_line;
+struct cache_ptr;
 
 //////////////////////////////////////////////////////////////////////////
-struct	cache_line						// internal, LRU queue
+struct cache_line  // internal, LRU queue
 {
-	cache_line*				prev;
-	cache_line*				next;
-	void*					data;		// pre-formatted
-	u16*					loopback;	// dual-connectivity
-	u16						id;			// need this for dual-connectivity
+    cache_line* prev;
+    cache_line* next;
+    void* data;     // pre-formatted
+    u16* loopback;  // dual-connectivity
+    u16 id;         // need this for dual-connectivity
 };
 //////////////////////////////////////////////////////////////////////////
-struct	cache_cat						// cache allocation table
+struct cache_cat  // cache allocation table
 {
-	u16*					table;		// page-table
-	u32						size;		// in pages
+    u16* table;  // page-table
+    u32 size;    // in pages
 };
-#define CAT_FREE			0xffff
+#define CAT_FREE 0xffff
 //////////////////////////////////////////////////////////////////////////
-class	CSoundRender_Cache
+class CSoundRender_Cache
 {
-	u8*						data;		// just memory
-	cache_line*				c_storage;	// just memory
-	cache_line*				c_begin;	// >>> 
-	cache_line*				c_end;		// <<<
-	u32						_total;		// bytes total (heap)
-	u32						_line;		// line size (bytes)
-	u32						_count;		// number of lines
+    u8* data;               // just memory
+    cache_line* c_storage;  // just memory
+    cache_line* c_begin;    // >>>
+    cache_line* c_end;      // <<<
+    u32 _total;             // bytes total (heap)
+    u32 _line;              // line size (bytes)
+    u32 _count;             // number of lines
 public:
-	u32						_stat_hit;
-	u32						_stat_miss;
+    u32 _stat_hit;
+    u32 _stat_miss;
+
 private:
-	void					move2top	(cache_line* line);					// move one line to TOP-priority
-	void					disconnect	();									// disconnect from CATs
-	void					format		();									// format structure (like filesystem)
+    void move2top(cache_line* line);  // move one line to TOP-priority
+    void disconnect();                // disconnect from CATs
+    void format();                    // format structure (like filesystem)
 public:
-	BOOL					request		(cache_cat& cat, u32 id);			// TRUE=need to fill, FALSE=cached info avail
-	void					purge		();									// discard all contents of cache
+    BOOL request(cache_cat& cat, u32 id);  // TRUE=need to fill, FALSE=cached info avail
+    void purge();                          // discard all contents of cache
 
-	void*					get_dataptr	(cache_cat& cat, u32 id)			{ id%=cat.size; return c_storage[cat.table[id]].data;			} //.
-	u32						get_linesize()									{ return _line;													}
+    void* get_dataptr(cache_cat& cat, u32 id)
+    {
+        id %= cat.size;
+        return c_storage[cat.table[id]].data;
+    }  //.
+    u32 get_linesize() { return _line; }
+    void cat_create(cache_cat& cat, u32 bytes);
+    void cat_destroy(cache_cat& cat);
 
-	void					cat_create	(cache_cat& cat, u32 bytes);
-	void					cat_destroy	(cache_cat& cat);
+    void initialize(u32 _total_kb_approx, u32 bytes_per_line);
+    void destroy();
 
-	void					initialize	(u32 _total_kb_approx, u32 bytes_per_line);
-	void					destroy		();
+    void stats_clear()
+    {
+        _stat_hit = 0;
+        _stat_miss = 0;
+    }
 
-	void					stats_clear	()
-	{
-		_stat_hit			= 0;
-		_stat_miss			= 0;
-	}
-
-	CSoundRender_Cache		();
-	~CSoundRender_Cache		();
+    CSoundRender_Cache();
+    ~CSoundRender_Cache();
 };
 #endif
--- src/xrSound/sound.cpp
+++ src/xrSound/sound.cpp
@@ -3,22 +3,21 @@
 
 #include "SoundRender_CoreA.h"
 
-XRSOUND_API xr_token*		snd_devices_token	= NULL;
-XRSOUND_API u32				snd_device_id		= u32(-1);
+XRSOUND_API xr_token* snd_devices_token = NULL;
+XRSOUND_API u32 snd_device_id = u32(-1);
 void CSound_manager_interface::_create()
 {
     SoundRenderA = new CSoundRender_CoreA();
     SoundRender = SoundRenderA;
     Sound = SoundRender;
-    SoundRender->bPresent = strstr(Core.Params, "-nosound")==nullptr;
-	if (!SoundRender->bPresent)
-        return;
-	Sound->_initialize();
+    SoundRender->bPresent = strstr(Core.Params, "-nosound") == nullptr;
+    if (!SoundRender->bPresent) return;
+    Sound->_initialize();
 }
 
-void CSound_manager_interface::_destroy	()
+void CSound_manager_interface::_destroy()
 {
-	Sound->_clear		();
-    xr_delete			(SoundRender);
-    Sound				= 0;
+    Sound->_clear();
+    xr_delete(SoundRender);
+    Sound = 0;
 }
