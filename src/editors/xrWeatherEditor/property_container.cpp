OpenXRay/xray-16

commit c96c2e4cc9fadf29c023464ba1885e7351d68943

Reformat source code

src/editors/xrWeatherEditor/property_container.cpp
@@ -21,135 +21,126 @@ class ide_impl;
 extern ide_impl* g_ide;
 #pragma managed
 
-property_container::property_container			(
-		property_holder* holder,
-		property_container_holder^ container_holder
-	) :
-	m_holder					(holder),
-	m_container_holder			(container_holder),
-	m_properties				(gcnew Hashtable()),
-	m_ordered_properties		(gcnew ArrayList())
+property_container::property_container(property_holder* holder, property_container_holder ^ container_holder)
+    : m_holder(holder), m_container_holder(container_holder), m_properties(gcnew Hashtable()),
+      m_ordered_properties(gcnew ArrayList())
 {
-	GetValue					+= gcnew PropertySpecEventHandler(this, &property_container::get_value_handler);
-	SetValue					+= gcnew PropertySpecEventHandler(this, &property_container::set_value_handler);
+    GetValue += gcnew PropertySpecEventHandler(this, &property_container::get_value_handler);
+    SetValue += gcnew PropertySpecEventHandler(this, &property_container::set_value_handler);
 }
 
-property_container::~property_container			()
+property_container::~property_container()
 {
-	this->!property_container	();
+    this->!property_container();
 }
 
-property_container::!property_container			()
+property_container::!property_container()
 {
-	if (!m_holder)
-		return;
+    if (!m_holder) return;
 
-	if (!g_ide)
-		return;
+    if (!g_ide) return;
 
-	property_holder*			holder = dynamic_cast<property_holder*>(m_holder);
-	VERIFY						(holder);
+    property_holder* holder = dynamic_cast<property_holder*>(m_holder);
+    VERIFY(holder);
 
-	holder->on_dispose			();
+    holder->on_dispose();
 }
 
-property_holder &property_container::holder		()
+property_holder& property_container::holder()
 {
-	VERIFY						(m_holder);
-	return						(*m_holder);
+    VERIFY(m_holder);
+    return (*m_holder);
 }
 
-property_container_holder% property_container::container_holder	()
+property_container_holder % property_container::container_holder()
 {
-	VERIFY						(m_container_holder);
-	return						(*m_container_holder);
+    VERIFY(m_container_holder);
+    return (*m_container_holder);
 }
 
-bool property_container::equal_category			(String^ new_category, String^ old_category)
+bool property_container::equal_category(String ^ new_category, String ^ old_category)
 {
-	VERIFY						(!new_category->Length || (new_category[0] != '\t'));
-	if (!old_category->Length || (old_category[0] != '\t'))
-		return					(new_category == old_category);
+    VERIFY(!new_category->Length || (new_category[0] != '\t'));
+    if (!old_category->Length || (old_category[0] != '\t')) return (new_category == old_category);
 
-	for (u32 i=0, n=old_category->Length; i<n; ++i) {
-		if (old_category[i] == '\t')
-			continue;
+    for (u32 i = 0, n = old_category->Length; i < n; ++i)
+    {
+        if (old_category[i] == '\t') continue;
 
-		return					(new_category == old_category->Substring(i, n - i));
-	}
+        return (new_category == old_category->Substring(i, n - i));
+    }
 
-	NODEFAULT;
+    NODEFAULT;
 #ifdef DEBUG
-	return						(false);
-#endif // #ifdef DEBUG
+    return (false);
+#endif  // #ifdef DEBUG
 }
 
-String^ property_container::update_categories	(String^ new_category)
+String ^ property_container::update_categories(String ^ new_category)
 {
-	for each (PropertySpec^ i in m_ordered_properties) {
-		String^					category = i->Category;
-		if (!equal_category(new_category, category))
-			continue;
+    for
+        each(PropertySpec ^ i in m_ordered_properties)
+        {
+            String ^ category = i->Category;
+            if (!equal_category(new_category, category)) continue;
 
-		return					(category);
-	}
+            return (category);
+        }
 
-	for each (PropertySpec^ i in m_ordered_properties)
-		i->Category				= "\t" + i->Category;
+    for
+        each(PropertySpec ^ i in m_ordered_properties) i->Category = "\t" + i->Category;
 
-	return						(new_category);
+    return (new_category);
 }
 
-void property_container::try_update_name		(PropertySpec^ description, String^ name)
+void property_container::try_update_name(PropertySpec ^ description, String ^ name)
 {
-	VERIFY						(!!name->Length);
-	VERIFY						((name[0] != '\t'));
-	
-	String^						description_name = description->Name;
-	VERIFY						(!!description_name->Length);
-	if (description_name[0] != '\t') {
-		if (name != description_name)
-			return;
-		
-		description->Name		= "\t" + description_name;
-		return;
-	}
+    VERIFY(!!name->Length);
+    VERIFY((name[0] != '\t'));
 
-	for (u32 i=0, n=description_name->Length; i<n; ++i) {
-		if (description_name[i] == '\t')
-			continue;
+    String ^ description_name = description->Name;
+    VERIFY(!!description_name->Length);
+    if (description_name[0] != '\t') {
+        if (name != description_name) return;
 
-		if (name != description_name->Substring(i, n - i))
-			return;
-		
-		description->Name		= "\t" + description->Name;
-		return;
-	}
+        description->Name = "\t" + description_name;
+        return;
+    }
 
-	NODEFAULT;
+    for (u32 i = 0, n = description_name->Length; i < n; ++i)
+    {
+        if (description_name[i] == '\t') continue;
+
+        if (name != description_name->Substring(i, n - i)) return;
+
+        description->Name = "\t" + description->Name;
+        return;
+    }
+
+    NODEFAULT;
 }
 
-void property_container::update_names			(String^ name)
+void property_container::update_names(String ^ name)
 {
-	bool						found = false;
-	for each (PropertySpec^ i in m_ordered_properties) {
-		if (i->Name != name)
-			continue;
+    bool found = false;
+    for
+        each(PropertySpec ^ i in m_ordered_properties)
+        {
+            if (i->Name != name) continue;
 
-		found					= true;
-		break;
-	}
+            found = true;
+            break;
+        }
 
-	if (!found)
-		return;
+    if (!found) return;
 
-	for each (PropertySpec^ i in m_ordered_properties)
-		try_update_name			(i, name);
+    for
+        each(PropertySpec ^ i in m_ordered_properties) try_update_name(i, name);
 }
 
-void property_container::add_property			(PropertySpec^ description, IProperty^ value)
+void property_container::add_property(PropertySpec ^ description, IProperty ^ value)
 {
-	VERIFY						(!m_properties[description]);
+    VERIFY(!m_properties[description]);
 
 #if 0
 	u32							n = description->Attributes ? description->Attributes->Length : 0;
@@ -161,49 +152,49 @@ void property_container::add_property			(PropertySpec^ description, IProperty^ v
 	description->Attributes		= attributes;
 #endif
 
-	description->Category		= update_categories(description->Category);
-	update_names				(description->Name);
-	m_properties[description]	= value;
-	Properties->Add				(description);
-	m_ordered_properties->Add	(description);
+    description->Category = update_categories(description->Category);
+    update_names(description->Name);
+    m_properties[description] = value;
+    Properties->Add(description);
+    m_ordered_properties->Add(description);
 }
 
-IProperty^ property_container::GetProperty		(PropertySpec^ description)
+IProperty ^ property_container::GetProperty(PropertySpec ^ description)
 {
-	VERIFY						(m_properties[description]);
-	return						(safe_cast<IProperty^>(m_properties[description]));
+    VERIFY(m_properties[description]);
+    return (safe_cast<IProperty ^>(m_properties[description]));
 }
 
-IDictionary% property_container::properties		()
+IDictionary % property_container::properties()
 {
-	VERIFY						(m_properties);
-	return						(*m_properties);
+    VERIFY(m_properties);
+    return (*m_properties);
 }
 
-ArrayList% property_container::ordered_properties	()
+ArrayList % property_container::ordered_properties()
 {
-	VERIFY						(m_ordered_properties);
-	return						(*m_ordered_properties);
+    VERIFY(m_ordered_properties);
+    return (*m_ordered_properties);
 }
 
-void property_container::get_value_handler		(Object^ sender, PropertySpecEventArgs^ e)
+void property_container::get_value_handler(Object ^ sender, PropertySpecEventArgs ^ e)
 {
-	IProperty				^value = safe_cast<IProperty^>(m_properties[e->Property]);
-	VERIFY						(value);
-	e->Value					= value->GetValue();
+    IProperty ^ value = safe_cast<IProperty ^>(m_properties[e->Property]);
+    VERIFY(value);
+    e->Value = value->GetValue();
 }
 
-void property_container::set_value_handler		(Object^ sender, PropertySpecEventArgs^ e)
+void property_container::set_value_handler(Object ^ sender, PropertySpecEventArgs ^ e)
 {
-	IProperty				^value = safe_cast<IProperty^>(m_properties[e->Property]);
-	VERIFY						(value);
-	value->SetValue			(e->Value);
+    IProperty ^ value = safe_cast<IProperty ^>(m_properties[e->Property]);
+    VERIFY(value);
+    value->SetValue(e->Value);
 }
 
-void property_container::clear					()
+void property_container::clear()
 {
-	m_properties->Clear			();
-	m_categories->Clear			();
-	m_ordered_properties->Clear	();
-	Properties->Clear			();
+    m_properties->Clear();
+    m_categories->Clear();
+    m_ordered_properties->Clear();
+    Properties->Clear();
 }

src/editors/xrWeatherEditor/property_container_converter.cpp
@@ -18,64 +18,50 @@ using System::Array;
 using System::Collections::ArrayList;
 using Flobbster::Windows::Forms::PropertySpec;
 
-PropertyDescriptorCollection^ property_container_converter::GetProperties	(
-		ITypeDescriptorContext^ context,
-		Object^ value,
-		array<Attribute^>^ attributes
-	)
+PropertyDescriptorCollection ^ property_container_converter::GetProperties(
+                                   ITypeDescriptorContext ^ context, Object ^ value, array<Attribute ^> ^ attributes)
 {
-	PropertyDescriptorCollection^	current = TypeDescriptor::GetProperties(value, attributes);
-	VERIFY							(current);
-	property_container^				container = dynamic_cast<property_container^>(context->Instance);
-	if (container) {
-		ArrayList%					properties = container->ordered_properties();
-		ArrayList^					names = gcnew ArrayList();
-		for each (PropertySpec^ i in properties)
-			names->Add				(i->Name);
+    PropertyDescriptorCollection ^ current = TypeDescriptor::GetProperties(value, attributes);
+    VERIFY(current);
+    property_container ^ container = dynamic_cast<property_container ^>(context->Instance);
+    if (container) {
+        ArrayList % properties = container->ordered_properties();
+        ArrayList ^ names = gcnew ArrayList();
+        for
+            each(PropertySpec ^ i in properties) names->Add(i->Name);
 
-		return						(current->Sort(reinterpret_cast<array<String^>^>(names->ToArray(String::typeid))));
-	}
+        return (current->Sort(reinterpret_cast<array<String ^> ^>(names->ToArray(String::typeid))));
+    }
 
-	// here we should construct an intersection of properties sets
-	// instead of just getting the properties from the first object
-	Array^							objects = safe_cast<Array^>(context->Instance);
-	VERIFY							(objects->Length);
-	container						= safe_cast<property_container^>(objects->GetValue(0));
-	ArrayList%						properties = container->ordered_properties();
-	ArrayList^						names = gcnew ArrayList();
-	for each (PropertySpec^ i in properties)
-		names->Add					(i->Name);
+    // here we should construct an intersection of properties sets
+    // instead of just getting the properties from the first object
+    Array ^ objects = safe_cast<Array ^>(context->Instance);
+    VERIFY(objects->Length);
+    container = safe_cast<property_container ^>(objects->GetValue(0));
+    ArrayList % properties = container->ordered_properties();
+    ArrayList ^ names = gcnew ArrayList();
+    for
+        each(PropertySpec ^ i in properties) names->Add(i->Name);
 
-	return							(current->Sort(reinterpret_cast<array<String^>^>(names->ToArray(String::typeid))));
+    return (current->Sort(reinterpret_cast<array<String ^> ^>(names->ToArray(String::typeid))));
 }
 
-bool property_container_converter::GetPropertiesSupported					(
-		ITypeDescriptorContext^ context
-	)
+bool property_container_converter::GetPropertiesSupported(ITypeDescriptorContext ^ context)
 {
-	return		(true);
+    return (true);
 }
 
-bool property_container_converter::CanConvertTo								(
-		ITypeDescriptorContext^ context,
-		Type^ destination_type
-	)
+bool property_container_converter::CanConvertTo(ITypeDescriptorContext ^ context, Type ^ destination_type)
 {
-	if (destination_type == property_container::typeid)
-		return	(true);
+    if (destination_type == property_container::typeid) return (true);
 
-	return		(inherited::CanConvertTo(context, destination_type));
+    return (inherited::CanConvertTo(context, destination_type));
 }
 
-Object^ property_container_converter::ConvertTo								(
-		ITypeDescriptorContext^ context,
-		CultureInfo^ culture,
-		Object^ value,
-		Type^ destination_type
-	)
+Object ^ property_container_converter::ConvertTo(
+             ITypeDescriptorContext ^ context, CultureInfo ^ culture, Object ^ value, Type ^ destination_type)
 {
-	if (destination_type != String::typeid)
-		return	(inherited::ConvertTo(context, culture, value, destination_type));
+    if (destination_type != String::typeid) return (inherited::ConvertTo(context, culture, value, destination_type));
 
-	return		("...");
+    return ("...");
 }

commit 87fd7aa720fafc2b992d9fac4443f6d097d88d40

Fix compilation of xrWeatherEditor
Was broken with c96c2e4cc9fadf29c023464ba1885e7351d68943
Corresponding formatting fixes

src/editors/xrWeatherEditor/property_container.cpp
@@ -78,18 +78,17 @@ bool property_container::equal_category(String ^ new_category, String ^ old_cate
 
 String ^ property_container::update_categories(String ^ new_category)
 {
-    for
-        each(PropertySpec ^ i in m_ordered_properties)
-        {
-            String ^ category = i->Category;
-            if (!equal_category(new_category, category))
-                continue;
+    for each(PropertySpec ^ i in m_ordered_properties)
+    {
+        String ^ category = i->Category;
+        if (!equal_category(new_category, category))
+            continue;
 
-            return (category);
-        }
+        return (category);
+    }
 
-    for
-        each(PropertySpec ^ i in m_ordered_properties) i->Category = "\t" + i->Category;
+    for each(PropertySpec ^ i in m_ordered_properties)
+        i->Category = "\t" + i->Category;
 
     return (new_category);
 }
@@ -128,21 +127,19 @@ void property_container::try_update_name(PropertySpec ^ description, String ^ na
 void property_container::update_names(String ^ name)
 {
     bool found = false;
-    for
-        each(PropertySpec ^ i in m_ordered_properties)
-        {
-            if (i->Name != name)
-                continue;
+    for each(PropertySpec ^ i in m_ordered_properties)
+    {
+        if (i->Name != name)
+            continue;
 
-            found = true;
-            break;
-        }
+        found = true;
+        break;
+    }
 
     if (!found)
         return;
 
-    for
-        each(PropertySpec ^ i in m_ordered_properties) try_update_name(i, name);
+    for each(PropertySpec ^ i in m_ordered_properties) try_update_name(i, name);
 }
 
 void property_container::add_property(PropertySpec ^ description, IProperty ^ value)

src/editors/xrWeatherEditor/property_container_converter.cpp
@@ -28,8 +28,7 @@ PropertyDescriptorCollection ^ property_container_converter::GetProperties(
     {
         ArrayList % properties = container->ordered_properties();
         ArrayList ^ names = gcnew ArrayList();
-        for
-            each(PropertySpec ^ i in properties) names->Add(i->Name);
+        for each(PropertySpec ^ i in properties) names->Add(i->Name);
 
         return (current->Sort(reinterpret_cast<array<String ^> ^>(names->ToArray(String::typeid))));
     }
@@ -41,8 +40,7 @@ PropertyDescriptorCollection ^ property_container_converter::GetProperties(
     container = safe_cast<property_container ^>(objects->GetValue(0));
     ArrayList % properties = container->ordered_properties();
     ArrayList ^ names = gcnew ArrayList();
-    for
-        each(PropertySpec ^ i in properties) names->Add(i->Name);
+    for each(PropertySpec ^ i in properties) names->Add(i->Name);
 
     return (current->Sort(reinterpret_cast<array<String ^> ^>(names->ToArray(String::typeid))));
 }

src/editors/xrWeatherEditor/property_converter_float_enum.cpp
@@ -55,14 +55,13 @@ Object ^ property_converter_float_enum::ConvertTo(
     property_float_enum_value ^ real_value = safe_cast<property_float_enum_value ^>(raw_value);
     float float_value = safe_cast<float>(value);
 
-    for
-        each(ValuePair ^ i in real_value->m_collection)
-        {
-            if (i->first != float_value)
-                continue;
+    for each(ValuePair ^ i in real_value->m_collection)
+    {
+        if (i->first != float_value)
+            continue;
 
-            return (i->second);
-        }
+        return (i->second);
+    }
 
     return (safe_cast<ValuePair ^>(real_value->m_collection[0])->second);
 }

src/editors/xrWeatherEditor/property_converter_integer_enum.cpp
@@ -55,14 +55,13 @@ Object ^ property_converter_integer_enum::ConvertTo(
     property_integer_enum_value ^ real_value = safe_cast<property_integer_enum_value ^>(raw_value);
     int int_value = safe_cast<int>(value);
 
-    for
-        each(ValuePair ^ i in real_value->m_collection)
-        {
-            if (i->first != int_value)
-                continue;
+    for each(ValuePair ^ i in real_value->m_collection)
+    {
+        if (i->first != int_value)
+            continue;
 
-            return (i->second);
-        }
+        return (i->second);
+     }
 
     return (safe_cast<ValuePair ^>(real_value->m_collection[0])->second);
 }

src/editors/xrWeatherEditor/property_float_enum_value.cpp
@@ -27,14 +27,13 @@ property_float_enum_value::property_float_enum_value(
 System::Object ^ property_float_enum_value::GetValue()
 {
     float value = safe_cast<float>(inherited::GetValue());
-    for
-        each(ValuePair ^ i in m_collection)
-        {
-            if (i->first != value)
-                continue;
+    for each(ValuePair ^ i in m_collection)
+    {
+        if (i->first != value)
+            continue;
 
-            return (value);
-        }
+        return (value);
+    }
 
     return (safe_cast<ValuePair ^>(m_collection[0])->first);
 }
@@ -43,15 +42,14 @@ void property_float_enum_value::SetValue(Object ^ object)
 {
     String ^ string_value = dynamic_cast<String ^>(object);
 
-    for
-        each(ValuePair ^ i in m_collection)
-        {
-            if (!i->second->Equals(string_value))
-                continue;
+    for each(ValuePair ^ i in m_collection)
+    {
+        if (!i->second->Equals(string_value))
+            continue;
 
-            inherited::SetValue(i->first);
-            return;
-        }
+        inherited::SetValue(i->first);
+        return;
+    }
 
     inherited::SetValue(safe_cast<ValuePair ^>(m_collection[0])->first);
 }

src/editors/xrWeatherEditor/property_float_enum_value_reference.cpp
@@ -27,14 +27,13 @@ property_float_enum_value_reference::property_float_enum_value_reference(
 System::Object ^ property_float_enum_value_reference::GetValue()
 {
     float value = safe_cast<float>(inherited::GetValue());
-    for
-        each(ValuePair ^ i in m_collection)
-        {
-            if (i->first != value)
-                continue;
+    for each(ValuePair ^ i in m_collection)
+    {
+        if (i->first != value)
+            continue;
 
-            return (value);
-        }
+        return (value);
+    }
 
     return (safe_cast<ValuePair ^>(m_collection[0])->first);
 }
@@ -43,15 +42,14 @@ void property_float_enum_value_reference::SetValue(Object ^ object)
 {
     String ^ string_value = dynamic_cast<String ^>(object);
 
-    for
-        each(ValuePair ^ i in m_collection)
-        {
-            if (!i->second->Equals(string_value))
-                continue;
+    for each(ValuePair ^ i in m_collection)
+    {
+        if (!i->second->Equals(string_value))
+            continue;
 
-            inherited::SetValue(i->first);
-            return;
-        }
+        inherited::SetValue(i->first);
+        return;
+    }
 
     inherited::SetValue(safe_cast<ValuePair ^>(m_collection[0])->first);
 }

src/editors/xrWeatherEditor/property_integer_enum_value.cpp
@@ -27,14 +27,13 @@ property_integer_enum_value::property_integer_enum_value(
 System::Object ^ property_integer_enum_value::GetValue()
 {
     int value = safe_cast<int>(inherited::GetValue());
-    for
-        each(ValuePair ^ i in m_collection)
-        {
-            if (i->first != value)
-                continue;
+    for each(ValuePair ^ i in m_collection)
+    {
+        if (i->first != value)
+            continue;
 
-            return (value);
-        }
+        return (value);
+    }
 
     return (safe_cast<ValuePair ^>(m_collection[0])->first);
 }
@@ -43,15 +42,14 @@ void property_integer_enum_value::SetValue(Object ^ object)
 {
     String ^ string_value = dynamic_cast<String ^>(object);
 
-    for
-        each(ValuePair ^ i in m_collection)
-        {
-            if (!i->second->Equals(string_value))
-                continue;
+    for each(ValuePair ^ i in m_collection)
+    {
+        if (!i->second->Equals(string_value))
+            continue;
 
-            inherited::SetValue(i->first);
-            return;
-        }
+        inherited::SetValue(i->first);
+        return;
+    }
 
     inherited::SetValue(safe_cast<ValuePair ^>(m_collection[0])->first);
 }

src/editors/xrWeatherEditor/property_integer_enum_value_reference.cpp
@@ -27,14 +27,13 @@ property_integer_enum_value_reference::property_integer_enum_value_reference(
 System::Object ^ property_integer_enum_value_reference::GetValue()
 {
     int value = safe_cast<int>(inherited::GetValue());
-    for
-        each(ValuePair ^ i in m_collection)
-        {
-            if (i->first != value)
-                continue;
+    for each(ValuePair ^ i in m_collection)
+    {
+        if (i->first != value)
+            continue;
 
-            return (value);
-        }
+        return (value);
+    }
 
     return (safe_cast<ValuePair ^>(m_collection[0])->first);
 }
@@ -43,15 +42,14 @@ void property_integer_enum_value_reference::SetValue(Object ^ object)
 {
     String ^ string_value = dynamic_cast<String ^>(object);
 
-    for
-        each(ValuePair ^ i in m_collection)
-        {
-            if (!i->second->Equals(string_value))
-                continue;
+    for each(ValuePair ^ i in m_collection)
+    {
+        if (!i->second->Equals(string_value))
+            continue;
 
-            inherited::SetValue(i->first);
-            return;
-        }
+        inherited::SetValue(i->first);
+        return;
+    }
 
     inherited::SetValue(safe_cast<ValuePair ^>(m_collection[0])->first);
 }

src/editors/xrWeatherEditor/window_tree_values.cpp
@@ -41,27 +41,25 @@ void window_tree_values::values(property_string_values_value_base::collection_ty
 
     TreeNode ^ selected = nullptr;
 
-    for
-        each(String ^ i in values)
+    for each(String ^ i in values)
+    {
+        TreeNode ^ current = nullptr;
+        String ^ j = i;
+        while (j->Length)
         {
-            TreeNode ^ current = nullptr;
-            String ^ j = i;
-            while (j->Length)
+            int index = j->IndexOf('\\');
+            String ^ k = index < 0 ? j : j->Substring(0, index);
+            j = index < 0 ? "" : j->Substring(index + 1);
+            if (!current)
             {
-                int index = j->IndexOf('\\');
-                String ^ k = index < 0 ? j : j->Substring(0, index);
-                j = index < 0 ? "" : j->Substring(index + 1);
-                if (!current)
+                for each(TreeNode ^ i in TreeView->Nodes)
                 {
-                for
-                    each(TreeNode ^ i in TreeView->Nodes)
-                    {
-                        if (i->Text != k)
-                            continue;
+                    if (i->Text != k)
+                        continue;
 
-                        current = i;
-                        break;
-                    }
+                    current = i;
+                    break;
+                }
 
                 if (current)
                     continue;
@@ -70,33 +68,32 @@ void window_tree_values::values(property_string_values_value_base::collection_ty
                 current->ImageIndex = 0;
                 current->SelectedImageIndex = 0;
                 continue;
-                }
+            }
 
-                bool found = false;
-            for
-                each(TreeNode ^ i in current->Nodes)
-                {
-                    if (i->Text != k)
-                        continue;
+            bool found = false;
+            for each(TreeNode ^ i in current->Nodes)
+            {
+                if (i->Text != k)
+                    continue;
 
-                    found = true;
-                    current = i;
-                    break;
-                }
+                found = true;
+                current = i;
+                break;
+            }
             if (!found)
             {
                 current->ImageIndex = 0;
                 current->SelectedImageIndex = 0;
                 current = current->Nodes->Add(k);
             }
-            }
-
-            if (current->FullPath == current_value)
-                selected = current;
-
-            current->ImageIndex = 2;
-            current->SelectedImageIndex = 2;
         }
+
+        if (current->FullPath == current_value)
+            selected = current;
+
+        current->ImageIndex = 2;
+        current->SelectedImageIndex = 2;
+    }
 
     TreeView->TreeViewNodeSorter = gcnew NodeSorter();
     TreeView->Sort();