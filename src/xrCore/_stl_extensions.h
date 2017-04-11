18ec79dde3c9145fff1ca4d7b6585d8b3cc04b56
Xottab-DUTY <xottab-duty@yandex.ru>
2017-04-11 22:14:23 +0500

    Removed xrCore mk_pair

--- src/xrCore/_stl_extensions.h
+++ src/xrCore/_stl_extensions.h
@@ -346,12 +346,6 @@ public:
 
 #endif
 
-template <class _Ty1, class _Ty2>
-inline std::pair<_Ty1, _Ty2> mk_pair(_Ty1 _Val1, _Ty2 _Val2)
-{
-    return (std::pair<_Ty1, _Ty2>(_Val1, _Val2));
-}
-
 struct pred_str : public std::binary_function<char*, char*, bool>
 {
     IC bool operator()(const char* x, const char* y) const { return xr_strcmp(x, y) < 0; }
