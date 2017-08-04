c2e6bd8948ca60b6b06cebad7606448a1b0a095c
Xottab-DUTY <xottab-duty@yandex.ru>
2017-08-04 06:17:46 +0500

    How did this happen
    Did not see some mk_pair and thus forgot to replace it

--- src/Layers/xrRenderPC_R3/r3_rendertarget_enable_scissor.cpp
+++ src/Layers/xrRenderPC_R3/r3_rendertarget_enable_scissor.cpp
@@ -92,7 +92,7 @@ BOOL CRenderTarget::enable_scissor(light* L) // true if intersects near plane
     {
         Fsphere S;
         S.set(L->spatial.sphere.P, L->spatial.sphere.R);
-        dbg_spheres.push_back(mk_pair(S, L->color));
+        dbg_spheres.push_back(std::make_pair(S, L->color));
     }
 #endif
 
