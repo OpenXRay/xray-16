c8eae93b2fe0b6303ad2e21d89cf744cddba56a4
Xottab-DUTY <xottab-duty@yandex.ru>
2017-08-12 21:09:55 +0500

    Fix x64 issues Thanks to Im-Dex. xrParticles problem: https://github.com/Im-dex/xray-162/commit/8da28e52c043a98366d909eb18223dd59fd3e443
    Collision detection issue:
    https://github.com/Im-dex/xray-162/commit/cd40b5692da9ae178e6c35fe78da6c7c5ffc126d

--- src/xrParticles/particle_effect.h
+++ src/xrParticles/particle_effect.h
@@ -29,7 +29,10 @@ public:
         particles_allocated = max_particles;
 
         real_ptr = xr_malloc(sizeof(Particle) * (max_particles + 1));
-        particles = (Particle*)((DWORD)real_ptr + (64 - ((DWORD)real_ptr & 63)));
+        
+        particles = (Particle*)((uintptr_t)real_ptr + (64 - ((uintptr_t)real_ptr & 63)));
+        //particles = static_cast<Particle*>(real_ptr);
+
         // Msg( "Allocated %u bytes (%u particles) with base address 0x%p" , max_particles * sizeof( Particle ) ,
         // max_particles , particles );
     }
@@ -58,7 +61,7 @@ public:
             return max_particles;
         }
 
-        Particle* new_particles = (Particle*)((DWORD)new_real_ptr + (64 - ((DWORD)new_real_ptr & 63)));
+        Particle* new_particles = (Particle*)((uintptr_t)new_real_ptr + (64 - ((uintptr_t)new_real_ptr & 63)));
         // Msg( "Re-allocated %u bytes (%u particles) with base address 0x%p" , max_count * sizeof( Particle ) ,
         // max_count , new_particles );
 
--- src/xrPhysics/tri-colliderknoopc/dTriColliderCommon.h
+++ src/xrPhysics/tri-colliderknoopc/dTriColliderCommon.h
@@ -10,7 +10,8 @@ extern xr_vector<flags8> gl_cl_tries_state;
 // extern xr_vector<int>::iterator		I,E,B				;
 
 #define CONTACT(Ptr, Stride) ((dContactGeom*)(((char*)Ptr) + (Stride)))
-#define SURFACE(Ptr, Stride) ((dSurfaceParameters*)(((char*)Ptr) + (Stride - sizeof(dSurfaceParameters))))
+//#define SURFACE(Ptr, Stride) ((dSurfaceParameters*)(((char*)Ptr) + (Stride - sizeof(dSurfaceParameters))))
+#define SURFACE(Ptr, Stride) ((dSurfaceParameters*) (((char*)Ptr) + (Stride - offsetof(dContact, geom) + offsetof(dContact, surface))))
 #define NUMC_MASK (0xffff)
 
 #define M_SIN_PI_3 REAL(0.8660254037844386467637231707529362)
