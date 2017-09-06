8ea3ddbb5bdb2d9e38af221c9a2e5fae2b0eaa20
Xottab-DUTY <xottab-duty@yandex.ru>
2017-09-06 20:52:15 +0500

    Add some stub for future refactoring (https://github.com/Xottab-DUTY/xray-16/issues/1)

--- src/Include/editor/interfaces.hpp
+++ src/Include/editor/interfaces.hpp
@@ -9,6 +9,19 @@
 #ifndef EDITOR_INTERFACES_HPP_INCLUDED
 #define EDITOR_INTERFACES_HPP_INCLUDED
 
+namespace XRay
+{
+namespace Editor
+{
+class ide_base;
+class engine_base;
+
+// XXX: is __cdecl needed?
+using initialize_function_ptr = void(__cdecl*)(ide_base*&, engine_base*);
+using finalize_function_ptr = void(__cdecl*)(ide_base*&);
+}
+}
+
 namespace editor
 {
 class ide_base;
