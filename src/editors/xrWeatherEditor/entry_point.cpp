4c23540f267bab498bdf32d0f08dda10069a486d
Xottab-DUTY <xottab-duty@yandex.ru>
2017-11-05 06:42:57 +0500

    Add x64 support for xrWeatherEditor
    Thanks to ForserX

--- src/editors/xrWeatherEditor/entry_point.cpp
+++ src/editors/xrWeatherEditor/entry_point.cpp
@@ -30,10 +30,14 @@ public:
 protected:
     virtual void WndProc(Message % m) override
     {
-        LRESULT result;
-        if (m_engine &&
-            m_engine->on_message((HWND)m.HWnd.ToInt32(), m.Msg, m.WParam.ToInt32(), m.LParam.ToInt32(), result))
+        LONG_PTR result;
+#ifdef XR_X64
+        if (m_engine && m_engine->on_message((HWND)m.HWnd.ToInt32(), m.Msg, m.WParam.ToInt64(), m.LParam.ToInt64(), result))
             return;
+#else
+        if (m_engine && m_engine->on_message((HWND)m.HWnd.ToInt32(), m.Msg, m.WParam.ToInt32(), m.LParam.ToInt32(), result))
+            return;
+#endif
 
         editor::window_ide::WndProc(m);
     }
