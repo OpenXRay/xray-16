ea56b09e00b9b55ddc095ab663e2b7208f477635
Xottab-DUTY <xottab-duty@yandex.ru>
2017-11-11 14:25:10 +0500

    New Module class for dynamic library linking

--- src/xrCore/ModuleLookup.cpp
+++ src/xrCore/ModuleLookup.cpp
@@ -2,11 +2,53 @@
 
 #include "ModuleLookup.hpp"
 
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
 namespace XRay
 {
+Module::Module() : handle(nullptr) {}
+
+Module::Module(pcstr moduleName, bool log /*= true*/)
+{
+    open(moduleName, log);
+}
+
+Module::~Module()
+{
+    close();
+}
+
+void* Module::open(pcstr moduleName, bool log /*= true*/)
+{
+    if (exist())
+        close();
+
+    if (log)
+        Log("Loading DLL:", moduleName);
+
+    handle = ::LoadLibrary(moduleName);
+    return handle;
+}
+
+void Module::close()
+{
+    FreeLibrary(static_cast<HMODULE>(handle));
+    handle = nullptr;
+}
+
+bool Module::exist() const
+{
+    return handle != nullptr;
+}
+
+void* Module::operator()() const
+{
+    return handle;
+}
+
+void* Module::getProcAddress(pcstr procName) const
+{
+    return ::GetProcAddress(static_cast<HMODULE>(handle), procName);
+}
+
 HMODULE LoadLibrary(const char* libraryFileName, bool log)
 {
     if (log)
--- src/xrCore/ModuleLookup.hpp
+++ src/xrCore/ModuleLookup.hpp
@@ -4,6 +4,25 @@
 
 namespace XRay
 {
+class XRCORE_API Module
+{
+    void* handle;
+
+public:
+    Module();
+    Module(pcstr moduleName, bool log = true);
+    ~Module();
+
+    void* open(pcstr moduleName, bool log = true);;
+    void close();
+
+    bool exist() const;
+
+    void* operator()() const;
+
+    void* getProcAddress(pcstr procName) const;
+};
+
 XRCORE_API HMODULE LoadLibrary(const char* libraryFileName, bool log = true);
 XRCORE_API void UnloadLibrary(HMODULE libraryHandle);
 XRCORE_API void* GetProcAddress(HMODULE libraryHandle, const char* procName);
