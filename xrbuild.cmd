689954c7c243ed976e6cea63b4fcdccd625b9693
Xottab-DUTY <xottab-duty@yandex.ru>
2017-12-11 15:53:08 +0500

    Add batch build script for AppVeyor

---
+++ xrbuild.cmd
@@ -0,0 +1,59 @@
+md res\bins\ 
+
+if %PLATFORM% == 'Debug' if %CONFIGURATION% == 'x86' goto :DX86 
+if %PLATFORM% == 'Debug' if %CONFIGURATION% == 'x64' goto :DX64 
+if %PLATFORM% == 'Release' if %CONFIGURATION% == 'x86' goto :RX86 
+if %PLATFORM% == 'Release' if %CONFIGURATION% == 'x64' goto :RX64 
+
+echo FAIL
+goto :END 
+
+:DX86 
+cd bin\Win32\Debug 
+copy *.dll ..\..\..\res\bins\ 
+copy *.exe ..\..\..\res\bins\ 
+cd ..\..\..\ 
+dir 
+copy License.txt .\res\ 
+copy README.md .\res\ 
+cd res\ 
+7z a xdOpenXRay.Dx86.7z .\* 
+goto :END 
+
+:DX64 
+cd bin\Win64\Debug 
+copy *.dll ..\..\..\res\bins\ 
+copy *.exe ..\..\..\res\bins\ 
+cd ..\..\..\ 
+dir 
+copy License.txt .\res\ 
+copy README.md .\res\ 
+cd res\ 
+7z a xdOpenXRay.Dx64.7z .\* 
+goto :END 
+
+:RX86 
+cd bin\Win86\Release 
+copy *.dll ..\..\..\res\bins\ 
+copy *.exe ..\..\..\res\bins\ 
+cd ..\..\..\ 
+dir 
+copy License.txt .\res\ 
+copy README.md .\res\ 
+cd res\ 
+7z a xdOpenXRay.Rx86.7z .\* 
+goto :END 
+
+:RX64 
+cd bin\Win64\Release 
+copy *.dll ..\..\..\res\bins\ 
+copy *.exe ..\..\..\res\bins\ 
+cd ..\..\..\ 
+dir 
+copy License.txt .\res\ 
+copy README.md .\res\ 
+cd res\ 
+7z a xdOpenXRay.Rx64.7z .\* 
+goto :END 
+
+:END 
