5e5cf4754e7c993536741cd691a8eb993714b581
Xottab-DUTY <xottab-duty@yandex.ru>
2017-12-12 22:36:21 +0500

    Fix crash when NPC trying to open the door

--- src/xrGame/doors_door.cpp
+++ src/xrGame/doors_door.cpp
@@ -157,7 +157,7 @@ void door::change_state(actor* initiator)
     if (m_state == m_target_state)
         return;
 
-    m_object.callback(GameObject::eUseObject)(m_object.lua_game_object(), (CScriptGameObject*)initiator->lua_game_object());
+    m_object.callback(GameObject::eUseObject)(m_object.lua_game_object(), initiator ? static_cast<CScriptGameObject*>(initiator->lua_game_object()) : nullptr);
 #ifdef DEBUG
     if (g_debug_doors)
         Msg("door[%s] started to change its state to [%s]", m_object.cName().c_str(),
