10b682127e24eb81acacfbfa9cf40004c1d2806f
Igor Novikov <intorr@gmail.com>
2017-12-17 22:58:51 +0500

    Fix Memory leak in xrCompress.

--- src/utils/xrCompress/xrCompress.cpp
+++ src/utils/xrCompress/xrCompress.cpp
@@ -339,7 +339,6 @@ void xrCompressor::OpenPack(LPCSTR tgt_folder, int num)
     _unlink(fname);
     fs_pack_writer = FS.w_open(fname);
     fs_desc.clear();
-    aliases.clear();
 
     bytesSRC = 0;
     bytesDST = 0;
@@ -419,6 +418,10 @@ void xrCompressor::ClosePack()
         100.f * float(bytesDST) / float(bytesSRC), ((dwTimeEnd - dwTimeStart) / 1000) / 60,
         ((dwTimeEnd - dwTimeStart) / 1000) % 60,
         float((float(bytesDST) / float(1024 * 1024)) / (t_compress.GetElapsed_sec())));
+
+    for (auto &it : aliases)
+        xr_free(it.second.path);
+    aliases.clear();
 }
 
 void xrCompressor::PerformWork()
