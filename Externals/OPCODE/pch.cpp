fca4cdce6dbcdca8c2ff000362515e4bae769b64
Xottab-DUTY <xottab-duty@yandex.ru>
2017-12-22 01:45:34 +0500

    Fix arena allocator for OPCODE

--- Externals/OPCODE/pch.cpp
+++ Externals/OPCODE/pch.cpp
@@ -1,1 +1,7 @@
 #include "pch.hpp"
+
+#ifdef USE_ARENA_ALLOCATOR
+static const u32 s_arena_size = (128 + 16) * 1024 * 1024;
+static char s_fake_array[s_arena_size];
+doug_lea_allocator g_collision_allocator(s_fake_array, s_arena_size, "opcode");
+#endif // #ifdef USE_ARENA_ALLOCATOR
