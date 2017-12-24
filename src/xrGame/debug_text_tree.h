adfa5b3d127c625b332552f72afd75b907271825
Xottab-DUTY <xottab-duty@yandex.ru>
2017-12-24 20:39:30 +0500

    Fix Debug and Mixed compilation

--- src/xrGame/debug_text_tree.h
+++ src/xrGame/debug_text_tree.h
@@ -79,7 +79,7 @@ void log_text_tree(text_tree& tree);
 
 } // namespace debug
 
-IC xr_string __cdecl make_xrstr(LPCSTR format, ...)
+IC xr_string __cdecl make_xrstr(pcstr format, ...)
 {
     va_list args;
     va_start(args, format);
@@ -87,6 +87,7 @@ IC xr_string __cdecl make_xrstr(LPCSTR format, ...)
     char temp[4096];
     vsprintf(temp, format, args);
 
+    va_end(args);
     return xr_string(temp);
 }
 
@@ -94,6 +95,8 @@ IC xr_string __cdecl make_xrstr(bool b) { return b ? "+" : "-"; }
 IC xr_string __cdecl make_xrstr(float f) { return make_xrstr("%f", f); }
 IC xr_string __cdecl make_xrstr(s32 d) { return make_xrstr("%i", d); }
 IC xr_string __cdecl make_xrstr(u32 d) { return make_xrstr("%u", d); }
+IC xr_string __cdecl make_xrstr(s64 d) { return make_xrstr("%i", d); }
+IC xr_string __cdecl make_xrstr(u64 d) { return make_xrstr("%u", d); }
 IC xr_string __cdecl make_xrstr(Fvector3 v) { return make_xrstr("[%f][%f][%f]", v.x, v.y, v.z); }
 IC xr_string __cdecl make_xrstr(const xr_string& s) { return s; }
 #endif // defined(AI_DEBUG_TEXT_TREE_H_INCLUDED)
