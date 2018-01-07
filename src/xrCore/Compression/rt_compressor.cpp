2ac00fbd319e7b7c323fe82457c667c465d0c62b
Xottab-DUTY <xottab-duty@yandex.ru>
2018-01-07 11:45:43 +0500

    xrCore/rt_compressor: fixed stack corruption properly
    Also removed include guard
    Thanks @Im-Dex.

--- src/xrCore/Compression/rt_compressor.cpp
+++ src/xrCore/Compression/rt_compressor.cpp
@@ -14,16 +14,16 @@ u32 rtc_csize(u32 in)
     return in + in / 64 + 16 + 3;
 }
 
-u32 rtc_compress(void* dst, u32 dst_len, const void* src, u32 src_len)
+size_t rtc_compress(void* dst, size_t dst_len, const void* src, size_t src_len)
 {
-    u64 out_size = dst_len;
+    lzo_uint out_size = dst_len;
     int r = lzo1x_1_compress((const lzo_byte*)src, (lzo_uint)src_len, (lzo_byte*)dst, (lzo_uintp)&out_size, rtc_wrkmem);
     VERIFY(r == LZO_E_OK);
     return out_size;
 }
-u32 rtc_decompress(void* dst, u32 dst_len, const void* src, u32 src_len)
+size_t rtc_decompress(void* dst, size_t dst_len, const void* src, size_t src_len)
 {
-    u64 out_size = dst_len;
+    lzo_uint out_size = dst_len;
     int r = lzo1x_decompress((const lzo_byte*)src, (lzo_uint)src_len, (lzo_byte*)dst, (lzo_uintp)&out_size, rtc_wrkmem);
     VERIFY(r == LZO_E_OK);
     return out_size;
--- src/xrCore/Compression/rt_compressor.h
+++ src/xrCore/Compression/rt_compressor.h
@@ -1,10 +1,8 @@
-#ifndef rt_compressorH
-#define rt_compressorH
 #pragma once
 
 extern XRCORE_API void rtc_initialize();
-extern XRCORE_API u32 rtc_compress(void* dst, u32 dst_len, const void* src, u32 src_len);
-extern XRCORE_API u32 rtc_decompress(void* dst, u32 dst_len, const void* src, u32 src_len);
+extern XRCORE_API size_t rtc_compress(void* dst, size_t dst_len, const void* src, size_t src_len);
+extern XRCORE_API size_t rtc_decompress(void* dst, size_t dst_len, const void* src, size_t src_len);
 extern XRCORE_API u32 rtc_csize(u32 in);
 
 extern XRCORE_API void rtc9_initialize();
@@ -12,5 +10,3 @@ extern XRCORE_API void rtc9_uninitialize();
 extern XRCORE_API u32 rtc9_compress(void* dst, u32 dst_len, const void* src, u32 src_len);
 extern XRCORE_API u32 rtc9_decompress(void* dst, u32 dst_len, const void* src, u32 src_len);
 extern XRCORE_API u32 rtc9_csize(u32 in);
-
-#endif
