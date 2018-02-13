10efe46222b6c814065d20da891ff310721163e1
Igor Novikov <intorr@gmail.com>
2018-02-13 18:51:18 +0500

    Fix double ShaderResourceView creation for some cases in R3 and R4.

--- src/Layers/xrRenderDX10/dx10SH_Texture.cpp
+++ src/Layers/xrRenderDX10/dx10SH_Texture.cpp
@@ -547,10 +547,11 @@ void CTexture::Load()
             // pSurface->SetPriority	(PRIORITY_NORMAL);
             flags.MemoryUsage = mem;
         }
+
+        if (pSurface && bCreateView)
+            CHK_DX(HW.pDevice->CreateShaderResourceView(pSurface, NULL, &m_pSRView));
     }
 
-    if (pSurface && bCreateView)
-        CHK_DX(HW.pDevice->CreateShaderResourceView(pSurface, NULL, &m_pSRView));
     PostLoad();
 }
 
