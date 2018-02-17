56933620493403032cf87717b6ed9ab4aade4957
Xottab-DUTY <xottab-duty@yandex.ru>
2018-02-17 12:12:08 +0500

    xrGame/xr_level_controller.cpp: format mistake fix

--- src/xrGame/xr_level_controller.cpp
+++ src/xrGame/xr_level_controller.cpp
@@ -155,7 +155,7 @@ _action* action_name_to_ptr(pcstr _name)
             return &actions[idx];
         ++idx;
     }
-    Msg("! cant find corresponding [id] for action_name", _name);
+    Msg("! cant find corresponding [id] for '%s'", _name);
     return NULL;
 }
 
