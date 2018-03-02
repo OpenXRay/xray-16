46ea987356de5fd381eac1228e49854317b57e1a
Xottab-DUTY <xottab-duty@yandex.ru>
2018-03-03 02:03:41 +0500

    xrEProps/EditorChooseEvents.h: range-based for usage, errors fixes

--- src/editors/xrEProps/ChoseForm.h
+++ src/editors/xrEProps/ChoseForm.h
@@ -19,7 +19,7 @@
 #include "mxPlacemnt.hpp"
 #include "ElXPThemedControl.hpp"
 #include "Gradient.hpp"
-#include "ChooseTypes.H"
+#include "xrCore/ChooseTypes.H"
 
 //---------------------------------------------------------------------------
 // refs
