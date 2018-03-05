fa4fb77bdb45234ed0b95718bdab3ad2d1749188
Xottab-DUTY <xottab-duty@yandex.ru>
2018-03-05 22:58:43 +0500

    Fixed bug with wallmarks on R3-R4

--- res/gamedata/shaders/r3/effects_wallmark.s
+++ res/gamedata/shaders/r3/effects_wallmark.s
@@ -3,6 +3,6 @@ function normal(shader, t_base, t_second, t_detail)
 		: blend		(true,blend.destcolor,blend.srccolor)
 		: zb 		(true,false)
 	shader:dx10texture	("s_base", t_base)
-	shader:dx10sampler	("smp_base")
+	shader:dx10sampler	("smp_rtlinear")
 	shader: dx10color_write_enable( true, true, true, false)
 end
