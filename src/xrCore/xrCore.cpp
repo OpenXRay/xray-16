10a1478e1833bbb2a72be526cc2935f75953ab54
Igor Novikov <intorr@gmail.com>
2018-03-17 18:43:56 +0500

    In this case, it makes no sense. Resetting to default values and later will be reinstalled by calling _initialize_cpu.

--- src/xrCore/xrCore.cpp
+++ src/xrCore/xrCore.cpp
@@ -212,22 +212,12 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD ul_reason_for_call, LPVOID lpvRese
 {
     switch (ul_reason_for_call)
     {
-    case DLL_PROCESS_ATTACH:
-    {
-        _clear87();
-        _control87(_PC_53, MCW_PC);
-        _control87(_RC_CHOP, MCW_RC);
-        _control87(_RC_NEAR, MCW_RC);
-        _control87(_MCW_EM, MCW_EM);
-    }
-    //. LogFile.reserve (256);
-    break;
+    case DLL_PROCESS_ATTACH: break;
     case DLL_THREAD_ATTACH:
         timeBeginPeriod(1);
         break;
-    case DLL_THREAD_DETACH: break;
-    case DLL_PROCESS_DETACH:
-        break;
+    case DLL_THREAD_DETACH:  break;
+    case DLL_PROCESS_DETACH: break;
     }
     return TRUE;
 }
