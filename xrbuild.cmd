ee065f1585b27dc0f4e341e8638eb66254c9786b
DESKTOP-JIR6MDI\en1515 <qweasdd136963@gmail.com>
2018-04-30 01:19:36 +0500

    + Добавил иконку и сплэш из СоС ~ Отредактировал xrbuild

--- xrbuild.cmd
+++ xrbuild.cmd
@@ -4,16 +4,22 @@ if %CONFIGURATION%==Debug if %PLATFORM%==x86 goto :DX86
 if %CONFIGURATION%==Debug if %PLATFORM%==x64 goto :DX64 
 if %CONFIGURATION%==Mixed if %PLATFORM%==x86 goto :MX86 
 if %CONFIGURATION%==Mixed if %PLATFORM%==x64 goto :MX64 
+if %CONFIGURATION%==Mixed_COC if %PLATFORM%==x86 goto :MX_COC86 
+if %CONFIGURATION%==Mixed_COC if %PLATFORM%==x64 goto :MX_COC64 
 if %CONFIGURATION%==Release if %PLATFORM%==x86 goto :RX86 
 if %CONFIGURATION%==Release if %PLATFORM%==x64 goto :RX64 
+if %CONFIGURATION%==Release_COC if %PLATFORM%==x86 goto :RX_COC86 
+if %CONFIGURATION%==Release_COC if %PLATFORM%==x64 goto :RX_COC64 
 
 echo FAIL
 goto :END 
 
 :DX86 
-cd bin\Win32\Debug 
-copy *.dll ..\..\..\res\bins\ 
-copy *.exe ..\..\..\res\bins\ 
+cd _build\bin\Win32\Debug 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy OpenXRay.exe .\res\ 
 cd ..\..\..\ 
 copy License.txt .\res\ 
 copy README.md .\res\ 
@@ -22,9 +28,11 @@ cd res\
 goto :END 
 
 :DX64 
-cd bin\Win64\Debug 
-copy *.dll ..\..\..\res\bins\ 
-copy *.exe ..\..\..\res\bins\ 
+cd _build\bin\Win64\Debug 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy OpenXRay.exe .\res\ 
 cd ..\..\..\ 
 copy License.txt .\res\ 
 copy README.md .\res\ 
@@ -33,9 +41,11 @@ cd res\
 goto :END 
 
 :MX86 
-cd bin\Win32\Mixed 
-copy *.dll ..\..\..\res\bins\ 
-copy *.exe ..\..\..\res\bins\ 
+cd _build\bin\Win32\Mixed 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy OpenXRay.exe .\res\ 
 cd ..\..\..\ 
 copy License.txt .\res\ 
 copy README.md .\res\ 
@@ -44,9 +54,11 @@ cd res\
 goto :END 
 
 :MX64 
-cd bin\Win64\Mixed 
-copy *.dll ..\..\..\res\bins\ 
-copy *.exe ..\..\..\res\bins\ 
+cd _build\bin\Win64\Mixed 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy OpenXRay.exe .\res\ 
 cd ..\..\..\ 
 copy License.txt .\res\ 
 copy README.md .\res\ 
@@ -54,10 +66,38 @@ cd res\
 7z a xdOpenXRay.Mx64.7z .\* 
 goto :END 
 
+:MX_COC86 
+cd _build\bin\Win32\Mixed_COC 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy Stalker-CoC.exe .\res\ 
+cd ..\..\..\ 
+copy License.txt .\res\ 
+copy README.md .\res\ 
+cd res\ 
+7z a xdOpenXRay.Mx_CoC86.7z .\* 
+goto :END 
+
+:MX_COC64 
+cd _build\bin\Win64\Mixed_COC 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy Stalker-CoC.exe .\res\ 
+cd ..\..\..\ 
+copy License.txt .\res\ 
+copy README.md .\res\ 
+cd res\ 
+7z a xdOpenXRay.Mx_CoC64.7z .\* 
+goto :END 
+
 :RX86 
-cd bin\Win32\Release 
-copy *.dll ..\..\..\res\bins\ 
-copy *.exe ..\..\..\res\bins\ 
+cd _build\bin\Win32\Release 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy OpenXRay.exe .\res\ 
 cd ..\..\..\ 
 copy License.txt .\res\ 
 copy README.md .\res\ 
@@ -66,14 +106,42 @@ cd res\
 goto :END 
 
 :RX64 
-cd bin\Win64\Release 
-copy *.dll ..\..\..\res\bins\ 
-copy *.exe ..\..\..\res\bins\ 
+cd _build\bin\Win64\Release 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy OpenXRay.exe .\res\ 
 cd ..\..\..\ 
 copy License.txt .\res\ 
 copy README.md .\res\ 
 cd res\ 
 7z a xdOpenXRay.Rx64.7z .\* 
+goto :END 
+
+:RX_COC86 
+cd _build\bin\Win32\Release_COC 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy Stalker-CoC.exe .\res\ 
+cd ..\..\..\ 
+copy License.txt .\res\ 
+copy README.md .\res\ 
+cd res\ 
+7z a xdOpenXRay.Rx_CoC86.7z .\* 
+goto :END 
+
+:RX_COC64 
+cd _build\bin\Win64\Release_COC 
+copy *.dll ..\..\..\..\res\bins\ 
+copy *.exe ..\..\..\..\res\bins\ 
+cd ..\ 
+copy Stalker-CoC.exe .\res\ 
+cd ..\..\..\ 
+copy License.txt .\res\ 
+copy README.md .\res\ 
+cd res\ 
+7z a xdOpenXRay.Rx_CoC64.7z .\* 
 goto :END 
 
 :END 
