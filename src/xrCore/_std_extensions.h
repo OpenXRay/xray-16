841978770873a14f4d6adc3020a1e3a8a47aabcb
Aleksey Komarov <q4arus@ya.ru>
2018-05-28 22:04:07 +0300

    Fix code style

--- src/xrCore/_std_extensions.h
+++ src/xrCore/_std_extensions.h
@@ -85,7 +85,7 @@ IC T _sqr(T a)
 
 IC bool _valid(const float x) noexcept
 {
-    // check for: Signaling NaN, Quiet NaN, Negative infinity ( –INF), Positive infinity (+INF), Negative denormalized,
+    // check for: Signaling NaN, Quiet NaN, Negative infinity ( ???INF), Positive infinity (+INF), Negative denormalized,
     // Positive denormalized
 #if defined(WINDOWS)
     int cls = _fpclass(double(x));
@@ -94,7 +94,7 @@ IC bool _valid(const float x) noexcept
 #endif
     /* *****other cases are*****
     _FPCLASS_NN Negative normalized non-zero
-    _FPCLASS_NZ Negative zero ( – 0)
+    _FPCLASS_NZ Negative zero ( ??? 0)
     _FPCLASS_PZ Positive 0 (+0)
     _FPCLASS_PN Positive normalized non-zero
     */
@@ -104,7 +104,7 @@ IC bool _valid(const float x) noexcept
 // double
 IC bool _valid(const double x)
 {
-    // check for: Signaling NaN, Quiet NaN, Negative infinity ( –INF), Positive infinity (+INF), Negative denormalized,
+    // check for: Signaling NaN, Quiet NaN, Negative infinity ( ???INF), Positive infinity (+INF), Negative denormalized,
     // Positive denormalized
 #if defined(WINDOWS)
     int cls = _fpclass(x);
@@ -113,7 +113,7 @@ IC bool _valid(const double x)
 #endif
     /* *****other cases are*****
     _FPCLASS_NN Negative normalized non-zero
-    _FPCLASS_NZ Negative zero ( – 0)
+    _FPCLASS_NZ Negative zero ( ??? 0)
     _FPCLASS_PZ Positive 0 (+0)
     _FPCLASS_PN Positive normalized non-zero
     */
