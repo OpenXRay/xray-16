7cd6f8fe2edc0d68ad7215c1aed71e8c90a428e5
Xottab-DUTY <xottab-duty@yandex.ru>
2018-08-11 23:11:19 +0500

    Fixed wrong ingame text translation
    That was my mistake..

--- src/xrCommon/xr_unordered_map.h
+++ src/xrCommon/xr_unordered_map.h
@@ -1,7 +1,9 @@
 #pragma once
-#include <unordered_map>
+//#include <unordered_map>
+#include "unordered_map.hpp"
 #include "xrCore/xrMemory.h"
 
 template <typename K, class V, class Hasher = std::hash<K>, class Traits = std::equal_to<K>,
           typename allocator = tbb::tbb_allocator<std::pair<const K, V>>>
-using xr_unordered_map = std::unordered_map<K, V, Hasher, Traits, allocator>;
+//using xr_unordered_map = std::unordered_map<K, V, Hasher, Traits, allocator>;
+using xr_unordered_map = ska::unordered_map<K, V, Hasher, Traits, allocator>;
