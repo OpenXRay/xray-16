ac02a1f2f771ff405689356f814775dd4f41f5c6
Xottab-DUTY <xottab-duty@yandex.ru>
2018-08-14 07:35:57 +0500

    AppVeyor: output pdb as artifacts

--- xr_pack_build.cmd
+++ xr_pack_build.cmd
@@ -12,41 +12,65 @@ goto :EOF
 
 :DX86
 cd bin\Win32\Debug
-call :PACK
+call :COPY_ENGINE
 7z a OpenXRay.Dx86.7z .\*
+
+cd ..\bin\Win32\Debug
+call :COPY_SYMBOLS
+7z a Symbols.Dx86.7z .\*
 goto :EOF
 
 :DX64
 cd bin\Win64\Debug
-call :PACK
+call :COPY_ENGINE
 7z a OpenXRay.Dx64.7z .\*
+
+cd ..\bin\Win64\Debug
+call :COPY_SYMBOLS
+7z a Symbols.Dx64.7z .\*
 goto :EOF
 
 :MX86
 cd bin\Win32\Mixed
-call :PACK
+call :COPY_ENGINE
 7z a OpenXRay.Mx86.7z .\*
+
+cd ..\bin\Win32\Mixed
+call :COPY_SYMBOLS
+7z a Symbols.Mx86.7z .\*
 goto :EOF
 
 :MX64
 cd bin\Win64\Mixed
-call :PACK
+call :COPY_ENGINE
 7z a OpenXRay.Mx64.7z .\*
+
+cd ..\bin\Win64\Mixed
+call :COPY_SYMBOLS
+7z a Symbols.Mx64.7z .\*
 goto :EOF
 
 :RX86
 cd bin\Win32\Release
-call :PACK
+call :COPY_ENGINE
 7z a OpenXRay.Rx86.7z .\*
+
+cd ..\bin\Win32\Release
+call :COPY_SYMBOLS
+7z a Symbols.Rx86.7z .\*
 goto :EOF
 
 :RX64
 cd bin\Win64\Release
-call :PACK
+call :COPY_ENGINE
 7z a OpenXRay.Rx64.7z .\*
+
+cd ..\bin\Win64\Release
+call :COPY_SYMBOLS
+7z a Symbols.Rx64.7z .\*
 goto :EOF
 
-:PACK
+:COPY_ENGINE
 copy *.dll ..\..\..\res\bin\
 copy *.exe ..\..\..\res\bin\
 cd ..\..\..\
@@ -55,3 +79,10 @@ copy README.md .\res\
 cd res\
 goto :EOF
 
+:COPY_SYMBOLS
+copy *.pdb ..\..\..\res\bin\
+cd ..\..\..\
+copy Licence.txt .\res\
+copy README.md .\res\
+cd res\
+goto :EOF
