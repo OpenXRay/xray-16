d53f0a4e3a0b1ab2011d64b6eec1d51b598dc2e2
Xottab-DUTY <xottab-duty@yandex.ru>
2018-09-23 03:43:49 +0500

    The return of original std::unordered_map

--- src/xrCommon/xr_unordered_map.h
+++ src/xrCommon/xr_unordered_map.h
@@ -1,9 +1,7 @@
 #pragma once
-//#include <unordered_map>
-#include "unordered_map.hpp"
+#include <unordered_map>
 #include "xrCore/xrMemory.h"
 
 template <typename K, class V, class Hasher = std::hash<K>, class Traits = std::equal_to<K>,
           typename allocator = tbb::tbb_allocator<std::pair<const K, V>>>
-//using xr_unordered_map = std::unordered_map<K, V, Hasher, Traits, allocator>;
-using xr_unordered_map = ska::unordered_map<K, V, Hasher, Traits, allocator>;
+using xr_unordered_map = std::unordered_map<K, V, Hasher, Traits, allocator>;
