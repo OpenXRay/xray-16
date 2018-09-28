947db1dc1b5837ab4c8c862e76efb8e76b733149
Ilya Orlov <eagleivg@mail.ru>
2018-09-28 20:04:55 +0300

    xrCore: reformat  _std_extensions

--- src/xrCore/_std_extensions.h
+++ src/xrCore/_std_extensions.h
@@ -146,24 +146,24 @@ IC bool _valid(const double x)
 // XXX: "magic" specializations, that really require profiling to see if they are worth this effort.
 // int8
 IC s8 _abs(s8 x) { return (x >= 0) ? x : s8(-x); }
-IC s8 _min(s8 x, s8 y) { return y + ((x - y) & ((x - y) >> (sizeof(s8) * 8 - 1))); };
-IC s8 _max(s8 x, s8 y) { return x - ((x - y) & ((x - y) >> (sizeof(s8) * 8 - 1))); };
+IC s8 _min(s8 x, s8 y) { return y + ((x - y) & ((x - y) >> (sizeof(s8) * 8 - 1))); }
+IC s8 _max(s8 x, s8 y) { return x - ((x - y) & ((x - y) >> (sizeof(s8) * 8 - 1))); }
 // unsigned int8
 IC u8 _abs(u8 x) { return x; }
 // int16
 IC s16 _abs(s16 x) { return (x >= 0) ? x : s16(-x); }
-IC s16 _min(s16 x, s16 y) { return y + ((x - y) & ((x - y) >> (sizeof(s16) * 8 - 1))); };
-IC s16 _max(s16 x, s16 y) { return x - ((x - y) & ((x - y) >> (sizeof(s16) * 8 - 1))); };
+IC s16 _min(s16 x, s16 y) { return y + ((x - y) & ((x - y) >> (sizeof(s16) * 8 - 1))); }
+IC s16 _max(s16 x, s16 y) { return x - ((x - y) & ((x - y) >> (sizeof(s16) * 8 - 1))); }
 // unsigned int16
 IC u16 _abs(u16 x) { return x; }
 // int32
 IC s32 _abs(s32 x) { return (x >= 0) ? x : s32(-x); }
-IC s32 _min(s32 x, s32 y) { return y + ((x - y) & ((x - y) >> (sizeof(s32) * 8 - 1))); };
-IC s32 _max(s32 x, s32 y) { return x - ((x - y) & ((x - y) >> (sizeof(s32) * 8 - 1))); };
+IC s32 _min(s32 x, s32 y) { return y + ((x - y) & ((x - y) >> (sizeof(s32) * 8 - 1))); }
+IC s32 _max(s32 x, s32 y) { return x - ((x - y) & ((x - y) >> (sizeof(s32) * 8 - 1))); }
 // int64
 IC s64 _abs(s64 x) { return (x >= 0) ? x : s64(-x); }
-IC s64 _min(s64 x, s64 y) { return y + ((x - y) & ((x - y) >> (sizeof(s64) * 8 - 1))); };
-IC s64 _max(s64 x, s64 y) { return x - ((x - y) & ((x - y) >> (sizeof(s64) * 8 - 1))); };
+IC s64 _min(s64 x, s64 y) { return y + ((x - y) & ((x - y) >> (sizeof(s64) * 8 - 1))); }
+IC s64 _max(s64 x, s64 y) { return x - ((x - y) & ((x - y) >> (sizeof(s64) * 8 - 1))); }
 
 // string management
 
