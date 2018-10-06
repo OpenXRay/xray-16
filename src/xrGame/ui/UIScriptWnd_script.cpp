331b08470f2f4e0515c0dbbee699cb8a0c5110b7
Zegeri <zegeridp@gmail.com>
2018-10-06 10:20:57 +0200

    Luabind: Force correct order of loading of CUIScriptWnd dependencies and remove wrapper that doesn't do anything

--- src/xrGame/ui/UIScriptWnd_script.cpp
+++ src/xrGame/ui/UIScriptWnd_script.cpp
@@ -2,26 +2,20 @@
 
 // UI-controls
 #include "UIScriptWnd.h"
-#include "uiscriptwnd_script.h"
 #include "xrScriptEngine/ScriptExporter.hpp"
 #include "xrScriptEngine/Functor.hpp"
 
 using namespace luabind;
 
-extern export_class& script_register_ui_window1(export_class&);
-extern export_class& script_register_ui_window2(export_class&);
-
-SCRIPT_EXPORT(CUIDialogWndEx, (), {
-    export_class instance("CUIScriptWnd");
-
-    module(luaState)[script_register_ui_window2(script_register_ui_window1(instance)).def("Load", &BaseType::Load)];
+SCRIPT_EXPORT(CUIDialogWndEx, (CUIDialogWnd, IFactoryObject), {
+    module(luaState)[class_<CUIDialogWndEx, bases<CUIDialogWnd, IFactoryObject>>("CUIScriptWnd")
+        .def("OnKeyboard", &CUIDialogWndEx::OnKeyboardAction)
+        .def("Update", &CUIDialogWndEx::Update)
+        .def("Dispatch", &CUIDialogWndEx::Dispatch)
+        .def("AddCallback", (void (CUIDialogWndEx::*)(LPCSTR, s16, const functor<void>&, const object&)) &
+            CUIDialogWndEx::AddCallback)
+        .def("Register", (void (CUIDialogWndEx::*)(CUIWindow*, LPCSTR)) &CUIDialogWndEx::Register)
+        .def("Load", &CUIDialogWndEx::Load)
+    ];
 });
 
-export_class& script_register_ui_window1(export_class& instance)
-{
-    instance.def(constructor<>())
-        .def("AddCallback", (void (BaseType::*)(LPCSTR, s16, const luabind::functor<void>&, const luabind::object&)) &
-                BaseType::AddCallback)
-        .def("Register", (void (BaseType::*)(CUIWindow*, LPCSTR)) & BaseType::Register);
-    return (instance);
-}
