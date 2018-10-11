60f49d24acaa6d8268d2e0fb2f70b248442f21b7
Romann <nru.2018@yandex.ru>
2018-10-11 05:32:40 +0300

    *res* Шейдеры. r3-r4 полёт нормальный.

--- res/gamedata_COC/shaders/r3/effects_wallmark.s
+++
@@ -1,8 +0,0 @@
-function normal(shader, t_base, t_second, t_detail)
-  shader:begin("effects_wallmark","stub_default_ma")
-		: blend		(true,blend.destcolor,blend.srccolor)
-		: zb 		(true,false)
-	shader:dx10texture	("s_base", t_base)
-	shader:dx10sampler	("smp_rtlinear")
-	shader: dx10color_write_enable( true, true, true, false)
-end
