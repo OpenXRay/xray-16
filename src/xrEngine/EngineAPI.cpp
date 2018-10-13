91d4f0df00350c455ba277941c155ceb08ee0ada
drug007 <drug2004@bk.ru>
2018-10-14 00:30:42 +0300

    Diagnostic improved.

--- src/xrEngine/EngineAPI.cpp
+++ src/xrEngine/EngineAPI.cpp
@@ -143,7 +143,7 @@ void CEngineAPI::Initialize(void)
     InitializeRenderers();
 
     hGame = XRay::LoadModule("xrGame");
-    R_ASSERT2(hGame, "Game DLL raised exception during loading or there is no game DLL at all");
+    R_ASSERT2(hGame->IsLoaded(), "Game DLL raised exception during loading or there is no game DLL at all");
 
     pCreate = (Factory_Create*)hGame->GetProcAddress("xrFactory_Create");
     R_ASSERT(pCreate);
