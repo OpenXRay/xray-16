e7ad3fb7e390fdb91239308c4b48f056711d0378
Xottab-DUTY <xottab-duty@yandex.ru>
2018-10-17 02:45:50 +0500

    xrGame/UIXmlInit.h: cleanup

--- src/xrGame/ui/UIXmlInit.cpp
+++ src/xrGame/ui/UIXmlInit.cpp
@@ -24,65 +24,8 @@
 #include "UITabButtonMP.h"
 #include "xrUICore/Lines/UILines.h"
 
-CUIXmlInit::CUIXmlInit() : baseClass() {}
+CUIXmlInit::CUIXmlInit() : CUIXmlInitBase() {}
 CUIXmlInit::~CUIXmlInit() {}
-
-Frect CUIXmlInit::GetFRect(CUIXml& xml_doc, LPCSTR path, int index)
-{
-    return baseClass::GetFRect(xml_doc, path, index);
-}
-
-bool CUIXmlInit::InitWindow(CUIXml& xml_doc, LPCSTR path, int index, CUIWindow* pWnd)
-{
-    return baseClass::InitWindow(xml_doc, path, index, pWnd);
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-bool CUIXmlInit::InitFrameWindow(CUIXml& xml_doc, LPCSTR path, int index, CUIFrameWindow* pWnd)
-{
-    return baseClass::InitFrameWindow(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitOptionsItem(CUIXml& xml_doc, LPCSTR path, int index, CUIOptionsItem* pWnd)
-{
-    return baseClass::InitOptionsItem(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitStatic(CUIXml& xml_doc, LPCSTR path, int index, CUIStatic* pWnd)
-{
-    return baseClass::InitStatic(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitTextWnd(CUIXml& xml_doc, LPCSTR path, int index, CUITextWnd* pWnd)
-{
-    return baseClass::InitTextWnd(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitCheck(CUIXml& xml_doc, LPCSTR path, int index, CUICheckButton* pWnd)
-{
-    return baseClass::InitCheck(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitSpin(CUIXml& xml_doc, LPCSTR path, int index, CUICustomSpin* pWnd)
-{
-    return baseClass::InitSpin(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitText(CUIXml& xml_doc, LPCSTR path, int index, CUIStatic* pWnd)
-{
-    return baseClass::InitText(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitText(CUIXml& xml_doc, LPCSTR path, int index, CUILines* pLines)
-{
-    return baseClass::InitText(xml_doc, path, index, pLines);
-}
-
-bool CUIXmlInit::Init3tButton(CUIXml& xml_doc, LPCSTR path, int index, CUI3tButton* pWnd)
-{
-    return baseClass::Init3tButton(xml_doc, path, index, pWnd);
-}
 
 bool CUIXmlInit::InitTabButtonMP(CUIXml& xml_doc, LPCSTR path, int index, CUITabButtonMP* pWnd)
 {
@@ -109,11 +52,6 @@ bool CUIXmlInit::InitTabButtonMP(CUIXml& xml_doc, LPCSTR path, int index, CUITab
     }
 
     return true;
-}
-
-bool CUIXmlInit::InitSound(CUIXml& xml_doc, LPCSTR path, int index, CUI3tButton* pWnd)
-{
-    return baseClass::InitSound(xml_doc, path, index, pWnd);
 }
 
 bool CUIXmlInit::InitDragDropListEx(CUIXml& xml_doc, LPCSTR path, int index, CUIDragDropListEx* pWnd)
@@ -176,60 +114,6 @@ bool CUIXmlInit::InitDragDropListEx(CUIXml& xml_doc, LPCSTR path, int index, CUI
     return true;
 }
 
-bool CUIXmlInit::InitProgressBar(CUIXml& xml_doc, LPCSTR path, int index, CUIProgressBar* pWnd)
-{
-    return baseClass::InitProgressBar(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitProgressShape(CUIXml& xml_doc, LPCSTR path, int index, CUIProgressShape* pWnd)
-{
-    return baseClass::InitProgressShape(xml_doc, path, index, pWnd);
-}
-
-void CUIXmlInit::InitAutoStaticGroup(CUIXml& xml_doc, LPCSTR path, int index, CUIWindow* pParentWnd)
-{
-    baseClass::InitAutoStaticGroup(xml_doc, path, index, pParentWnd);
-}
-
-void CUIXmlInit::InitAutoFrameLineGroup(CUIXml& xml_doc, LPCSTR path, int index, CUIWindow* pParentWnd)
-{
-    baseClass::InitAutoFrameLineGroup(xml_doc, path, index, pParentWnd);
-}
-
-bool CUIXmlInit::InitFont(CUIXml& xml_doc, LPCSTR path, int index, u32& color, CGameFont*& pFnt)
-{
-    return baseClass::InitFont(xml_doc, path, index, color, pFnt);
-}
-
-bool CUIXmlInit::InitTabControl(CUIXml& xml_doc, LPCSTR path, int index, CUITabControl* pWnd)
-{
-    return baseClass::InitTabControl(xml_doc, path, index, pWnd);
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-bool CUIXmlInit::InitFrameLine(CUIXml& xml_doc, LPCSTR path, int index, CUIFrameLineWnd* pWnd)
-{
-    return baseClass::InitFrameLine(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitCustomEdit(CUIXml& xml_doc, LPCSTR path, int index, CUICustomEdit* pWnd)
-{
-    return baseClass::InitCustomEdit(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitEditBox(CUIXml& xml_doc, LPCSTR path, int index, CUIEditBox* pWnd)
-{
-    return baseClass::InitEditBox(xml_doc, path, index, pWnd);
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-bool CUIXmlInit::InitAnimatedStatic(CUIXml& xml_doc, LPCSTR path, int index, CUIAnimatedStatic* pWnd)
-{
-    return baseClass::InitAnimatedStatic(xml_doc, path, index, pWnd);
-}
-
 bool CUIXmlInit::InitSleepStatic(CUIXml& xml_doc, LPCSTR path, int index, CUISleepStatic* pWnd)
 {
     R_ASSERT4(xml_doc.NavigateToNode(path, index), "XML node not found", path, xml_doc.m_xml_file_name);
@@ -237,58 +121,6 @@ bool CUIXmlInit::InitSleepStatic(CUIXml& xml_doc, LPCSTR path, int index, CUISle
     InitStatic(xml_doc, path, index, pWnd);
 
     return true;
-}
-
-bool CUIXmlInit::InitTexture(CUIXml& xml_doc, LPCSTR path, int index, ITextureOwner* pWnd)
-{
-    return baseClass::InitTexture(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitTextureOffset(CUIXml& xml_doc, LPCSTR path, int index, CUIStatic* pWnd)
-{
-    return baseClass::InitTextureOffset(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitMultiTexture(CUIXml& xml_doc, LPCSTR path, int index, CUI3tButton* pWnd)
-{
-    return baseClass::InitMultiTexture(xml_doc, path, index, pWnd);
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-bool CUIXmlInit::InitAlignment(CUIXml& xml_doc, const char* path, int index, float& x, float& y, CUIWindow* pWnd)
-{
-    return baseClass::InitAlignment(xml_doc, path, index, x, y, pWnd);
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-void CUIXmlInit::InitColorDefs() { baseClass::InitColorDefs(); }
-
-bool CUIXmlInit::InitScrollView(CUIXml& xml_doc, LPCSTR path, int index, CUIScrollView* pWnd)
-{
-    return baseClass::InitScrollView(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitListBox(CUIXml& xml_doc, LPCSTR path, int index, CUIListBox* pWnd)
-{
-    return baseClass::InitListBox(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitTrackBar(CUIXml& xml_doc, LPCSTR path, int index, CUITrackBar* pWnd)
-{
-    return baseClass::InitTrackBar(xml_doc, path, index, pWnd);
-}
-
-bool CUIXmlInit::InitComboBox(CUIXml& xml_doc, LPCSTR path, int index, CUIComboBox* pWnd)
-{
-    return baseClass::InitComboBox(xml_doc, path, index, pWnd);
-}
-
-void CUIXmlInit::AssignColor(LPCSTR name, u32 clr) { baseClass::AssignColor(name, clr); }
-u32 CUIXmlInit::GetColor(CUIXml& xml_doc, LPCSTR path, int index, u32 def_clr)
-{
-    return baseClass::GetColor(xml_doc, path, index, def_clr);
 }
 
 bool CUIXmlInit::InitHintWindow(CUIXml& xml_doc, LPCSTR path, int index, UIHintWindow* pWnd)
--- src/xrGame/ui/UIXmlInit.h
+++ src/xrGame/ui/UIXmlInit.h
@@ -7,58 +7,12 @@ class CUITabButtonMP;
 
 class CUIXmlInit : public CUIXmlInitBase
 {
-    using baseClass = CUIXmlInitBase;
 public:
     CUIXmlInit();
     virtual ~CUIXmlInit();
 
-    static bool InitWindow(CUIXml& xml_doc, LPCSTR path, int index, CUIWindow* pWnd);
-    static bool InitFrameWindow(CUIXml& xml_doc, LPCSTR path, int index, CUIFrameWindow* pWnd);
-    static bool InitFrameLine(CUIXml& xml_doc, LPCSTR path, int index, CUIFrameLineWnd* pWnd);
-    static bool InitCustomEdit(CUIXml& xml_doc, LPCSTR paht, int index, CUICustomEdit* pWnd);
-    static bool InitEditBox(CUIXml& xml_doc, LPCSTR paht, int index, CUIEditBox* pWnd);
-    static bool InitStatic(CUIXml& xml_doc, LPCSTR path, int index, CUIStatic* pWnd);
-    static bool InitTextWnd(CUIXml& xml_doc, LPCSTR path, int index, CUITextWnd* pWnd);
-    static bool InitCheck(CUIXml& xml_doc, LPCSTR path, int index, CUICheckButton* pWnd);
-    static bool InitSpin(CUIXml& xml_doc, LPCSTR path, int index, CUICustomSpin* pWnd);
-    static bool InitText(CUIXml& xml_doc, LPCSTR path, int index, CUIStatic* pWnd);
-    static bool InitText(CUIXml& xml_doc, LPCSTR path, int index, CUILines* pLines);
-    static bool Init3tButton(CUIXml& xml_doc, LPCSTR path, int index, CUI3tButton* pWnd);
     static bool InitDragDropListEx(CUIXml& xml_doc, LPCSTR path, int index, CUIDragDropListEx* pWnd);
-    static bool InitProgressBar(CUIXml& xml_doc, LPCSTR path, int index, CUIProgressBar* pWnd);
-    static bool InitProgressShape(CUIXml& xml_doc, LPCSTR path, int index, CUIProgressShape* pWnd);
-    static bool InitFont(CUIXml& xml_doc, LPCSTR path, int index, u32& color, CGameFont*& pFnt);
     static bool InitTabButtonMP(CUIXml& xml_doc, LPCSTR path, int index, CUITabButtonMP* pWnd);
-    static bool InitTabControl(CUIXml& xml_doc, LPCSTR path, int index, CUITabControl* pWnd);
-    static bool InitAnimatedStatic(CUIXml& xml_doc, LPCSTR path, int index, CUIAnimatedStatic* pWnd);
     static bool InitSleepStatic(CUIXml& xml_doc, LPCSTR path, int index, CUISleepStatic* pWnd);
-    static bool InitTextureOffset(CUIXml& xml_doc, LPCSTR path, int index, CUIStatic* pWnd);
-    static bool InitSound(CUIXml& xml_doc, LPCSTR path, int index, CUI3tButton* pWnd);
-    static bool InitMultiTexture(CUIXml& xml_doc, LPCSTR path, int index, CUI3tButton* pWnd);
-    static bool InitTexture(CUIXml& xml_doc, LPCSTR path, int index, ITextureOwner* pWnd);
-    static bool InitOptionsItem(CUIXml& xml_doc, LPCSTR paht, int index, CUIOptionsItem* pWnd);
-    static bool InitScrollView(CUIXml& xml_doc, LPCSTR path, int index, CUIScrollView* pWnd);
-    static bool InitListBox(CUIXml& xml_doc, LPCSTR path, int index, CUIListBox* pWnd);
-    static bool InitComboBox(CUIXml& xml_doc, LPCSTR path, int index, CUIComboBox* pWnd);
-    static bool InitTrackBar(CUIXml& xml_doc, LPCSTR path, int index, CUITrackBar* pWnd);
     static bool InitHintWindow(CUIXml& xml_doc, LPCSTR path, int index, UIHintWindow* pWnd);
-    static Frect GetFRect(CUIXml& xml_doc, LPCSTR path, int index);
-    static u32 GetColor(CUIXml& xml_doc, LPCSTR path, int index, u32 def_clr);
-
-    static bool InitAlignment(CUIXml& xml_doc, const char* path, int index, float& x, float& y, CUIWindow* pWnd);
-
-    static void InitAutoStaticGroup(CUIXml& xml_doc, LPCSTR path, int index, CUIWindow* pParentWnd);
-    static void InitAutoFrameLineGroup(CUIXml& xml_doc, LPCSTR path, int index, CUIWindow* pParentWnd);
-
-    // Initialize and store predefined colors
-    using ColorDefs = baseClass::ColorDefs;
-
-    static const ColorDefs* GetColorDefs()
-    {
-        baseClass::GetColorDefs();
-    }
-
-    static void InitColorDefs();
-    static void DeleteColorDefs() { baseClass::DeleteColorDefs(); }
-    static void AssignColor(LPCSTR name, u32 clr);
 };
