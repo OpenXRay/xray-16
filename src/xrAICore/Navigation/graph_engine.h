03a32d296ea604eeb5fbef24b7e35fb9bab0d009
Zegeri <zegeridp@gmail.com>
2018-10-22 18:51:45 +0200

    xrAICore: Fix hash_fixed_vertex_manager::to_u32 in Linux
    Hopefully this time it doesn't break Windows.

--- src/xrAICore/Navigation/graph_engine.h
+++ src/xrAICore/Navigation/graph_engine.h
@@ -13,12 +13,12 @@
 #include "xrAICore/Navigation/vertex_manager_fixed.h"
 #include "xrAICore/Navigation/vertex_allocator_fixed.h"
 #include "xrAICore/Navigation/data_storage_bucket_list.h"
+#include "xrAICore/Navigation/PathManagers/path_manager.h"
+#include "xrAICore/Navigation/graph_engine_space.h"
 #ifndef AI_COMPILER
 #include "xrAICore/Navigation/vertex_manager_hash_fixed.h"
 #include "xrAICore/Navigation/data_storage_binary_heap.h"
 #endif
-#include "xrAICore/Navigation/PathManagers/path_manager.h"
-#include "xrAICore/Navigation/graph_engine_space.h"
 #include "xrEngine/profiler.h"
 #include "xrAICore/Components/problem_solver.h"
 #include "xrAICore/Components/operator_condition.h"
