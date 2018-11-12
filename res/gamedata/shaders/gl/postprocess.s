aaacdfa9eca1f86984339c52d5b554e11587540e
Xottab-DUTY <xottab-duty@yandex.ru>
2018-11-12 23:00:45 +0500

    xrRenderGL: fixed crash when applying postprocess
    For example, It happens when anomaly activates

--- res/gamedata/shaders/gl/postprocess.s
+++ res/gamedata/shaders/gl/postprocess.s
@@ -19,6 +19,6 @@ function l_special        (shader, t_base, t_second, t_detail)
 	shader:sampler	("s_base1")    	:texture("$user$albedo")	: clamp() : f_linear ()
 	shader:sampler	("s_noise")    	:texture("fx\\fx_noise2")	: f_linear ()
 
-	shader:sampler	("s_grad1")    	:texture("grad\\grad_red_yellow")	: clamp() : f_linear ()
-	shader:sampler	("s_grad1")    	:texture("grad\\grad_test1")	: clamp() : f_linear ()
+	shader:sampler	("s_grad0")    	:texture("$user$cmap0")	: clamp() : f_linear ()
+	shader:sampler	("s_grad1")    	:texture("$user$cmap1")	: clamp() : f_linear ()
 end
