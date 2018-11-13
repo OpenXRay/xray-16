e7e2ab8a3ca17cd203bdf8f35bbbbc89d51a565c
Xottab-DUTY <xottab-duty@yandex.ru>
2018-11-14 01:26:11 +0500

    xrGame/Level_Buller_Manager.cpp: verify thread id only if mtBullets is enabled

--- src/xrGame/Level_Bullet_Manager.cpp
+++ src/xrGame/Level_Bullet_Manager.cpp
@@ -184,7 +184,8 @@ void CBulletManager::AddBullet(const Fvector& position, const Fvector& direction
     const CCartridge& cartridge, float const air_resistance_factor, bool SendHit, bool AimBullet)
 {
 #ifdef DEBUG
-    VERIFY(m_thread_id == GetCurrentThreadId());
+    if (g_mt_config.test(mtBullets))
+        VERIFY(m_thread_id == GetCurrentThreadId());
 #endif
 
     VERIFY(u16(-1) != cartridge.bullet_material_idx);
@@ -209,7 +210,8 @@ void CBulletManager::AddBullet(const Fvector& position, const Fvector& direction
 void CBulletManager::UpdateWorkload()
 {
 #ifdef DEBUG
-    VERIFY						( m_thread_id == GetCurrentThreadId() );
+    if (g_mt_config.test(mtBullets))
+        VERIFY(m_thread_id == GetCurrentThreadId());
 #endif
 
     rq_storage.r_clear();
