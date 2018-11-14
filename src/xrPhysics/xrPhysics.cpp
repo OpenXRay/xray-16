e81b8eb80a543d94b7011b19c89716dea7003a33
Ilya Orlov <eagleivg@mail.ru>
2018-11-15 00:57:55 +0300

    xrPhysics: add init at DLL loading for Linux

--- src/xrPhysics/xrPhysics.cpp
+++ src/xrPhysics/xrPhysics.cpp
@@ -13,6 +13,16 @@ static void* ode_alloc(size_t size) { return xr_malloc(size); }
 static void* ode_realloc(void* ptr, size_t oldsize, size_t newsize) { return xr_realloc(ptr, newsize); }
 static void ode_free(void* ptr, size_t size) { return xr_free(ptr); }
 
+#ifdef LINUX
+__attribute__((constructor))
+#endif
+static void load(int argc, char** argv, char** envp)
+{
+    dSetAllocHandler(ode_alloc);
+    dSetReallocHandler(ode_realloc);
+    dSetFreeHandler(ode_free);
+}
+
 #if defined(WINDOWS)
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
@@ -21,9 +31,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     {
     case DLL_PROCESS_ATTACH:
 
-        dSetAllocHandler(ode_alloc);
-        dSetReallocHandler(ode_realloc);
-        dSetFreeHandler(ode_free);
+        load(0, nullptr, nullptr);
 
         break;
     case DLL_PROCESS_DETACH: break;
