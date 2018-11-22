9e5ac97dda5726cbff4bde3dc7cd656cfe6b698d
SkyLoaderr <skyloader1@mail.ru>
2018-11-22 15:30:25 +0300

    OpenGL: Casting detail shadows from dynamic lights Corrected waterfall normal map for 'rain_path_normal_new' shader

--- src/Layers/xrRenderPC_GL/r2_R_lights.cpp
+++ src/Layers/xrRenderPC_GL/r2_R_lights.cpp
@@ -114,6 +114,8 @@ void CRender::render_lights(light_Package& LP)
                 RCache.set_xform_view(L->X.S.view);
                 RCache.set_xform_project(L->X.S.project);
                 r_dsgraph_render_graph(0);
+                if (ps_r2_ls_flags.test(R2FLAG_DETAIL_SHADOW))
+                    Details->Render();
                 L->X.S.transluent = FALSE;
                 if (bSpecial)
                 {
