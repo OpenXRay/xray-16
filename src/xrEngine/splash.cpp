4899530576be963cd120cfb8e21a09f5e0ef25ff
Ilya Orlov <eagleivg@mail.ru>
2018-12-18 22:42:54 +0300

    xrEngine: hide some SDL2 flags under version define (fix build on Ubuntu16.04 with SDL2 v2.0.0)

--- src/xrEngine/splash.cpp
+++ src/xrEngine/splash.cpp
@@ -61,9 +61,13 @@ void show(const bool topmost)
         return;
     }
 
-    Uint32 flags = SDL_WINDOW_BORDERLESS | SDL_WINDOW_HIDDEN | SDL_WINDOW_SKIP_TASKBAR;
+    Uint32 flags = SDL_WINDOW_BORDERLESS | SDL_WINDOW_HIDDEN;
+    
+#if SDL_VERSION_ATLEAST(2,0,5)
+    flags |= SDL_WINDOW_SKIP_TASKBAR;
     if (topmost)
         flags |= SDL_WINDOW_ALWAYS_ON_TOP;
+#endif
 
     logoWindow = SDL_CreateWindow("OpenXRay", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, surface->w, surface->h, flags);
     const auto logoSurface = SDL_GetWindowSurface(logoWindow);
