87d9b051d342f8ac7a251fc6873cf3454f5a5da2
mrnotbadguy <dmitrysafronov1996@gmail.com>
2019-02-04 15:22:54 +0300

    Removed legacy plugins from repository

--- src/editors/ECore/Editor/ELog.cpp
+++ src/editors/ECore/Editor/ELog.cpp
@@ -25,56 +25,6 @@ void __stdcall ELogCallback(LPCSTR txt)
         TfrmLog::AddMessage(mt, txt);
 }
 #endif
-#ifdef _LW_EXPORT
-#include <lwhost.h>
-extern "C" LWMessageFuncs* g_msg;
-void ELogCallback(void* context, LPCSTR txt)
-{
-    if (0 == txt[0])
-        return;
-    bool bDlg = ('#' == txt[0]) || ((0 != txt[1]) && ('#' == txt[1]));
-    if (bDlg)
-    {
-        int mt = ('!' == txt[0]) || ((0 != txt[1]) && ('!' == txt[1])) ? 1 : 0;
-        if (('!' == txt[0]) || ('#' == txt[0]))
-            txt++;
-        if (('!' == txt[0]) || ('#' == txt[0]))
-            txt++;
-        if (mt == 1)
-            g_msg->error(txt, 0);
-        else
-            g_msg->info(txt, 0);
-    }
-}
-#endif
-#ifdef _MAX_EXPORT
-#include "NetDeviceLog.h"
-void ELogCallback(void* context, LPCSTR txt)
-{
-    if (0 != txt[0])
-    {
-        if (txt[0] == '!')
-            EConsole.print(mtError, txt + 1);
-        else
-            EConsole.print(mtInformation, txt);
-    }
-}
-#endif
-#ifdef _MAYA_PLUGIN
-void ELogCallback(void* context, LPCSTR txt)
-{
-    if (0 != txt[0])
-    {
-        if (txt[0] == '!')
-            std::cerr << "XR-Error: " << txt + 1 << "\n";
-        else
-            std::cerr << "XR-Info: " << txt << "\n";
-    }
-    //.		MStringArray res;
-    //		MGlobal::executeCommand("confirmDialog -title \"Error\" -message \"Mesh have non-triangulated polygon.\"
-    //-button \"Ok\" -defaultButton \"Ok\"",res);
-}
-#endif
 
 //----------------------------------------------------
 CLog ELog;
@@ -103,22 +53,6 @@ int CLog::DlgMsg(TMsgDlgType mt, TMsgDlgButtons btn, LPCSTR _Format, ...)
         case mrCancel: strcat(buf, " - Cancel."); break;
         default: strcat(buf, " - Something.");
         }
-    }
-#endif
-#ifdef _LW_EXPORT
-    switch (mt)
-    {
-    case mtError: g_msg->error(buf, 0); break;
-    case mtInformation: g_msg->info(buf, 0); break;
-    default: g_msg->info(buf, 0); break;
-    }
-#endif
-#ifdef _MAX_PLUGIN
-    switch (mt)
-    {
-    case mtError: MessageBox(0, buf, "Error", MB_OK | MB_ICONERROR); break;
-    case mtInformation: MessageBox(0, buf, "Information", MB_OK | MB_ICONINFORMATION); break;
-    default: MessageBox(0, buf, "Information", MB_OK | MB_ICONINFORMATION); break;
     }
 #endif
 
@@ -157,22 +91,6 @@ int CLog::DlgMsg(TMsgDlgType mt, LPCSTR _Format, ...)
         }
     }
 #endif
-#ifdef _LW_EXPORT
-    switch (mt)
-    {
-    case mtError: g_msg->error(buf, 0); break;
-    case mtInformation: g_msg->info(buf, 0); break;
-    default: g_msg->info(buf, 0); break;
-    }
-#endif
-#ifdef _MAX_PLUGIN
-    switch (mt)
-    {
-    case mtError: MessageBox(0, buf, "Error", MB_OK | MB_ICONERROR); break;
-    case mtInformation: MessageBox(0, buf, "Information", MB_OK | MB_ICONINFORMATION); break;
-    default: MessageBox(0, buf, "Information", MB_OK | MB_ICONINFORMATION); break;
-    }
-#endif
 
     Msg(mt, buf);
 
@@ -190,15 +108,6 @@ void CLog::Msg(TMsgDlgType mt, LPCSTR _Format, ...)
 
 #ifdef _EDITOR
     TfrmLog::AddMessage(mt, AnsiString(buf));
-#endif
-#ifdef _MAX_EXPORT
-    EConsole.print(mt, buf);
-#endif
-#ifdef _LW_EXPORT
-    switch (mt)
-    {
-    case mtError: g_msg->error(buf, 0); break;
-    }
 #endif
 
     ::LogExecCB = FALSE;
--- src/plugins/lw/Server/shutdown.c
+++
@@ -1,9 +0,0 @@
-/*
- * LWSDK Library Source File
- * Copyright 1995,1997  NewTek, Inc.
- *
- * Default 'Shutdown' implementaion does nothing.
- */
-#include <lwsdk/lwserver.h>
-
-void Shutdown(void* serverData) {}
