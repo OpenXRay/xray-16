a3fd4f9a4f01202b1479b8017609492b73d0f4b3
mrnotbadguy <dmitrysafronov1996@gmail.com>
2019-02-24 00:24:10 +0300

    Solved: remove system.ltx for vanilla mods compatibility

--- src/xrGame/Inventory.cpp
+++ src/xrGame/Inventory.cpp
@@ -44,8 +44,14 @@ CInventory::CInventory()
 {
     m_fMaxWeight = pSettings->r_float("inventory", "max_weight");
 
-    u32 sz = LAST_SLOT + 1; //pSettings->r_s32("inventory", "slots_count"); //Alundaio: Get slot count directly to automate this process
+    u16 sz;
+    const u16 tempSlotsCount = pSettings->r_s16("inventory", "slots_count");
+    if (tempSlotsCount > 0 && tempSlotsCount <= LAST_SLOT)
+        sz = tempSlotsCount + 1;
+    else
+        sz = 1;
     m_slots.resize(sz + 1); // first is [1]
+    m_iLastSlot = sz - 1;
 
     m_iActiveSlot = NO_ACTIVE_SLOT;
     m_iNextActiveSlot = NO_ACTIVE_SLOT;
@@ -71,7 +77,7 @@ CInventory::CInventory()
     InitPriorityGroupsForQSwitch();
     m_next_item_iteration_time = 0;
 
-    for (u16 i = 0; i < LAST_SLOT + 1; ++i)
+    for (u16 i = 0; i < sz; ++i)
     {
         m_blocked_slots[i] = 0;
     }
@@ -1410,7 +1416,7 @@ void CInventory::TryDeactivateActiveSlot()
 
 void CInventory::BlockSlot(u16 slot_id)
 {
-    VERIFY(slot_id <= LAST_SLOT);
+    VERIFY(slot_id <= LastSlot());
 
     ++m_blocked_slots[slot_id];
 
@@ -1419,7 +1425,7 @@ void CInventory::BlockSlot(u16 slot_id)
 
 void CInventory::UnblockSlot(u16 slot_id)
 {
-    VERIFY(slot_id <= LAST_SLOT);
+    VERIFY(slot_id <= LastSlot());
     VERIFY2(m_blocked_slots[slot_id] > 0, make_string("blocked slot [%d] underflow").c_str());
 
     --m_blocked_slots[slot_id];
@@ -1427,7 +1433,7 @@ void CInventory::UnblockSlot(u16 slot_id)
 
 bool CInventory::IsSlotBlocked(u16 slot_id) const
 {
-    VERIFY(slot_id <= LAST_SLOT);
+    VERIFY(slot_id <= LastSlot());
     return m_blocked_slots[slot_id] > 0;
 }
 
