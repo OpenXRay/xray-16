6d5aee2bf0f26fe8463e93afe0dbf6be37bd885e
SkyLoaderr <skyloader1@mail.ru>
2019-03-04 14:27:15 +0300

    OpenGL shaders: fixed 'ssao_blur' compilation, some fixes in 'ssao_calc' shader

--- res/gamedata/shaders/gl/iostructs/p_ssao_calc.h
+++ res/gamedata/shaders/gl/iostructs/p_ssao_calc.h
@@ -1,6 +1,4 @@
-
-out vec4 SV_Target0;
-out vec4 SV_Target1;
+out vec4 SV_Target;
 in vec4 gl_FragCoord;
 
 #ifdef MSAA_OPTIMIZATION
@@ -18,12 +16,6 @@ struct	_input
 	float4	pos2d	; // SV_Position;
 };
 
-struct	_out
-{
-	float4	low	; // SV_Target0;
-	float4	high	; // SV_Target1;
-};
-
 #ifdef USE_VTF
 layout(location = TEXCOORD0)	in float4	p_combine_tc0	; // TEXCOORD0;	// tc.xy, tc.w = tonemap scale
 #else // USE_VTF
@@ -32,24 +24,21 @@ layout(location = TEXCOORD0)	in float2	p_combine_tc0	; // TEXCOORD0;	// tc.xy
 layout(location = TEXCOORD1)	in float2	p_combine_tcJ	; // TEXCOORD1;	// jitter coords
 
 #ifndef MSAA_OPTIMIZATION
-_out _main ( _input I );
+float4 _main ( _input I );
 #else
-_out _main ( _input I, uint iSample );
+float4 _main ( _input I, uint iSample );
 #endif
 
 void main()
 {
 	_input		I;
-	I.tc0 		= p_combine_tc0;
-	I.tcJ 		= p_combine_tcJ;
+	I.tc0			= p_combine_tc0;
+	I.tcJ			= p_combine_tcJ;
 	I.pos2d		= gl_FragCoord;
 
 #ifndef MSAA_OPTIMIZATION
-	_out O		= _main ( I );
+	SV_Target	= _main ( I );
 #else
-	_out O		= _main ( I, gl_SampleID );
+	SV_Target	= _main ( I, gl_SampleID );
 #endif
-
-	SV_Target0	= O.low;
-	SV_Target1	= O.high;
 }
