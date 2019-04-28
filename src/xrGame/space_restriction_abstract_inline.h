9d9d23ee8e04cc1f11e0a00399635b2b80dd1d39
Xottab-DUTY <xottab-duty@yandex.ru>
2019-04-28 22:05:01 +0500

    xrGame/space_restriction_abstract_inline.h: replaced THROW with VERIFY
    Prevents crash when debugger is not connected

--- src/xrGame/space_restriction_abstract_inline.h
+++ src/xrGame/space_restriction_abstract_inline.h
@@ -20,7 +20,7 @@ IC const xr_vector<u32>& CSpaceRestrictionAbstract::border()
         initialize();
 
     THROW(initialized());
-    THROW3(!m_border.empty(), "Space restrictor has no border!", *name());
+    VERIFY3(!m_border.empty(), "Space restrictor has no border!", *name());
     return (m_border);
 }
 
