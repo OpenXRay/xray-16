0ba62eb78ce249f02ee14f6cd8c7053e7aaa6beb
Xottab-DUTY <xottab-duty@yandex.ru>
2019-05-16 21:04:41 +0500

    Fixed solution compiltion after cbaf46fc9420b09359831995f950afc82f231d30

--- src/utils/xrLC/xrLC.cpp
+++ src/utils/xrLC/xrLC.cpp
@@ -133,7 +133,7 @@ void Startup(LPSTR lpCmdLine)
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     // Initialize debugging
-    xrDebug::Initialize();
+    xrDebug::Initialize(lpCmdLine);
     Core.Initialize("xrLC");
 
     if (strstr(Core.Params, "-nosmg"))
