OpenXRay/xray-16

commit 2c0d05225b1c57a585d7f2b3d64558b28c9733f4

Cleaned up Config.hpp and gamedata
Callbacks from COC removed:
    _G.CALifeUpdateManager__on_before_change_level
    alife_storage_manager.CALifeStorageManager_save
    alife_storage_manager.CALifeStorageManager_load

src/xrGame/InventoryOwner.cpp
@@ -449,23 +449,21 @@ void CInventoryOwner::OnItemDropUpdate() {}
 void CInventoryOwner::OnItemBelt(CInventoryItem* inventory_item, const SInvItemPlace& previous_place)
 {
     /* avo: script callback */
-#ifdef EXTENDED_ITEM_CALLBACKS
-    CGameObject	*object = smart_cast<CGameObject*>(this);
+    CGameObject* object = smart_cast<CGameObject*>(this);
     VERIFY(object);
-    object->callback(GameObject::eItemToBelt)(inventory_item->object().lua_game_object());
-#endif
-    /* avo: end */
+    auto& cb = object->callback(GameObject::eItemToBelt);
+    if (cb)
+        cb(inventory_item->object().lua_game_object());
 }
 
 void CInventoryOwner::OnItemRuck(CInventoryItem* inventory_item, const SInvItemPlace& previous_place)
 {
     /* avo: script callback */
-#ifdef EXTENDED_ITEM_CALLBACKS
-    CGameObject	*object = smart_cast<CGameObject*>(this);
+    CGameObject* object = smart_cast<CGameObject*>(this);
     VERIFY(object);
-    object->callback(GameObject::eItemToRuck)(inventory_item->object().lua_game_object());
-#endif
-    /* avo: end */
+    auto& cb = object->callback(GameObject::eItemToRuck);
+    if (cb)
+        cb(inventory_item->object().lua_game_object());
 
     detach(inventory_item);
 }
@@ -473,12 +471,11 @@ void CInventoryOwner::OnItemRuck(CInventoryItem* inventory_item, const SInvItemP
 void CInventoryOwner::OnItemSlot(CInventoryItem* inventory_item, const SInvItemPlace& previous_place)
 {
     /* avo: script callback */
-#ifdef EXTENDED_ITEM_CALLBACKS
     CGameObject	*object = smart_cast<CGameObject*>(this);
     VERIFY(object);
-    object->callback(GameObject::eItemToSlot)(inventory_item->object().lua_game_object());
-#endif
-    /* avo: end */
+    auto& cb = object->callback(GameObject::eItemToSlot);
+    if (cb)
+        cb(inventory_item->object().lua_game_object());
 
     attach(inventory_item);
 }

src/xrGame/Level_input.cpp
@@ -48,12 +48,14 @@ void CLevel::IR_OnMouseWheel(int x, int y)
     if (g_bDisableAllInput)
         return;
 
-#ifdef INPUT_CALLBACKS
     /* avo: script callback */
     if (g_actor)
-        g_actor->callback(GameObject::eMouseWheel)(x);
-    /* avo: end */
-#endif
+    {
+        auto& cb = g_actor->callback(GameObject::eMouseWheel);
+        if (cb)
+            cb(x);
+    }
+
     if (CurrentGameUI()->IR_UIOnMouseWheel(x, y))
         return;
     if (Device.Paused()
@@ -79,12 +81,13 @@ void CLevel::IR_OnMouseMove(int dx, int dy)
     if (g_bDisableAllInput)
         return;
 
-#ifdef INPUT_CALLBACKS
     /* avo: script callback */
     if (g_actor)
-        g_actor->callback(GameObject::eMouseMove)(dx, dy);
-    /* avo: end */
-#endif
+    {
+        auto& cb = g_actor->callback(GameObject::eMouseMove);
+        if (cb)
+            cb(dx, dy);
+    }
 
     if (CurrentGameUI()->IR_UIOnMouseMove(dx, dy))
         return;
@@ -124,12 +127,13 @@ void CLevel::IR_OnKeyboardPress(int key)
 
     EGameActions _curr = get_binded_action(key);
 
-#ifdef INPUT_CALLBACKS
     /* avo: script callback */
     if (!g_bDisableAllInput && g_actor)
-        g_actor->callback(GameObject::eKeyPress)(key);
-    /* avo: end */
-#endif
+    {
+        auto& cb = g_actor->callback(GameObject::eKeyPress);
+        if (cb)
+            cb(key);
+    }
 
     if (_curr == kPAUSE)
     {
@@ -501,12 +505,13 @@ void CLevel::IR_OnKeyboardRelease(int key)
     if (!bReady || g_bDisableAllInput)
         return;
 
-#ifdef INPUT_CALLBACKS
     /* avo: script callback */
     if (g_actor)
-        g_actor->callback(GameObject::eKeyRelease)(key);
-    /* avo: end */
-#endif
+    {
+        auto& cb = g_actor->callback(GameObject::eKeyRelease);
+        if (cb)
+            cb(key);
+    }
 
     if (CurrentGameUI() && CurrentGameUI()->IR_UIOnKeyboardRelease(key))
         return;
@@ -532,12 +537,13 @@ void CLevel::IR_OnKeyboardHold(int key)
     if (g_bDisableAllInput)
         return;
 
-#ifdef INPUT_CALLBACKS
     /* avo: script callback */
     if (g_actor)
-        g_actor->callback(GameObject::eKeyHold)(key);
-    /* avo: end */
-#endif
+    {
+        auto& cb = g_actor->callback(GameObject::eKeyHold);
+        if (cb)
+            cb(key);
+    }
 
 #ifdef DEBUG
     // Lain: added

res/gamedata/scripts/ai_stalker.script
@@ -1,11 +1,0 @@
--- if return game_object then it ignores engine. If return nil, then engine tries to find item to kill
--- called from engine!
-function update_best_weapon(npc,cur_wpn)
-	return
-end
-
--- called from engine
--- return false to use Engine technique, return true to ignore engine technique
-function CSE_ALifeObject_spawn_supplies(se_obj,id,ini_string)
-	return false
-end

res/gamedata/scripts/alife_storage_manager.script
@@ -1,14 +1,0 @@
--- called from engine!
-function CALifeStorageManager_before_save(fname)
-
-end 
-
--- called from engine!
-function CALifeStorageManager_save(fname)
-
-end 
-
--- called from engine
-function CALifeStorageManager_load(fname)
-
-end

res/gamedata/scripts/level_input.script
@@ -1,5 +1,0 @@
-function on_key_press(key,bind)
-	-- return true on valid action
-
-	return false
-end

src/Common/Config.hpp
@@ -6,18 +6,12 @@
 // CONFIG_SCRIPT_ENGINE_LOG_SKIPPED_EXPORTS
 
 /* Callbacks */
-#define EXTENDED_ITEM_CALLBACKS // (eItemToBelt, eItemToSlot, eItemToRuck)
-#define EXTENDED_WEAPON_CALLBACKS // (eOnWeaponZoomIn, eOnWeaponZoomOut, eOnWeaponJammed)
 //#define ACTOR_BEFORE_DEATH_CALLBACK // For extending the life of the actor to fake death or do other tasks that need to happen before actor is dead
-#define INPUT_CALLBACKS // (eKeyPress, eKeyRelease, eKeyHold, eMouseMove, eMouseWheel)
-#define ENGINE_LUA_ALIFE_STORAGE_MANAGER_CALLBACKS // calls lua functions from engine in a script named alife_storage_manager.script  (alife_storage_manager.CALifeStorageManager_save) and (alife_storage_manager.CALifeStorageManager_load)
-#define ENGINE_LUA_ALIFE_UPDAGE_MANAGER_CALLBACKS // calls lua function named on_before_change_level and on_after_new_game in _G.script when enabled
 
 /* Scripts */
 #define MORE_INVENTORY_SLOTS // Adds 5 more slots CUSTOM_SLOT_1..5
 #define GAME_OBJECT_EXTENDED_EXPORTS // see: script_game_object*.cpp/h
 #define GAME_OBJECT_CASTING_EXPORTS // see: script_game_object4.cpp  functions for object casting (ie. cast_Car(), cast_Heli())
-#define NAMESPACE_LEVEL_EXPORTS // see: level_script.cpp
 
 /* Tweaks: */
 #define DEAD_BODY_COLLISION // restore collision with dead bodies (thanks malandrinus)

commit 1835156585c0276578f95a8846686bc8d1dd8744

Fixed callbacks broken with 2c0d05225b1c57a585d7f2b3d64558b28c9733f4

src/xrGame/InventoryOwner.cpp
@@ -451,9 +451,7 @@ void CInventoryOwner::OnItemBelt(CInventoryItem* inventory_item, const SInvItemP
     /* avo: script callback */
     CGameObject* object = smart_cast<CGameObject*>(this);
     VERIFY(object);
-    auto& cb = object->callback(GameObject::eItemToBelt);
-    if (cb)
-        cb(inventory_item->object().lua_game_object());
+    object->callback(GameObject::eItemToBelt)(inventory_item->object().lua_game_object());
 }
 
 void CInventoryOwner::OnItemRuck(CInventoryItem* inventory_item, const SInvItemPlace& previous_place)
@@ -461,9 +459,7 @@ void CInventoryOwner::OnItemRuck(CInventoryItem* inventory_item, const SInvItemP
     /* avo: script callback */
     CGameObject* object = smart_cast<CGameObject*>(this);
     VERIFY(object);
-    auto& cb = object->callback(GameObject::eItemToRuck);
-    if (cb)
-        cb(inventory_item->object().lua_game_object());
+    object->callback(GameObject::eItemToRuck)(inventory_item->object().lua_game_object());
 
     detach(inventory_item);
 }
@@ -471,11 +467,9 @@ void CInventoryOwner::OnItemRuck(CInventoryItem* inventory_item, const SInvItemP
 void CInventoryOwner::OnItemSlot(CInventoryItem* inventory_item, const SInvItemPlace& previous_place)
 {
     /* avo: script callback */
-    CGameObject	*object = smart_cast<CGameObject*>(this);
+    CGameObject* object = smart_cast<CGameObject*>(this);
     VERIFY(object);
-    auto& cb = object->callback(GameObject::eItemToSlot);
-    if (cb)
-        cb(inventory_item->object().lua_game_object());
+    object->callback(GameObject::eItemToSlot)(inventory_item->object().lua_game_object());
 
     attach(inventory_item);
 }

src/xrGame/Level_input.cpp
@@ -51,9 +51,7 @@ void CLevel::IR_OnMouseWheel(int x, int y)
     /* avo: script callback */
     if (g_actor)
     {
-        auto& cb = g_actor->callback(GameObject::eMouseWheel);
-        if (cb)
-            cb(x);
+        g_actor->callback(GameObject::eMouseWheel)(x);
     }
 
     if (CurrentGameUI()->IR_UIOnMouseWheel(x, y))
@@ -84,9 +82,7 @@ void CLevel::IR_OnMouseMove(int dx, int dy)
     /* avo: script callback */
     if (g_actor)
     {
-        auto& cb = g_actor->callback(GameObject::eMouseMove);
-        if (cb)
-            cb(dx, dy);
+        g_actor->callback(GameObject::eMouseMove)(dx, dy);
     }
 
     if (CurrentGameUI()->IR_UIOnMouseMove(dx, dy))
@@ -130,9 +126,7 @@ void CLevel::IR_OnKeyboardPress(int key)
     /* avo: script callback */
     if (!g_bDisableAllInput && g_actor)
     {
-        auto& cb = g_actor->callback(GameObject::eKeyPress);
-        if (cb)
-            cb(key);
+        g_actor->callback(GameObject::eKeyPress)(key);
     }
 
     if (_curr == kPAUSE)
@@ -508,9 +502,7 @@ void CLevel::IR_OnKeyboardRelease(int key)
     /* avo: script callback */
     if (g_actor)
     {
-        auto& cb = g_actor->callback(GameObject::eKeyRelease);
-        if (cb)
-            cb(key);
+        g_actor->callback(GameObject::eKeyRelease)(key);
     }
 
     if (CurrentGameUI() && CurrentGameUI()->IR_UIOnKeyboardRelease(key))
@@ -540,9 +532,7 @@ void CLevel::IR_OnKeyboardHold(int key)
     /* avo: script callback */
     if (g_actor)
     {
-        auto& cb = g_actor->callback(GameObject::eKeyHold);
-        if (cb)
-            cb(key);
+        g_actor->callback(GameObject::eKeyHold)(key);
     }
 
 #ifdef DEBUG