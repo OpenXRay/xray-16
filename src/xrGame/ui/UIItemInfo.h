4725e9681349e91024f291ffb5cae998099345f0
Xottab-DUTY <xottab-duty@yandex.ru>
2019-05-23 09:23:24 +0500

    xrGame/ui/UIItemInfo.cpp: code simplification Replaced C-style cast with static_cast Also changed types: NULL -> nullptr LPCSTR -> pcstr

--- src/xrGame/ui/UIItemInfo.h
+++ src/xrGame/ui/UIItemInfo.h
@@ -36,8 +36,8 @@ public:
     CInventoryItem* CurrentItem() const { return m_pInvItem; }
     void InitItemInfo(Fvector2 pos, Fvector2 size, LPCSTR xml_name);
     void InitItemInfo(LPCSTR xml_name);
-    void InitItem(
-        CUICellItem* pCellItem, CInventoryItem* pCompareItem = NULL, u32 item_price = u32(-1), LPCSTR trade_tip = NULL);
+    void InitItem(CUICellItem* pCellItem, CInventoryItem* pCompareItem = nullptr,
+        u32 item_price = u32(-1), pcstr trade_tip = nullptr);
 
     void TryAddConditionInfo(CInventoryItem& pInvItem, CInventoryItem* pCompareItem);
     void TryAddWpnInfo(CInventoryItem& pInvItem, CInventoryItem* pCompareItem);
