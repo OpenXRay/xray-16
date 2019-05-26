20c12e9583f067af92285b0e0002d96931015f7a
Xottab-DUTY <xottab-duty@yandex.ru>
2019-05-26 05:52:48 +0500

    xrGame/StalkerOutfit.cpp: formatting

--- src/xrGame/StalkerOutfit.cpp
+++ src/xrGame/StalkerOutfit.cpp
@@ -7,8 +7,14 @@ CStalkerOutfit::CStalkerOutfit() {}
 CStalkerOutfit::~CStalkerOutfit() {}
 using namespace luabind;
 
-SCRIPT_EXPORT(CStalkerOutfit, (CGameObject), {
-    module(luaState)[class_<CStalkerOutfit, CGameObject>("CStalkerOutfit").def(constructor<>()),
+SCRIPT_EXPORT(CStalkerOutfit, (CGameObject),
+{
+    module(luaState)
+    [
+        class_<CStalkerOutfit, CGameObject>("CStalkerOutfit")
+            .def(constructor<>()),
 
-        class_<CHelmet, CGameObject>("CHelmet").def(constructor<>())];
+        class_<CHelmet, CGameObject>("CHelmet")
+            .def(constructor<>())
+    ];
 });
