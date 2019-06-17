b401747f1238270289413ab26cd0993691d63f3d
Xottab-DUTY <xottab-duty@yandex.ru>
2019-06-17 16:44:44 +0500

    ScriptXMLInit.cpp, UITabControl_script.cpp: reformatting

--- src/xrUICore/TabControl/UITabControl_script.cpp
+++ src/xrUICore/TabControl/UITabControl_script.cpp
@@ -6,17 +6,20 @@
 using namespace luabind;
 using namespace luabind::policy;
 
-SCRIPT_EXPORT(CUITabControl, (CUIWindow), {
-    module(luaState)[class_<CUITabControl, CUIWindow>("CUITabControl")
-                         .def(constructor<>())
-                         .def("AddItem", (bool (CUITabControl::*)(CUITabButton*))(&CUITabControl::AddItem), adopt<2>())
-                         .def("AddItem",
-                             (bool (CUITabControl::*)(LPCSTR, LPCSTR, Fvector2, Fvector2)) & CUITabControl::AddItem)
-                         .def("RemoveAll", &CUITabControl::RemoveAll)
-                         .def("GetActiveId", &CUITabControl::GetActiveId_script)
-                         .def("GetTabsCount", &CUITabControl::GetTabsCount)
-                         .def("SetActiveTab", &CUITabControl::SetActiveTab_script)
-                         .def("GetButtonById", &CUITabControl::GetButtonById_script)];
+SCRIPT_EXPORT(CUITabControl, (CUIWindow),
+{
+    module(luaState)
+    [
+        class_<CUITabControl, CUIWindow>("CUITabControl")
+            .def(constructor<>())
+            .def("AddItem", (bool (CUITabControl::*)(CUITabButton*))(&CUITabControl::AddItem), adopt<2>())
+            .def("AddItem", (bool (CUITabControl::*)(LPCSTR, LPCSTR, Fvector2, Fvector2)) & CUITabControl::AddItem)
+            .def("RemoveAll", &CUITabControl::RemoveAll)
+            .def("GetActiveId", &CUITabControl::GetActiveId_script)
+            .def("GetTabsCount", &CUITabControl::GetTabsCount)
+            .def("SetActiveTab", &CUITabControl::SetActiveTab_script)
+            .def("GetButtonById", &CUITabControl::GetButtonById_script)
+    ];
 });
 
 SCRIPT_EXPORT(CUITabButton, (CUIButton),
