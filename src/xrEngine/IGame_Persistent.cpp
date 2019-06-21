a4920069f57ab1bca59800d41a2feaba4c80eb92
Xottab-DUTY <xottab-duty@yandex.ru>
2019-06-21 07:12:14 +0500

    Check for psSoundPrecacheAll before prefetching sounds

--- src/xrEngine/IGame_Persistent.cpp
+++ src/xrEngine/IGame_Persistent.cpp
@@ -142,8 +142,11 @@ void IGame_Persistent::Prefetch()
     timer.Start();
     const auto memoryBefore = Memory.mem_usage();
 
-    Log("Loading sounds...");
-    GEnv.Sound->prefetch();
+    if (psSoundPrecacheAll != 0)
+    {
+        Log("Loading sounds...");
+        GEnv.Sound->prefetch();
+    }
 
     Log("Loading objects...");
     ObjectPool.prefetch();
