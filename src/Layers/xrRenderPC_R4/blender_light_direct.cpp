693e6829d6b131502891f0555a6f62053770b14a
Xottab-DUTY <xottab-duty@yandex.ru>
2019-06-23 01:36:29 +0500

    xrRender_R3-R4: use stub_notransform_2uv when accum_sun doesn't exist (#382)

--- src/Layers/xrRenderPC_R4/blender_light_direct.cpp
+++ src/Layers/xrRenderPC_R4/blender_light_direct.cpp
@@ -24,7 +24,8 @@ void CBlender_accum_direct::Compile(CBlender_Compile& C)
     case SE_SUN_NEAR: // near pass - enable Z-test to perform depth-clipping
     case SE_SUN_MIDDLE: // middle pass - enable Z-test to perform depth-clipping
         //	FVF::TL2uv
-        C.r_Pass("accum_sun", "accum_sun_near_nomsaa_nominmax", false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
+        C.r_Pass({ "accum_sun", "stub_notransform_2uv" }, "accum_sun_near_nomsaa_nominmax",
+            false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
 
         C.r_CullMode(D3DCULL_NONE);
         C.PassSET_ZB(TRUE, FALSE, TRUE); // force inverted Z-Buffer
@@ -48,7 +49,8 @@ void CBlender_accum_direct::Compile(CBlender_Compile& C)
     case SE_SUN_FAR: // far pass, only stencil clipping performed
         //	FVF::TL2uv
         // C.r_Pass			("null",			"accum_sun_far",	false,	TRUE,	FALSE,blend,D3DBLEND_ONE,dest);
-        C.r_Pass("accum_sun", "accum_sun_far_nomsaa", false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
+        C.r_Pass({ "accum_sun", "stub_notransform_2uv" }, "accum_sun_far_nomsaa",
+            false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
         C.r_CullMode(D3DCULL_NONE);
         // C.r_Sampler_rtf		("s_position",		r2_RT_P			);
         // C.r_Sampler_rtf		("s_normal",		r2_RT_N			);
@@ -107,7 +109,8 @@ void CBlender_accum_direct::Compile(CBlender_Compile& C)
     //	SE_SUN_NEAR for min/max
     case SE_SUN_NEAR_MINMAX: // near pass - enable Z-test to perform depth-clipping
         //	FVF::TL2uv
-        C.r_Pass("accum_sun", "accum_sun_near_nomsaa_minmax", false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
+        C.r_Pass({ "accum_sun", "stub_notransform_2uv" }, "accum_sun_near_nomsaa_minmax",
+            false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
         C.r_CullMode(D3DCULL_NONE);
         C.PassSET_ZB(TRUE, FALSE, TRUE); // force inverted Z-Buffer
 
@@ -187,7 +190,8 @@ void CBlender_accum_direct_msaa::Compile(CBlender_Compile& C)
     case SE_SUN_MIDDLE: // middle pass - enable Z-test to perform depth-clipping
         //	FVF::TL2uv
         // C.r_Pass			("null",			"accum_sun_near",	false,	TRUE,	FALSE,blend,D3DBLEND_ONE,dest);
-        C.r_Pass("accum_sun", "accum_sun_near_msaa_nominmax", false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
+        C.r_Pass({ "accum_sun", "stub_notransform_2uv" }, "accum_sun_near_msaa_nominmax",
+            false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
         C.r_CullMode(D3DCULL_NONE);
         C.PassSET_ZB(TRUE, FALSE, TRUE); // force inverted Z-Buffer
         // C.r_Sampler_rtf		("s_position",		r2_RT_P			);
@@ -222,7 +226,8 @@ void CBlender_accum_direct_msaa::Compile(CBlender_Compile& C)
     case SE_SUN_FAR: // far pass, only stencil clipping performed
         //	FVF::TL2uv
         // C.r_Pass			("null",			"accum_sun_far",	false,	TRUE,	FALSE,blend,D3DBLEND_ONE,dest);
-        C.r_Pass("accum_sun", "accum_sun_far_msaa", false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
+        C.r_Pass({ "accum_sun", "stub_notransform_2uv" }, "accum_sun_far_msaa",
+            false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
         C.r_CullMode(D3DCULL_NONE);
         // C.r_Sampler_rtf		("s_position",		r2_RT_P			);
         // C.r_Sampler_rtf		("s_normal",		r2_RT_N			);
@@ -281,7 +286,8 @@ void CBlender_accum_direct_msaa::Compile(CBlender_Compile& C)
     //	SE_SUN_NEAR for minmax
     case SE_SUN_NEAR_MINMAX: // near pass - enable Z-test to perform depth-clipping
         //	FVF::TL2uv
-        C.r_Pass("accum_sun", "accum_sun_near_msaa_minmax", false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
+        C.r_Pass({ "accum_sun", "stub_notransform_2uv" }, "accum_sun_near_msaa_minmax",
+            false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
         C.r_CullMode(D3DCULL_NONE);
         C.PassSET_ZB(TRUE, FALSE, TRUE); // force inverted Z-Buffer
 
@@ -335,7 +341,8 @@ void CBlender_accum_direct_volumetric_msaa::Compile(CBlender_Compile& C)
     switch (C.iElement)
     {
     case 0: // near pass - enable Z-test to perform depth-clipping
-        C.r_Pass("accum_sun", "accum_volumetric_sun_msaa", false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
+        C.r_Pass({ "accum_sun", "stub_notransform_2uv" }, "accum_volumetric_sun_msaa",
+            false, TRUE, FALSE, blend, D3DBLEND_ONE, dest);
         C.r_dx10Texture("s_lmap", C.L_textures[0]);
         C.r_dx10Texture("s_smap", r2_RT_smap_depth);
         C.r_dx10Texture("s_noise", "fx" DELIMITER "fx_noise");
@@ -369,8 +376,8 @@ void CBlender_accum_direct_volumetric_sun_msaa::Compile(CBlender_Compile& C)
     switch (C.iElement)
     {
     case 0: // near pass - enable Z-test to perform depth-clipping
-        C.r_Pass(
-            "accum_sun", "accum_volumetric_sun_msaa", false, false, false, true, D3DBLEND_ONE, D3DBLEND_ONE, false, 0);
+        C.r_Pass({ "accum_sun", "stub_notransform_2uv" }, "accum_volumetric_sun_msaa",
+            false, false, false, true, D3DBLEND_ONE, D3DBLEND_ONE, false, 0);
         C.r_dx10Texture("s_smap", r2_RT_smap_depth);
         C.r_dx10Texture("s_position", r2_RT_P);
         jitter(C);
