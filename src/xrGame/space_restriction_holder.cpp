0b16ccf7a529dfdc7ce6e0fce858baa115da40e6
Ilya Orlov <eagleivg@mail.ru>
2019-06-29 01:00:55 +0300

    xrGame: fix use-after-free under Linux at quick load

--- src/xrGame/space_restriction_holder.cpp
+++ src/xrGame/space_restriction_holder.cpp
@@ -25,7 +25,9 @@ const u32 time_to_delete = 300000;
 CSpaceRestrictionHolder::~CSpaceRestrictionHolder() { clear(); }
 void CSpaceRestrictionHolder::clear()
 {
+#ifndef __GNUC__ // At least GCC call destructor of members at call parent destructor
     delete_data(m_restrictions);
+#endif
     m_default_out_restrictions = "";
     m_default_in_restrictions = "";
 }
