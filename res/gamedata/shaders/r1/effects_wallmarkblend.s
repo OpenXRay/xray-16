c61a7a704b21b3cb4446cd99804760bb9bd9f0ab
SkyLoaderr <skyloader1@mail.ru>
2019-07-03 11:30:31 +0300

    xrRenderR1 shaders: fixed fogging wallmarks

--- res/gamedata/shaders/r1/effects_wallmarkblend.s
+++ res/gamedata/shaders/r1/effects_wallmarkblend.s
@@ -1,5 +1,5 @@
 function normal		(shader, t_base, t_second, t_detail)
-	shader:begin	("wmark",	"vert")
+	shader:begin	("wmark_blend",	"vert")
 			: sorting	(2, false)
 			: blend		(true,blend.srcalpha,blend.invsrcalpha)
 			: aref 		(true,0)
@@ -9,11 +9,11 @@ function normal		(shader, t_base, t_second, t_detail)
 end
 
 function l_spot    (shader, t_base, t_second, t_detail)
-  r1_lspot	(shader, t_base, "wmark_spot")
-	shader		: sorting	(2, false)	
+	r1_lspot	(shader, t_base, "wmark_spot")
+	shader:sorting(2, false)	
 end
 
 function l_point  (shader, t_base, t_second, t_detail)
-  r1_lpoint	(shader, t_base, "wmark_point")
-	shader		: sorting	(2, false)
+	r1_lpoint	(shader, t_base, "wmark_point")
+	shader:sorting(2, false)
 end
