0c6229c8abc75407e2a88e77bf7b7b29ad685210
Xottab-DUTY <xottab-duty@yandex.ru>
2019-07-11 21:15:52 +0500

    xrEngine/Device_Initialize.cpp: hide only GL context with -no_gl_context key

--- src/xrEngine/Device_Initialize.cpp
+++ src/xrEngine/Device_Initialize.cpp
@@ -52,14 +52,15 @@ void CRenderDevice::Initialize()
         {
             flags |= SDL_WINDOW_OPENGL;
 
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+            SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+            SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+            SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
             if (!strstr(Core.Params, "-no_gl_context"))
             {
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
                 SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
                 SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-                SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-                SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-                SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
             }
         }
 
