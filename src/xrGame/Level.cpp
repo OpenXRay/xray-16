OpenXRay/xray-16

commit 3d27d9c5d32a99aa2883f4f598ff62c641499cfd

Moved CAI_Stalker::update_object_handler processing from seqParallel to TaskScheduler

src/xrGame/Level.cpp
@@ -624,6 +624,10 @@ void CLevel::OnRender()
 {
     GEnv.Render->BeforeWorldRender();	//--#SM+#-- +SecondVP+
 
+    CAI_Stalker* stalker = smart_cast<CAI_Stalker*>(Level().CurrentEntity());
+    if (stalker)
+        stalker->ShouldProcessOnRender(true);
+
     inherited::OnRender();
     if (!game)
         return;
@@ -633,6 +637,7 @@ void CLevel::OnRender()
     // Device.Statistic->TEST1.End();
 
     GEnv.Render->AfterWorldRender(); //--#SM+#-- +SecondVP+
+    WorldRendered(true);
 
     if (!Device.IsAnselActive)
         HUD().RenderUI();
@@ -647,9 +652,11 @@ void CLevel::OnRender()
 #ifdef DEBUG_PRECISE_PATH
     test_precise_path();
 #endif
-    CAI_Stalker* stalker = smart_cast<CAI_Stalker*>(Level().CurrentEntity());
     if (stalker)
+    {
         stalker->OnRender();
+        stalker->ShouldProcessOnRender(false);
+    }
     if (bDebug)
     {
         for (u32 I = 0; I < Level().Objects.o_count(); I++)

src/xrEngine/IGame_Level.h
@@ -58,6 +58,8 @@ class ENGINE_API IGame_Level : public FactoryObjectBase,
                                public pureFrame,
                                public IEventReceiver
 {
+    bool m_world_rendered;
+
 protected:
     // Network interface
     IGameObject* pCurrentEntity;
@@ -111,6 +113,9 @@ public:
     virtual void OnFrame(void);
     virtual void OnRender(void);
     virtual void DumpStatistics(class IGameFont& font, class IPerformanceAlert* alert);
+
+    [[nodiscard]] bool WorldRendered() const { return m_world_rendered; }
+    void WorldRendered(bool rendered) { m_world_rendered = rendered; }
 
     virtual shared_str OpenDemoFile(const char* demo_file_name) = 0;
     virtual void net_StartPlayDemo() = 0;

src/xrEngine/xr_object.h
@@ -215,6 +215,7 @@ public:
     virtual void Load(LPCSTR section) = 0;
     virtual void PostLoad(LPCSTR section) = 0; //--#SM+#--
     // Update
+    virtual void PreUpdateCL() = 0;
     virtual void UpdateCL() = 0; // Called each frame, so no need for dt
     virtual void PostUpdateCL(bool bUpdateCL_disabled) = 0; //--#SM+#-- Вызывается всегда, в отличии от UpdateCL [called always for object regardless of it being active\sleep]
     // Position stack
@@ -301,6 +302,8 @@ public:
     virtual BOOL TestServerFlag(u32 flag) const = 0;
     virtual bool can_validate_position_on_spawn() = 0;
 #ifdef DEBUG
+    virtual bool ShouldProcessOnRender() const = 0;
+    virtual void ShouldProcessOnRender(bool should_process) = 0;
     virtual void OnRender() = 0;
 #endif
     virtual void reinit() = 0;

src/xrEngine/xr_object_list.cpp
@@ -272,7 +272,10 @@ void CObjectList::Update(bool bForce)
             }
 
             for (IGameObject** i = b; i != e; ++i)
+            {
+                (*i)->PreUpdateCL();
                 SingleUpdate(*i);
+            }
 
             //--#SM+#-- PostUpdateCL для всех клиентских объектов [for crowed and non-crowed]
             for (auto& object : objects_active)

src/xrGame/GameObject.cpp
@@ -1397,7 +1397,8 @@ void CGameObject::UpdateCL()
     m_previous_matrix = XFORM();
 }
 
-void CGameObject::PostUpdateCL(bool bUpdateCL_disabled) {}
+void CGameObject::PreUpdateCL() { m_client_updated = false; }
+void CGameObject::PostUpdateCL(bool /*bUpdateCL_disabled*/) { m_client_updated = true;}
 
 void CGameObject::on_matrix_change(const Fmatrix& previous) { obstacle().on_move(); }
 #ifdef DEBUG

src/xrGame/GameObject.h
@@ -99,6 +99,8 @@ protected:
     CScriptBinder scriptBinder;
     bool m_bObjectRemoved;
     CInifile* m_ini_file;
+    bool m_should_process_onrender{};
+    bool m_client_updated{};
 
 public:
     CGameObject();
@@ -219,6 +221,7 @@ public:
     // Methods
     virtual void Load(LPCSTR section) override;
     void PostLoad(LPCSTR section) override; //--#SM+#--
+    void PreUpdateCL() override;
     virtual void UpdateCL() override; // Called each frame, so no need for dt
     void PostUpdateCL(bool bUpdateCL_disabled) override; //--#SM+#--
     virtual void OnChangeVisual() override;
@@ -277,6 +280,8 @@ public:
     virtual BOOL TestServerFlag(u32 Flag) const override;
     virtual bool can_validate_position_on_spawn() override { return true; }
 #ifdef DEBUG
+    bool ShouldProcessOnRender() const override { return m_should_process_onrender; }
+    void ShouldProcessOnRender(bool should_process) override { m_should_process_onrender = should_process; }
     virtual void OnRender() override;
 #endif
     virtual void reinit() override;

src/xrGame/GamePersistent.cpp
@@ -618,6 +618,8 @@ void CGamePersistent::OnFrame()
     if (!g_pGameLevel->bReady)
         return;
 
+    g_pGameLevel->WorldRendered(false);
+
     if (Device.Paused())
     {
         if (Level().IsDemoPlay())

src/xrGame/ai/stalker/ai_stalker.cpp
@@ -61,6 +61,7 @@
 #include "smart_cover_animation_selector.h"
 #include "smart_cover_animation_planner.h"
 #include "smart_cover_planner_target_selector.h"
+#include "xrEngine/TaskScheduler.hpp"
 
 #ifdef DEBUG
 #include "alife_simulator.h"
@@ -638,14 +639,7 @@ void CAI_Stalker::net_Destroy()
     CInventoryOwner::net_Destroy();
     m_pPhysics_support->in_NetDestroy();
 
-    Device.remove_from_seq_parallel(fastdelegate::FastDelegate0<>(this, &CAI_Stalker::update_object_handler));
-
-#ifdef DEBUG
-    fastdelegate::FastDelegate0<> f = fastdelegate::FastDelegate0<>(this, &CAI_Stalker::update_object_handler);
-    xr_vector<fastdelegate::FastDelegate0<>>::const_iterator I;
-    I = std::find(Device.seqParallel.begin(), Device.seqParallel.end(), f);
-    VERIFY(I == Device.seqParallel.end());
-#endif // DEBUG
+    TaskScheduler->RemoveTask({ this, &CAI_Stalker::update_object_handler });
 
     xr_delete(m_ce_close);
     xr_delete(m_ce_far);
@@ -789,6 +783,11 @@ void CAI_Stalker::update_object_handler()
     }
 }
 
+bool CAI_Stalker::mt_object_handler_update_allowed() const
+{
+    return !ShouldProcessOnRender() && g_pGameLevel->WorldRendered() && m_client_updated;
+}
+
 void CAI_Stalker::create_anim_mov_ctrl(CBlend* b, Fmatrix* start_pose, bool local_animation)
 {
     inherited::create_anim_mov_ctrl(b, start_pose, local_animation);
@@ -823,13 +822,9 @@ void CAI_Stalker::UpdateCL()
     {
         if (g_mt_config.test(mtObjectHandler) && CObjectHandler::planner().initialized())
         {
-            fastdelegate::FastDelegate0<> f = fastdelegate::FastDelegate0<>(this, &CAI_Stalker::update_object_handler);
-#ifdef DEBUG
-            xr_vector<fastdelegate::FastDelegate0<>>::const_iterator I;
-            I = std::find(Device.seqParallel.begin(), Device.seqParallel.end(), f);
-            VERIFY(I == Device.seqParallel.end());
-#endif
-            Device.seqParallel.push_back(fastdelegate::FastDelegate0<>(this, &CAI_Stalker::update_object_handler));
+            TaskScheduler->AddTask("CAI_Stalker::update_object_handler", Task::Type::Game,
+                { this, &CAI_Stalker::update_object_handler },
+                { this, &CAI_Stalker::mt_object_handler_update_allowed });
         }
         else
         {

commit 0ee3e886fa46571c6a3006ee73aade4826994e2e

Fixed Release compilation (broken after 3d27d9c5d32a99aa2883f4f598ff62c641499cfd)

src/xrGame/Level.cpp
@@ -624,9 +624,11 @@ void CLevel::OnRender()
 {
     GEnv.Render->BeforeWorldRender();	//--#SM+#-- +SecondVP+
 
+#ifdef DEBUG
     CAI_Stalker* stalker = smart_cast<CAI_Stalker*>(Level().CurrentEntity());
     if (stalker)
         stalker->ShouldProcessOnRender(true);
+#endif
 
     inherited::OnRender();
     if (!game)