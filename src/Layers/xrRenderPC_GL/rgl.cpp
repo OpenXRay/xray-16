549ffa3f6900cf395a2343dc31ae312a8290c4fe
Xottab-DUTY <xottab-duty@yandex.ru>
2019-08-22 16:22:20 +0500

    xrRenderPC_GL/rgl.cpp: removed dead code

--- src/Layers/xrRenderPC_GL/rgl.cpp
+++ src/Layers/xrRenderPC_GL/rgl.cpp
@@ -449,16 +449,6 @@ void CRender::reset_end()
     m_bFirstFrameAfterReset = true;
 }
 
-/*
-void CRender::OnFrame()
-{
-    Models->DeleteQueue			();
-    if (ps_r2_ls_flags.test(R2FLAG_EXP_MT_CALC))	{
-        Device.seqParallel.insert	(Device.seqParallel.begin(),
-            fastdelegate::FastDelegate0<>(&HOM,&CHOM::MT_RENDER));
-    }
-}*/
-
 void CRender::BeforeFrame()
 {
     // MT-HOM (@front)
