37b0ceb8bb82d434ae4103c25772b9ab10bbc4bb
Xottab-DUTY <xottab-duty@yandex.ru>
2019-10-04 00:36:10 +0500

    Fixed low damage to stalkers and mutants in CS and SOC (#382 and #392)

--- src/xrGame/ai/stalker/ai_stalker_fire.cpp
+++ src/xrGame/ai/stalker/ai_stalker_fire.cpp
@@ -229,7 +229,29 @@ void CAI_Stalker::Hit(SHit* pHDS)
     {
         float BoneArmor = m_boneHitProtection->getBoneArmor(HDS.bone());
         float ap = HDS.armor_piercing;
-        if (!fis_zero(BoneArmor, EPS))
+
+        if (ShadowOfChernobylMode || ClearSkyMode)
+        {
+            if (ap > EPS && ap > BoneArmor)
+            {
+                const float d_ap = ap - BoneArmor;
+                hit_power *= (d_ap / ap);
+
+                if (hit_power < m_boneHitProtection->m_fHitFracNpc)
+                {
+                    hit_power = m_boneHitProtection->m_fHitFracNpc;
+                }
+                if (hit_power < 0.0f) {
+                    hit_power = 0.0f;
+                }
+            }
+            else
+            {
+                hit_power *= m_boneHitProtection->m_fHitFracNpc;
+                HDS.add_wound = false;
+            }
+        }
+        else if (!fis_zero(BoneArmor, EPS))
         {
             if (ap > BoneArmor)
             {
