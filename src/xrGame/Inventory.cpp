10d6d6bfb7d35961363f4206e9f0d8f7fb99340c
Xottab-DUTY <xottab-duty@yandex.ru>
2019-10-04 06:50:41 +0500

    xrGame/Inventory.cpp: cleaned up dead code

--- src/xrGame/Inventory.cpp
+++ src/xrGame/Inventory.cpp
@@ -546,14 +546,6 @@ void CInventory::Activate(u16 slot, bool bForce)
 
     if (GetActiveSlot() == slot || (GetNextActiveSlot() == slot && !bForce))
     {
-        //		if (m_iNextActiveSlot != slot) {
-        //			LPCSTR const name = smart_cast<CGameObject const*>(m_pOwner)->cName().c_str();
-        //			if ( !xr_strcmp("jup_b43_stalker_assistant_pri6695", name) )
-        //				LogStackTrace	("");
-        //			Msg				( "[%6d][%s] CInventory::Activate changing next active slot to %d",
-        //Device.dwTimeGlobal,
-        // name, slot );
-        //		}
         m_iNextActiveSlot = slot;
 #ifdef DEBUG
 //		Msg("--- There's no need to activate slot [%d], next active slot is [%d]", slot, m_iNextActiveSlot);
@@ -575,13 +567,6 @@ void CInventory::Activate(u16 slot, bool bForce)
     {
         if (tmp_item)
         {
-            //			if ( m_iNextActiveSlot != slot) {
-            //				LPCSTR const name = smart_cast<CGameObject const*>(m_pOwner)->cName().c_str();
-            //				if ( !xr_strcmp("jup_b43_stalker_assistant_pri6695", name) )
-            //					LogStackTrace	("");
-            //				Msg				( "[%6d][%s] CInventory::Activate changing next active slot2 to %d",
-            // Device.dwTimeGlobal, name, slot );
-            //			}
             m_iNextActiveSlot = slot;
         }
         else
@@ -613,25 +598,8 @@ void CInventory::Activate(u16 slot, bool bForce)
             if (tmp_item)
                 tmp_item->ActivateItem();
 
-            //!			if ( m_iActiveSlot != slot ) {
-            //!				LPCSTR const name = smart_cast<CGameObject const*>(m_pOwner)->cName().c_str();
-            //				if ( !xr_strcmp("jup_b43_stalker_assistant_pri6695", name) )
-            //					LogStackTrace	("");
-            //!				Msg				("[%6d][%s] CInventory::Activate changing active slot from %d to %d",
-            //!Device.dwTimeGlobal,
-            //! name, m_iActiveSlot, slot );
-            //!			}
-
             m_iActiveSlot = slot;
         }
-        //		if ( m_iNextActiveSlot != slot ) {
-        //			LPCSTR const name = smart_cast<CGameObject const*>(m_pOwner)->cName().c_str();
-        //			if ( !xr_strcmp("jup_b43_stalker_assistant_pri6695", name) && !slot )
-        //				LogStackTrace	("");
-        //			Msg				( "[%6d][%s] CInventory::Activate changing next active slot3 to %d",
-        //Device.dwTimeGlobal,
-        // name, slot );
-        //		}
         m_iNextActiveSlot = slot;
     }
 }
@@ -838,14 +806,6 @@ void CInventory::Update()
                 }
             }
 
-            //			if ( m_iActiveSlot != GetNextActiveSlot() ) {
-            //				LPCSTR const name = smart_cast<CGameObject const*>(m_pOwner)->cName().c_str();
-            //				if ( !xr_strcmp("jup_b43_stalker_assistant_pri6695", name) )
-            //					LogStackTrace	("");
-            //				Msg					("[%6d][%s] CInventory::Activate changing active slot from %d to next active
-            //slot
-            //%d", Device.dwTimeGlobal, name, m_iActiveSlot, GetNextActiveSlot() );
-            //			}
             m_iActiveSlot = GetNextActiveSlot();
         }
         if ((GetNextActiveSlot() != NO_ACTIVE_SLOT) && ActiveItem() && ActiveItem()->cast_hud_item()->IsHidden())
