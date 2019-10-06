52ce00154a7675f517cccfbeeac7ff903094bc48
Xottab-DUTY <xottab-duty@yandex.ru>
2019-10-07 02:45:37 +0500

    Fixed some not working smart covers in CS (#382)

--- src/xrGame/stalker_movement_manager_smart_cover.cpp
+++ src/xrGame/stalker_movement_manager_smart_cover.cpp
@@ -252,11 +252,7 @@ void stalker_movement_manager_smart_cover::reach_enter_location(u32 const& time_
     if (!object().sight().current_action().target_reached())
         return;
 
-    // morrey
-    // смарткаверы чн-ные не работали. заюзал из чн алгоритм
-    //if (target_params().cover()->can_fire()) // ЗП
-    //if (target_params().cover()->is_combat_cover()) // ЧН
-    if (target_params().cover()->can_fire() || target_params().cover()->is_combat_cover()) // Xottab_DUTY to morrey: а если так попробовать?
+    if (target_params().cover()->can_fire())
     {
         CInventoryItem const* const inventory_item = object().inventory().ActiveItem();
         if (!inventory_item)
