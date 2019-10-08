e70dabeb296b4e1b1d385403d0ff97543fd8cb1d
Xottab_DUTY <Xottab-DUTY@users.noreply.github.com>
2019-10-08 08:47:07 +0500

    Forgot to tell vsprintf_s about size...

--- src/xrGame/PHDebug.cpp
+++ src/xrGame/PHDebug.cpp
@@ -1004,7 +1004,7 @@ class CPHDebugOutput : public IDebugOutput
         string1024 t;
         va_list marker;
         va_start(marker, s);
-        vsprintf_s(t, s, marker);
+        vsprintf_s(t, sizeof(t), s, marker);
         va_end(marker);
         DBG_DrawPHAbstruct(new SPHDBGOutText(t));
     }
