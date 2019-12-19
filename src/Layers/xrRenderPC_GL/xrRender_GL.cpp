02522180737f2dd0f79553634b6f284f1534824c
ensign Curvedhand <eagleivg@users.noreply.github.com>
2019-12-19 16:45:11 +0300

    xrRenderPC_GL: add checking OpenGL version (#531)

--- src/Layers/xrRenderPC_GL/xrRender_GL.cpp
+++ src/Layers/xrRenderPC_GL/xrRender_GL.cpp
@@ -30,7 +30,36 @@ XR_EXPORT bool CheckRendererSupport()
     }
 
     // Check if minimal required OpenGL features are available
-    // XXX: implement minimal feature availability check
+    SDL_Window* gl_test_window = SDL_CreateWindow("TestOpenGLWindow", 0, 0, 1, 1, SDL_WINDOW_HIDDEN | SDL_WINDOW_OPENGL);
+
+    if (!gl_test_window)
+    {
+        Log("~ Cannot create helper window for OpenGL:", SDL_GetError());
+        return false;
+    }
+
+    SDL_GLContext gl_test_context = SDL_GL_CreateContext(gl_test_window);
+    if (!gl_test_context)
+    {
+        Log("~ Cannot create OpenGL context:", SDL_GetError());
+        return false;
+    }
+
+    if (glewInit() != GLEW_OK)
+    {
+        Log("~ Could not initialize glew.");
+        return false;
+    }
+
+    if (!glewIsSupported("GL_VERSION_4_1 GL_EXT_multi_draw_arrays"))
+    {
+        Log("~ GL_VERSION_4_1 or GL_EXT_multi_draw_arrays not supported");
+        return false;
+    }
+
+    SDL_GL_DeleteContext(gl_test_context);
+    SDL_DestroyWindow(gl_test_window);
+
     return true;
 }
 } // extern "C"
