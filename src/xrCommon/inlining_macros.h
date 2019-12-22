d270c0bb8e0ef9f8740c5c9f891a420c3b495378
Zigatun <jesumaron@yandex.ru>
2019-12-22 18:03:41 +0700

    Cross-platform realization of breakpoint

--- src/xrCommon/inlining_macros.h
+++ src/xrCommon/inlining_macros.h
@@ -4,7 +4,7 @@
 #define NO_INLINE __attribute__((noinline))
 #define FORCE_INLINE __attribute__((always_inline)) inline
 #define ALIGN(a) __attribute__((aligned(a)))
-#define DEBUG_BREAK asm("int $3")
+#define DEBUG_BREAK __builtin_trap()
 #elif defined(_MSC_VER)
 #define NO_INLINE __declspec(noinline)
 #define FORCE_INLINE __forceinline
