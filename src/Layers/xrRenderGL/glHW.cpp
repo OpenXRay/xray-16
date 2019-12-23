3480f1ed4dedbdb650fb059487d1718da598b01d
Xottab-DUTY <xottab-duty@yandex.ru>
2019-12-23 17:19:08 +0500

    xrRenderGL/glHW.cpp: request 32 bit RGBA

--- src/Layers/xrRenderGL/glHW.cpp
+++ src/Layers/xrRenderGL/glHW.cpp
@@ -139,6 +139,12 @@ void CHW::Reset()
 void CHW::SetPrimaryAttributes()
 {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
