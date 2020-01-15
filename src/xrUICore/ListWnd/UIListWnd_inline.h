556b31d6e0dc48dc4cdf19bcfe42588240cb64ee
Xottab-DUTY <xottab-duty@yandex.ru>
2020-01-15 12:49:55 +0500

    xrUICore/ListWnd/UIListWnd_inline.h: Fixed non-existing function call

--- src/xrUICore/ListWnd/UIListWnd_inline.h
+++ src/xrUICore/ListWnd/UIListWnd_inline.h
@@ -17,8 +17,8 @@ bool CUIListWnd::AddItem(const char* str, const float shift, void* pData,
     VERIFY(pItem);
 
     pItem->Init(str, shift, m_bVertFlip
-                                ? GetHeight() - GetSize() * m_iItemHeight - m_iItemHeight
-                                : GetSize() * m_iItemHeight,
+                                ? GetHeight() - GetItemsCount() * m_iItemHeight - m_iItemHeight
+                                : GetItemsCount() * m_iItemHeight,
                 m_iItemWidth, m_iItemHeight);
 
     pItem->SetData(pData);
