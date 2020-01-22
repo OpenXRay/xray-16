0b7b32bf191c61cbef45735c48c3c04bad0d63e3
Xottab-DUTY <xottab-duty@yandex.ru>
2020-01-23 01:38:42 +0500

    xrRender/r__sector.cpp: forgot to place !USE_DX9 defines

--- src/Layers/xrRender/r__sector.cpp
+++ src/Layers/xrRender/r__sector.cpp
@@ -56,8 +56,10 @@ void CPortal::OnRender()
         RCache.set_xform_world(Fidentity);
         // draw solid
         RCache.set_Shader(RImplementation.m_SelectionShader);
+#ifndef USE_DX9
         RCache.set_c("tfactor", float(color_get_R(portalColor)) / 255.f, float(color_get_G(portalColor)) / 255.f, \
             float(color_get_B(portalColor)) / 255.f, float(color_get_A(portalColor)) / 255.f);
+#endif // !USE_DX9
         RCache.dbg_Draw(D3DPT_TRIANGLELIST, &*V.begin(), V.size() / 3);
 
         // draw wire
@@ -72,8 +74,10 @@ void CPortal::OnRender()
             Device.SetNearer(TRUE);
       
         RCache.set_Shader(RImplementation.m_WireShader);
+#ifndef USE_DX9
         RCache.set_c("tfactor", float(color_get_R(portalColor)) / 255.f, float(color_get_G(portalColor)) / 255.f, \
             float(color_get_B(portalColor)) / 255.f, float(color_get_A(portalColor)) / 255.f);
+#endif // USE_DX9
         RCache.dbg_Draw(D3DPT_LINESTRIP, &*V.begin(), V.size() - 1);
         if (bDebug)
             RImplementation.rmNormal();
