d66e91265254babd1ea9ff1d65a128ba26545bbf
Xottab-DUTY <xottab-duty@yandex.ru>
2020-02-06 04:14:13 +0500

    Moved operator new and delete overloads back to xrCore
    This partially reverts commit ac274d7c6c2c786de6bd2b966b973e2c96be906f

--- src/xrCore/xrMemory.h
+++ src/xrCore/xrMemory.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "_types.h"
-#include "Memory/xr_memory_router.hpp"
 
 class XRCORE_API xrMemory
 {
@@ -34,6 +33,47 @@ extern XRCORE_API xrMemory Memory;
 #define ZeroMemory(dst, size) memset(dst, 0, size)
 #define CopyMemory(dst, src, size) memcpy(dst, src, size)
 #define FillMemory(dst, size, val) memset(dst, val, size)
+
+// Global C++ new/delete overrides.
+[[nodiscard]] inline void* operator new(size_t size)
+{
+    return Memory.mem_alloc(size);
+}
+
+[[nodiscard]] inline void* operator new(size_t size, const std::nothrow_t&)
+{
+    return Memory.mem_alloc(size);
+}
+
+[[nodiscard]] inline void* operator new(size_t size, std::align_val_t alignment)
+{
+    return Memory.mem_alloc(size, static_cast<size_t>(alignment));
+}
+
+[[nodiscard]] inline void* operator new(size_t size, std::align_val_t alignment, const std::nothrow_t&)
+{
+    return Memory.mem_alloc(size, static_cast<size_t>(alignment));
+}
+
+inline void operator delete(void* ptr) noexcept
+{
+    Memory.mem_free(ptr);
+}
+
+inline void operator delete(void* ptr, std::align_val_t alignment) noexcept
+{
+    Memory.mem_free(ptr, static_cast<size_t>(alignment));
+}
+
+inline void operator delete(void* ptr, size_t) noexcept
+{
+    Memory.mem_free(ptr);
+}
+
+inline void operator delete(void* ptr, size_t, std::align_val_t alignment) noexcept
+{
+    Memory.mem_free(ptr, static_cast<size_t>(alignment));
+}
 
 template <class T>
 inline void xr_delete(T*& ptr) noexcept
