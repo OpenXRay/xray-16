OpenXRay/xray-16

commit 689841237f84f2911f647a61dcf9233a38689fe9

xrRenderPC_R2/r2_rendertarget: removed rt_smap_ZB Code refactoring for simplification and reducing branching. This also reduces differences between renderers.

src/Layers/xrRenderPC_R2/r2_rendertarget.cpp
@@ -281,30 +281,29 @@ CRenderTarget::CRenderTarget()
     s_occq.create(b_occq, "r2" DELIMITER "occq");
 
     // DIRECT (spot)
-    D3DFORMAT depth_format = (D3DFORMAT)RImplementation.o.HW_smap_FORMAT;
-
-    if (RImplementation.o.HW_smap)
     {
-        D3DFORMAT nullrt = D3DFMT_R5G6B5;
-        if (RImplementation.o.nullrt)
-            nullrt = (D3DFORMAT)MAKEFOURCC('N', 'U', 'L', 'L');
+        const u32 smapsize = RImplementation.o.smapsize;
+        D3DFORMAT depth_format = D3DFMT_D24X8;
+        D3DFORMAT surf_format = D3DFMT_R32F;
 
-        u32 size = RImplementation.o.smapsize;
-        rt_smap_depth.create(r2_RT_smap_depth, size, size, depth_format);
-        rt_smap_surf.create(r2_RT_smap_surf, size, size, nullrt);
-        rt_smap_ZB = NULL;
-        s_accum_mask.create(b_accum_mask, "r2" DELIMITER "accum_mask");
+        Flags32 flags{};
+        if (!RImplementation.o.HW_smap)
+            flags.flags = CRT::CreateSurface;
+        else
+        {
+            depth_format = (D3DFORMAT)RImplementation.o.HW_smap_FORMAT;
+            if (RImplementation.o.nullrt) // use nullrt if possible
+                surf_format = (D3DFORMAT)MAKEFOURCC('N', 'U', 'L', 'L');
+            else
+                surf_format = D3DFMT_R5G6B5;
+        }
+
+        // Create D3DFMT_D24X8 depth-stencil surface if HW smap is not supported,
+        // otherwise - create texture with specified HW_smap_FORMAT
+        rt_smap_depth.create(r2_RT_smap_depth, smapsize, smapsize, depth_format, 1, flags);
+        rt_smap_surf.create(r2_RT_smap_surf, smapsize, smapsize, surf_format);
+        reinit_cascades();
     }
-    else
-    {
-        u32 size = RImplementation.o.smapsize;
-        rt_smap_surf.create(r2_RT_smap_surf, size, size, D3DFMT_R32F);
-        rt_smap_depth = NULL;
-        R_CHK(HW.pDevice->CreateDepthStencilSurface(
-            size, size, D3DFMT_D24X8, D3DMULTISAMPLE_NONE, 0, TRUE, &rt_smap_ZB, NULL));
-        s_accum_mask.create(b_accum_mask, "r2" DELIMITER "accum_mask");
-    }
-    reinit_cascades();
 
     // POINT
     {
@@ -639,8 +638,6 @@ CRenderTarget::~CRenderTarget()
     t_envmap_1->surface_set(NULL);
     t_envmap_0.destroy();
     t_envmap_1.destroy();
-
-    _RELEASE(rt_smap_ZB);
 
     // Jitter
     for (int it = 0; it < TEX_jitter_count; it++)

src/Layers/xrRenderPC_GL/gl_rendertarget.h
@@ -97,8 +97,6 @@ public:
     ref_rt rt_smap_surf; // 32bit,		color
     ref_rt rt_smap_depth; // 24(32) bit,	depth 
     ref_rt rt_smap_depth_minmax; //	is used for min/max sm
-    //	TODO: DX10: CHeck if we need old-style SMAP
-    //	IDirect3DSurface9*			rt_smap_ZB;		//
 
     //	Igor: for async screenshots
     GLuint t_ss_async; //32bit		(r,g,b,a) is situated in the system memory

src/Layers/xrRenderPC_GL/gl_rendertarget_phase_smap_D.cpp
@@ -2,13 +2,8 @@
 
 void CRenderTarget::phase_smap_direct(light* L, u32 sub_phase)
 {
-    //	TODO: DX10: Check thst we will never need old SMap implementation
     // Targets
-    if (RImplementation.o.HW_smap) u_setrt(rt_smap_surf, NULL, NULL, rt_smap_depth->pZRT);
-        //else								u_setrt	(rt_smap_surf, NULL, NULL, rt_smap_ZB);
-    else
-        VERIFY(!"Use HW SMap only for DX10!");
-
+    u_setrt(rt_smap_surf, nullptr, nullptr, rt_smap_depth->pZRT);
 
     //	Don't have rect clear for DX10
     //	TODO: DX9:	Full clear must be faster for the near phase for SLI

src/Layers/xrRenderPC_GL/gl_rendertarget_phase_smap_S.cpp
@@ -13,11 +13,7 @@ void CRenderTarget::phase_smap_spot_clear()
 void CRenderTarget::phase_smap_spot(light* L)
 {
     // Targets + viewport
-    //	TODO: DX10: CHeck if we don't need old-style SMAP
-    if (RImplementation.o.HW_smap) u_setrt(rt_smap_surf, NULL, NULL, rt_smap_depth->pZRT);
-        //else								u_setrt	(rt_smap_surf, NULL, NULL, rt_smap_ZB);
-    else
-        VERIFY(!"Use HW SMap only for DX10!");
+    u_setrt(rt_smap_surf, nullptr, nullptr, rt_smap_depth->pZRT);
     const D3D_VIEWPORT viewport = { L->X.S.posX, L->X.S.posY, L->X.S.size, L->X.S.size, 0.f, 1.f };
     RCache.SetViewport(viewport);
 

src/Layers/xrRenderPC_R2/r2_rendertarget.h
@@ -74,7 +74,6 @@ public:
     // smap
     ref_rt rt_smap_surf; // 32bit,		color
     ref_rt rt_smap_depth; // 24(32) bit,	depth
-    IDirect3DSurface9* rt_smap_ZB; //
 
     // Textures
     IDirect3DVolumeTexture9* t_material_surf;

src/Layers/xrRenderPC_R2/r2_rendertarget_phase_smap_D.cpp
@@ -3,10 +3,7 @@
 void CRenderTarget::phase_smap_direct(light* L, u32 sub_phase)
 {
     // Targets
-    if (RImplementation.o.HW_smap)
-        u_setrt(rt_smap_surf, NULL, NULL, rt_smap_depth->pRT);
-    else
-        u_setrt(rt_smap_surf, NULL, NULL, rt_smap_ZB);
+    u_setrt(rt_smap_surf, NULL, NULL, rt_smap_depth->pRT);
 
     // Clear
     if (SE_SUN_NEAR == sub_phase)

src/Layers/xrRenderPC_R2/r2_rendertarget_phase_smap_S.cpp
@@ -12,10 +12,7 @@ void CRenderTarget::phase_smap_spot_clear()
 void CRenderTarget::phase_smap_spot(light* L)
 {
     // Targets + viewport
-    if (RImplementation.o.HW_smap)
-        u_setrt(rt_smap_surf, NULL, NULL, rt_smap_depth->pRT);
-    else
-        u_setrt(rt_smap_surf, NULL, NULL, rt_smap_ZB);
+    u_setrt(rt_smap_surf, nullptr, nullptr, rt_smap_depth->pRT);
     RCache.SetViewport({ L->X.S.posX, L->X.S.posY, L->X.S.size, L->X.S.size, 0.f, 1.f });
 
     // Misc		- draw only front-faces //back-faces

src/Layers/xrRenderPC_R3/r3_rendertarget.h
@@ -94,8 +94,6 @@ public:
     ref_rt rt_smap_surf; // 32bit,		color
     ref_rt rt_smap_depth; // 24(32) bit,	depth
     ref_rt rt_smap_depth_minmax; //	is used for min/max sm
-    //	TODO: DX10: CHeck if we need old-style SMAP
-    //	IDirect3DSurface9*			rt_smap_ZB;		//
 
     //	Igor: for async screenshots
     ID3DTexture2D* t_ss_async; // 32bit		(r,g,b,a) is situated in the system memory

commit 1b530f2a4488d7fc25e9d15d563c182b1c462c93

Fixed broken R2 after 689841237f84f2911f647a61dcf9233a38689fe9
Oops, accidentally removed the shader

src/Layers/xrRenderPC_R2/r2_rendertarget.cpp
@@ -302,6 +302,8 @@ CRenderTarget::CRenderTarget()
         // otherwise - create texture with specified HW_smap_FORMAT
         rt_smap_depth.create(r2_RT_smap_depth, smapsize, smapsize, depth_format, 1, flags);
         rt_smap_surf.create(r2_RT_smap_surf, smapsize, smapsize, surf_format);
+
+        s_accum_mask.create(b_accum_mask, "r2" DELIMITER "accum_mask");
         reinit_cascades();
     }
 