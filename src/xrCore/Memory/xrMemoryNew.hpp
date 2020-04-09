16d2b99b74623628debf96ee3055a40ff82accc8
Xottab-DUTY <xottab-duty@yandex.ru>
2020-04-09 23:13:24 +0500

    Proper operator new overloading
    This reverts commit d66e91265254babd1ea9ff1d65a128ba26545bbf and adds new stuff also.

---
+++ src/xrCore/Memory/xrMemoryNew.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+// Force link our lib
+#ifndef COMPILING_XR_MEMORY_LIB
+#pragma comment(lib, "xrMemoryNew.lib")
+#endif
--- src/xrCore/xrMemory.h
+++ src/xrCore/xrMemory.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "_types.h"
+#include "Memory/xrMemoryNew.hpp"
 
 #include <utility>
 
@@ -35,47 +36,6 @@ extern XRCORE_API xrMemory Memory;
 #define ZeroMemory(dst, size) memset(dst, 0, size)
 #define CopyMemory(dst, src, size) memcpy(dst, src, size)
 #define FillMemory(dst, size, val) memset(dst, val, size)
-
-// Global C++ new/delete overrides.
-[[nodiscard]] inline void* operator new(size_t size)
-{
-    return Memory.mem_alloc(size);
-}
-
-[[nodiscard]] inline void* operator new(size_t size, const std::nothrow_t&)
-{
-    return Memory.mem_alloc(size);
-}
-
-[[nodiscard]] inline void* operator new(size_t size, std::align_val_t alignment)
-{
-    return Memory.mem_alloc(size, static_cast<size_t>(alignment));
-}
-
-[[nodiscard]] inline void* operator new(size_t size, std::align_val_t alignment, const std::nothrow_t&)
-{
-    return Memory.mem_alloc(size, static_cast<size_t>(alignment));
-}
-
-inline void operator delete(void* ptr) noexcept
-{
-    Memory.mem_free(ptr);
-}
-
-inline void operator delete(void* ptr, std::align_val_t alignment) noexcept
-{
-    Memory.mem_free(ptr, static_cast<size_t>(alignment));
-}
-
-inline void operator delete(void* ptr, size_t) noexcept
-{
-    Memory.mem_free(ptr);
-}
-
-inline void operator delete(void* ptr, size_t, std::align_val_t alignment) noexcept
-{
-    Memory.mem_free(ptr, static_cast<size_t>(alignment));
-}
 
 template <typename T, typename... Args>
 inline T* xr_new(Args&&... args)
