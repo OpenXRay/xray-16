69a5345bf2acf94806744ffcbb40982fbce8e483
vTurbine <vtrubiloff@gmail.com>
2020-05-30 12:33:45 +0300

    xrRender: Blenders: `ShWorld` refactored

--- src/Layers/xrRender/blenders/Blender_Shadow_World.h
+++ src/Layers/xrRender/blenders/Blender_Shadow_World.h
@@ -1,19 +1,12 @@
-#ifndef BLENDER_SHADOW_WORLD_H
-#define BLENDER_SHADOW_WORLD_H
 #pragma once
 
 class CBlender_ShWorld : public IBlender
 {
 public:
-    virtual LPCSTR getComment() { return "INTERNAL: shadow projecting"; }
-    virtual BOOL canBeLMAPped() { return FALSE; }
-    virtual void Save(IWriter& fs);
-    virtual void Load(IReader& fs, u16 version);
-
-    virtual void Compile(CBlender_Compile& C);
-
     CBlender_ShWorld();
-    virtual ~CBlender_ShWorld();
+    ~CBlender_ShWorld() override = default;
+    
+    LPCSTR getComment() override;
+    
+    void Compile(CBlender_Compile& C) override;
 };
-
-#endif // BLENDER_SHADOW_WORLD_H
