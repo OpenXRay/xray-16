122618422c43bae96cf85a178a503e31a7223b64
vTurbine <vtrubiloff@gmail.com>
2020-05-30 10:06:47 +0300

    xrRender: Blenders: unused palette removed

--- src/Layers/xrRender/Blender.h
+++ src/Layers/xrRender/Blender.h
@@ -49,7 +49,6 @@ protected:
 public:
     static IBlender* Create(CLASS_ID cls);
     static void Destroy(IBlender*& B);
-    static void CreatePalette(xr_vector<IBlender*>& palette);
 
     CBlender_DESC& getDescription() { return description; }
     virtual LPCSTR getName() { return description.cName; }
