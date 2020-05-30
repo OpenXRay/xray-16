030fdd1b17aa0ccb96873436ce0edfba0130c2f8
vTurbine <vtrubiloff@gmail.com>
2020-05-30 12:52:22 +0300

    xrRender: Blenders: `Screen_SET` refactored

--- src/Layers/xrRender/blenders/Blender_Screen_SET.h
+++ src/Layers/xrRender/blenders/Blender_Screen_SET.h
@@ -1,9 +1,3 @@
-// Blender_Screen_SET.h: interface for the Blender_Screen_SET class.
-//
-//////////////////////////////////////////////////////////////////////
-
-#if !defined(AFX_BLENDER_SCREEN_SET_H__A215FA40_D885_4D06_9032_ED934AE295E3__INCLUDED_)
-#define AFX_BLENDER_SCREEN_SET_H__A215FA40_D885_4D06_9032_ED934AE295E3__INCLUDED_
 #pragma once
 
 class CBlender_Screen_SET : public IBlender
@@ -16,16 +10,18 @@ class CBlender_Screen_SET : public IBlender
     xrP_BOOL oFog;
     xrP_BOOL oClamp;
 
+private:
+    void CompileFixed(CBlender_Compile& C);
+    void CompileProgrammed(CBlender_Compile& C);
+
 public:
-    virtual LPCSTR getComment() { return "basic (simple)"; }
-    virtual BOOL canBeLMAPped() { return FALSE; }
-    virtual void Save(IWriter& fs);
-    virtual void Load(IReader& fs, u16 version);
-
-    virtual void Compile(CBlender_Compile& C);
-
     CBlender_Screen_SET();
-    virtual ~CBlender_Screen_SET();
-};
+    ~CBlender_Screen_SET() override = default;
 
-#endif // !defined(AFX_BLENDER_SCREEN_SET_H__A215FA40_D885_4D06_9032_ED934AE295E3__INCLUDED_)
+    LPCSTR getComment() override;
+
+    void Save(IWriter& fs) override;
+    void Load(IReader& fs, u16 version) override;
+
+    void Compile(CBlender_Compile& C) override;
+};
