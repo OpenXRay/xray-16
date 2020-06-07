OpenXRay/xray-16

commit 40105998d414e9322540b52ed323196dc8deab78

Optimize if checks in CWeapon::OnStateSwitch()

src/xrGame/Weapon.cpp
@@ -1826,26 +1826,16 @@ void CWeapon::OnStateSwitch(u32 S, u32 oldState)
     inherited::OnStateSwitch(S, oldState);
     m_BriefInfo_CalcFrame = 0;
 
-    if (GetState() == eReload)
+    if (S == eReload)
     {
-        if (iAmmoElapsed == 0)
-        {
-            if (H_Parent() == Level().CurrentEntity() && !fsimilar(m_zoom_params.m_ReloadEmptyDof.w, -1.0f))
-            {
-                CActor* current_actor = smart_cast<CActor*>(H_Parent());
-                if (current_actor)
+        CActor* current_actor = smart_cast<CActor*>(H_Parent());
+        if (current_actor && H_Parent() == Level().CurrentEntity())
+            if (iAmmoElapsed == 0)
+                if (!fsimilar(m_zoom_params.m_ReloadEmptyDof.w, -1.0f))
                     current_actor->Cameras().AddCamEffector(new CEffectorDOF(m_zoom_params.m_ReloadEmptyDof));
-            }
-        }
-        else
-        {
-            if (H_Parent() == Level().CurrentEntity() && !fsimilar(m_zoom_params.m_ReloadDof.w, -1.0f))
-            {
-                CActor* current_actor = smart_cast<CActor*>(H_Parent());
-                if (current_actor)
+            else
+                if (!fsimilar(m_zoom_params.m_ReloadDof.w, -1.0f))
                     current_actor->Cameras().AddCamEffector(new CEffectorDOF(m_zoom_params.m_ReloadDof));
-            }
-        }
     }
 }
 

commit 491c71487cdc5246f4ad98e797c3d664c3edd22c

Fixed broken reload DOF when ammo count > 0
Was broken after my optimizations in
40105998d414e9322540b52ed323196dc8deab78

Almost 3 year old bug, huh.
But I was unexperienced in 2017, so.. It's good that the bug is found.

Also, I added extra whitespace in Weapon.h, as a part of code review of 044d38318cc60c9707af66100e6ccc11878f967e.

src/xrGame/Weapon.cpp
@@ -1874,12 +1874,11 @@ void CWeapon::OnStateSwitch(u32 S, u32 oldState)
     {
         CActor* current_actor = smart_cast<CActor*>(H_Parent());
         if (current_actor && H_Parent() == Level().CurrentEntity())
-            if (iAmmoElapsed == 0)
-                if (!fsimilar(m_zoom_params.m_ReloadEmptyDof.w, -1.0f))
-                    current_actor->Cameras().AddCamEffector(xr_new<CEffectorDOF>(m_zoom_params.m_ReloadEmptyDof));
-            else
-                if (!fsimilar(m_zoom_params.m_ReloadDof.w, -1.0f))
-                    current_actor->Cameras().AddCamEffector(xr_new<CEffectorDOF>(m_zoom_params.m_ReloadDof));
+        {
+            const Fvector4& reloadDof = iAmmoElapsed == 0 ? m_zoom_params.m_ReloadEmptyDof : m_zoom_params.m_ReloadDof;
+            if (!fsimilar(reloadDof.w, -1.0f))
+                current_actor->Cameras().AddCamEffector(xr_new<CEffectorDOF>(reloadDof));
+        }
     }
 }
 

src/xrGame/Weapon.h
@@ -207,6 +207,7 @@ protected:
         Fvector m_ZoomDof;
         Fvector4 m_ReloadDof;
         Fvector4 m_ReloadEmptyDof;
+
         bool m_bUseDynamicZoom;
         shared_str m_sUseZoomPostprocess;
         shared_str m_sUseBinocularVision;