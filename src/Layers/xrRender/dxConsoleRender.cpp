f76503c776cba285920a3e8148aa9d9ccb99fef1
Xottab-DUTY <xottab-duty@yandex.ru>
2020-06-09 22:43:03 +0500

    xrRender/dxConsoleRender.cpp: Fixed OpenGL using uninitialized variable Extracted the DX10+ code to separate function

--- src/Layers/xrRender/dxConsoleRender.cpp
+++ src/Layers/xrRender/dxConsoleRender.cpp
@@ -3,40 +3,45 @@
 
 dxConsoleRender::dxConsoleRender()
 {
-#if defined(USE_DX10) || defined(USE_DX11)
+#ifndef USE_DX9
     m_Shader.create("hud" DELIMITER "crosshair");
     m_Geom.create(FVF::F_TL, RCache.Vertex.Buffer(), RCache.QuadIB);
 #endif
 }
 
 void dxConsoleRender::Copy(IConsoleRender& _in) { *this = *(dxConsoleRender*)&_in; }
-void dxConsoleRender::OnRender(bool bGame)
-{
-    VERIFY(HW.pDevice);
-
-    D3DRECT R = {0, 0, static_cast<LONG>(Device.dwWidth), static_cast<LONG>(Device.dwHeight)};
-    if (bGame)
-        R.y2 /= 2;
 
 #ifndef USE_DX9
+ICF void ClearWithShader(const D3DRECT& rect, const Fcolor& color, ref_geom& geom, ref_shader& shader)
+{
     u32 vOffset = 0;
-    //	TODO: DX10: Implement console background clearing for DX10
-    FVF::TL* verts = (FVF::TL*)RCache.Vertex.Lock(4, m_Geom->vb_stride, vOffset);
-    verts->set(R.x1, R.y2, color_xrgb(32, 32, 32), 0, 0);
+    FVF::TL* verts = (FVF::TL*)RCache.Vertex.Lock(4, geom->vb_stride, vOffset);
+    verts->set(rect.x1, rect.y2, color.get(), 0, 0);
     verts++;
-    verts->set(R.x1, R.y1, color_xrgb(32, 32, 32), 0, 0);
+    verts->set(rect.x1, rect.y1, color.get(), 0, 0);
     verts++;
-    verts->set(R.x2, R.y2, color_xrgb(32, 32, 32), 0, 0);
+    verts->set(rect.x2, rect.y2, color.get(), 0, 0);
     verts++;
-    verts->set(R.x2, R.y1, color_xrgb(32, 32, 32), 0, 0);
+    verts->set(rect.x2, rect.y1, color.get(), 0, 0);
     verts++;
-    RCache.Vertex.Unlock(4, m_Geom->vb_stride);
+    RCache.Vertex.Unlock(4, geom->vb_stride);
 
-    RCache.set_Element(m_Shader->E[0]);
-    RCache.set_Geometry(m_Geom);
+    RCache.set_Element(shader->E[0]);
+    RCache.set_Geometry(geom);
 
     RCache.Render(D3DPT_TRIANGLELIST, vOffset, 0, 4, 0, 2);
+}
+#endif // !USE_DX9
+
+void dxConsoleRender::OnRender(bool bGame)
+{
+    D3DRECT rect = { 0, 0, static_cast<LONG>(Device.dwWidth), static_cast<LONG>(Device.dwHeight) };
+    if (bGame)
+        rect.y2 /= 2;
+
+#ifndef USE_DX9
+    ClearWithShader(rect, color_xrgb(32, 32, 32), m_Geom, m_Shader);
 #else //	USE_DX10
-    CHK_DX(HW.pDevice->Clear(1, &R, D3DCLEAR_TARGET, color_xrgb(32, 32, 32), 1, 0));
+    CHK_DX(HW.pDevice->Clear(1, &rect, D3DCLEAR_TARGET, color_xrgb(32, 32, 32), 1, 0));
 #endif //	USE_DX10
 }
