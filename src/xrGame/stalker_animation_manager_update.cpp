OpenXRay/xray-16

commit 78cfe2aa1aa1c6649ffe5dab9dc4b3e1fccff3b5

CStalkerAnimationManager: The infamous 'error in stalker with visual' related issues can be averted by resetting

src/xrGame/stalker_animation_manager_update.cpp
@@ -237,8 +237,13 @@ void CStalkerAnimationManager::update						()
 		update_impl			();
 	}
 	catch(...) {
-		Msg					("! error in stalker with visual %s",*object().cNameVisual());
-		throw;
+		Msg("! error in stalker with visual %s and ID %s",*object().cNameVisual(),object().ID());
+		head().reset();
+        torso().reset();
+        legs().reset();
+        global().reset();
+		return;
+		//throw;
 	}
 	STOP_PROFILE
 }

commit 1579d1fdba9a9be9163f2932d41e82d1cf811b37

xrGame/stalker_animation_manager_update.cpp: fixed wrong printf argument (#654)
Wrong argument leads to a crash at least on VC++ CRT. The bug was
introduced in 78cfe2aa1aa1c6649ffe5dab9dc4b3e1fccff3b5.

src/xrGame/stalker_animation_manager_update.cpp
@@ -228,7 +228,7 @@ void CStalkerAnimationManager::update()
     }
     catch (...)
     {
-        Msg("! error in stalker with visual %s and ID %s", *object().cNameVisual(), object().ID());
+        Msg("! error in stalker with visual %s and ID %u", *object().cNameVisual(), object().ID());
         head().reset();
         torso().reset();
         legs().reset();