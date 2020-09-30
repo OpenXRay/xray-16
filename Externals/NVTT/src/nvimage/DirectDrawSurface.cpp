0aecb65b399e8536e9bc2c2812d00fcc479dd7dc
Dmitry Tsarevich <dimhotepus@gmail.com>
2020-09-30 22:24:24 +0300

    [format] Fix UB on format <> arg type in printf

--- Externals/NVTT/src/nvimage/DirectDrawSurface.cpp
+++ Externals/NVTT/src/nvimage/DirectDrawSurface.cpp
@@ -1232,12 +1232,12 @@ void DirectDrawSurface::printInfo() const
 	if (header.flags & DDSD_LINEARSIZE) printf("\tDDSD_LINEARSIZE\n");
 	if (header.flags & DDSD_MIPMAPCOUNT) printf("\tDDSD_MIPMAPCOUNT\n");
 
-	printf("Height: %d\n", header.height);
-	printf("Width: %d\n", header.width);
-	printf("Depth: %d\n", header.depth);
-	if (header.flags & DDSD_PITCH) printf("Pitch: %d\n", header.pitch);
-	else if (header.flags & DDSD_LINEARSIZE) printf("Linear size: %d\n", header.pitch);
-	printf("Mipmap count: %d\n", header.mipmapcount);
+	printf("Height: %u\n", header.height);
+	printf("Width: %u\n", header.width);
+	printf("Depth: %u\n", header.depth);
+	if (header.flags & DDSD_PITCH) printf("Pitch: %u\n", header.pitch);
+	else if (header.flags & DDSD_LINEARSIZE) printf("Linear size: %u\n", header.pitch);
+	printf("Mipmap count: %u\n", header.mipmapcount);
 	
 	printf("Pixel Format:\n");
 	printf("\tFlags: 0x%.8X\n", header.pf.flags);
@@ -1267,7 +1267,7 @@ void DirectDrawSurface::printInfo() const
 	}
 	else
 	{
-		printf("\tBit count: %d\n", header.pf.bitcount);
+		printf("\tBit count: %u\n", header.pf.bitcount);
 	}
 	printf("\tRed mask: 0x%.8X\n", header.pf.rmask);
 	printf("\tGreen mask: 0x%.8X\n", header.pf.gmask);
