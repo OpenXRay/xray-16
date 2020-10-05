7e4ced5e180ec4901a3ea54f5e3ef02407774d5f
Xottab-DUTY <xottab-duty@yandex.ru>
2020-10-05 17:22:37 +0500

    Refactoring of the build package script
    Now everything works properly

--- xr_pack_build.cmd
+++ xr_pack_build.cmd
@@ -1,88 +1,71 @@
-md res\bin\
+if %CONFIGURATION%==Debug (
+    if %PLATFORM%==x86 (
+        set PLATFORM=Win32
+        set SHORT_NAME=Dx86
+        goto :START
+    )
+)
 
-if %CONFIGURATION%==Debug if %PLATFORM%==x86 goto :DX86
-if %CONFIGURATION%==Debug if %PLATFORM%==x64 goto :DX64
-if %CONFIGURATION%==Mixed if %PLATFORM%==x86 goto :MX86
-if %CONFIGURATION%==Mixed if %PLATFORM%==x64 goto :MX64
-if %CONFIGURATION%==Release if %PLATFORM%==x86 goto :RX86
-if %CONFIGURATION%==Release if %PLATFORM%==x64 goto :RX64
+if %CONFIGURATION%==Debug (
+    if %PLATFORM%==x64 (
+        set PLATFORM=Win64
+        set SHORT_NAME=Dx64
+        goto :START
+    )
+)
+
+if %CONFIGURATION%==Mixed (
+    if %PLATFORM%==x86 (
+        set PLATFORM=Win32
+        set SHORT_NAME=Mx86
+        goto :START
+    )
+)
+
+if %CONFIGURATION%==Mixed (
+    if %PLATFORM%==x64 (
+        set PLATFORM=Win64
+        set SHORT_NAME=Mx64
+        goto :START
+    )
+)
+
+if %CONFIGURATION%==Release (
+    if %PLATFORM%==x86 (
+        set PLATFORM=Win32
+        set SHORT_NAME=Rx86
+        goto :START
+    )
+)
+
+if %CONFIGURATION%==Release (
+    if %PLATFORM%==x64 (
+        set PLATFORM=Win64
+        set SHORT_NAME=Rx64
+        goto :START
+    )
+)
 
 echo ! Unknown configuration and/or platform
 goto :EOF
 
-:DX86
-cd bin\Win32\Debug
+:START
+md res\bin\
+md res\bin\utils
 call :COPY_ENGINE
-7z a OpenXRay.Dx86.7z .\*
 
-cd ..\bin\Win32\Debug
-call :COPY_SYMBOLS
-7z a Symbols.Dx86.7z .\*
-goto :EOF
-
-:DX64
-cd bin\Win64\Debug
-call :COPY_ENGINE
-7z a OpenXRay.Dx64.7z .\*
-
-cd ..\bin\Win64\Debug
-call :COPY_SYMBOLS
-7z a Symbols.Dx64.7z .\*
-goto :EOF
-
-:MX86
-cd bin\Win32\Mixed
-call :COPY_ENGINE
-7z a OpenXRay.Mx86.7z .\*
-
-cd ..\bin\Win32\Mixed
-call :COPY_SYMBOLS
-7z a Symbols.Mx86.7z .\*
-goto :EOF
-
-:MX64
-cd bin\Win64\Mixed
-call :COPY_ENGINE
-7z a OpenXRay.Mx64.7z .\*
-
-cd ..\bin\Win64\Mixed
-call :COPY_SYMBOLS
-7z a Symbols.Mx64.7z .\*
-goto :EOF
-
-:RX86
-cd bin\Win32\Release
-call :COPY_ENGINE
-7z a OpenXRay.Rx86.7z .\*
-
-cd ..\bin\Win32\Release
-call :COPY_SYMBOLS
-7z a Symbols.Rx86.7z .\*
-goto :EOF
-
-:RX64
-cd bin\Win64\Release
-call :COPY_ENGINE
-7z a OpenXRay.Rx64.7z .\*
-
-cd ..\bin\Win64\Release
-call :COPY_SYMBOLS
-7z a Symbols.Rx64.7z .\*
+cd res
+7z a OpenXRay.%SHORT_NAME%.7z * -xr!.* -xr!*.pdb -x!bin\utils
+7z a Symbols.%SHORT_NAME%.7z bin\*.pdb -i!License.txt -i!README.md -xr!.*
+7z a Utils.%SHORT_NAME%.7z bin\utils\* -i!License.txt -i!README.md -xr!.*
+cd ..
 goto :EOF
 
 :COPY_ENGINE
-copy *.dll ..\..\..\res\bin\
-copy *.exe ..\..\..\res\bin\
-cd ..\..\..\
-copy Licence.txt .\res\
-copy README.md .\res\
-cd res\
-goto :EOF
-
-:COPY_SYMBOLS
-copy *.pdb ..\..\..\res\bin\
-cd ..\..\..\
-copy Licence.txt .\res\
-copy README.md .\res\
-cd res\
+copy bin\%PLATFORM%\%CONFIGURATION%\*.dll res\bin\
+copy bin\%PLATFORM%\%CONFIGURATION%\*.exe res\bin\
+copy bin\%PLATFORM%\%CONFIGURATION%\*.pdb res\bin\
+copy bin\%PLATFORM%\%CONFIGURATION%\utils\* res\bin\utils\
+copy License.txt res\
+copy README.md res\
 goto :EOF
