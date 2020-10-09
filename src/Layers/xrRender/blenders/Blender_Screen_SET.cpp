OpenXRay/xray-16

commit 3f4917f1f7ccccce73322438291e4d55ca7f0981

CBlender_Screen_SET::CompileProgrammed(): fixed textures check and addressing not being done for all cases
Replaced a bunch of ifs with switch

src/Layers/xrRender/blenders/Blender_Screen_SET.cpp
@@ -173,38 +173,35 @@ void CBlender_Screen_SET::CompileFixed(CBlender_Compile& C)
 
 void CBlender_Screen_SET::CompileProgrammed(CBlender_Compile& C)
 {
-    if (oBlend.IDselected == 6)
+    switch (oBlend.IDselected)
     {
+    case 6:
+        // Usually for wallmarks
         C.PassSET_VS("stub_notransform_t");
         C.PassSET_PS("stub_default_ma");
-    }
-    else
-    {
-        if (9 == oBlend.IDselected)
-        {
-            C.PassSET_VS("stub_notransform_t_m4");
-            C.PassSET_PS("stub_default");
-        }
-        else
-        {
-            if ((7 == oBlend.IDselected) || (8 == oBlend.IDselected))
-            {
-                C.PassSET_VS("stub_notransform_t_m2");
-                C.PassSET_PS("stub_default");
-            }
-            else
-            {
-                C.PassSET_VS("stub_notransform_t");
-                C.PassSET_PS("stub_default");
-            }
-        }
 
-        VERIFY2(C.L_textures.size() > 0, "Not enough textures");
-        const u32 stage = C.SampledImage("s_base", "s_base", C.L_textures[0]);
-        if (oClamp.value)
-        {
-            C.i_Address(stage, D3DTADDRESS_CLAMP);
-        }
+    case 9:
+        // 4x R
+        C.PassSET_VS("stub_notransform_t_m4");
+        C.PassSET_PS("stub_default");
+
+    case 7:
+    case 8:
+        // 2x R
+        C.PassSET_VS("stub_notransform_t_m2");
+        C.PassSET_PS("stub_default");
+
+    default:
+        // 1x R
+        C.PassSET_VS("stub_notransform_t");
+        C.PassSET_PS("stub_default");
+    }
+
+    VERIFY2(C.L_textures.size() > 0, "Not enough textures");
+    const u32 stage = C.SampledImage("s_base", "s_base", C.L_textures[0]);
+    if (oClamp.value)
+    {
+        C.i_Address(stage, D3DTADDRESS_CLAMP);
     }
 }
 

commit 9d832539d30b65d028e3656cc31864a8f5ad62e4

xrRender/blenders/Blender_Screen_SET.cpp: fixed switch cases didn't have break
Bug was introduced in 3f4917f1f7ccccce73322438291e4d55ca7f0981

src/Layers/xrRender/blenders/Blender_Screen_SET.cpp
@@ -179,22 +179,26 @@ void CBlender_Screen_SET::CompileProgrammed(CBlender_Compile& C)
         // Usually for wallmarks
         C.PassSET_VS("stub_notransform_t");
         C.PassSET_PS("stub_default_ma");
+        break;
 
     case 9:
         // 4x R
         C.PassSET_VS("stub_notransform_t_m4");
         C.PassSET_PS("stub_default");
+        break;
 
     case 7:
     case 8:
         // 2x R
         C.PassSET_VS("stub_notransform_t_m2");
         C.PassSET_PS("stub_default");
+        break;
 
     default:
         // 1x R
         C.PassSET_VS("stub_notransform_t");
         C.PassSET_PS("stub_default");
+        break;
     }
 
     VERIFY2(C.L_textures.size() > 0, "Not enough textures");