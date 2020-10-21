OpenXRay/xray-16

commit 141609ba14562d3a8dc056d0cb0800a38c5770ab

Moved some R2 and R4 files into xrRender_R2 and resolved the diffs

src/Layers/xrRender_R2/r2_rendertarget_phase_luminance.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 
-#pragma pack(push,4)
+#pragma pack(push, 4)
 struct v_build
 {
     Fvector4 p;
@@ -20,19 +20,20 @@ struct v_filter
 void CRenderTarget::phase_luminance()
 {
     u32 Offset = 0;
-    //	float	eps		= EPS_S;
+#ifdef USE_DX9 // XXX: check why eps is 0 for other renderers
+    float eps = EPS_S;
+#else
     float eps = 0;
+#endif
 
     // Targets
     RCache.set_Stencil(FALSE);
     RCache.set_CullMode(CULL_NONE);
     RCache.set_ColorWriteEnable();
-    RCache.set_Z(FALSE);
-    //CHK_DX									(HW.pDevice->SetRenderState	(D3DRS_ZENABLE,FALSE));
+    RCache.set_Z(false);
 
     // 000: Perform LUM-SAT, pass 0, 256x256 => 64x64
-    u_setrt(rt_LUM_64, 0, 0, 0);
-    //RImplementation.rmNormal();
+    u_setrt(rt_LUM_64, NULL, NULL, NULL);
     {
         float ts = 64;
         float _w = float(BLOOM_size_X);
@@ -50,29 +51,29 @@ void CRenderTarget::phase_luminance()
 
         // Fill vertex buffer
         v_build* pv = (v_build*)RCache.Vertex.Lock(4, g_bloom_build->vb_stride, Offset);
-        pv->p.set(eps, eps, eps, 1.f);
-        pv->uv0.set(a_0.x, a_0.y);
-        pv->uv1.set(a_1.x, a_1.y);
-        pv->uv2.set(a_2.x, a_2.y);
-        pv->uv3.set(a_3.x, a_3.y);
-        pv++;
         pv->p.set(eps, float(ts + eps), eps, 1.f);
         pv->uv0.set(a_0.x, b_0.y);
         pv->uv1.set(a_1.x, b_1.y);
         pv->uv2.set(a_2.x, b_2.y);
         pv->uv3.set(a_3.x, b_3.y);
         pv++;
-        pv->p.set(float(ts + eps), eps, eps, 1.f);
-        pv->uv0.set(b_0.x, a_0.y);
-        pv->uv1.set(b_1.x, a_1.y);
-        pv->uv2.set(b_2.x, a_2.y);
-        pv->uv3.set(b_3.x, a_3.y);
+        pv->p.set(eps, eps, eps, 1.f);
+        pv->uv0.set(a_0.x, a_0.y);
+        pv->uv1.set(a_1.x, a_1.y);
+        pv->uv2.set(a_2.x, a_2.y);
+        pv->uv3.set(a_3.x, a_3.y);
         pv++;
         pv->p.set(float(ts + eps), float(ts + eps), eps, 1.f);
         pv->uv0.set(b_0.x, b_0.y);
         pv->uv1.set(b_1.x, b_1.y);
         pv->uv2.set(b_2.x, b_2.y);
         pv->uv3.set(b_3.x, b_3.y);
+        pv++;
+        pv->p.set(float(ts + eps), eps, eps, 1.f);
+        pv->uv0.set(b_0.x, a_0.y);
+        pv->uv1.set(b_1.x, a_1.y);
+        pv->uv2.set(b_2.x, a_2.y);
+        pv->uv3.set(b_3.x, a_3.y);
         pv++;
         RCache.Vertex.Unlock(4, g_bloom_build->vb_stride);
         RCache.set_Element(s_luminance->E[0]);
@@ -81,8 +82,7 @@ void CRenderTarget::phase_luminance()
     }
 
     // 111: Perform LUM-SAT, pass 1, 64x64 => 8x8
-    u_setrt(rt_LUM_8, 0, 0, 0);
-    //RImplementation.rmNormal();
+    u_setrt(rt_LUM_8, NULL, NULL, NULL);
     {
         // Build filter-kernel
         float _ts = 8;
@@ -98,17 +98,21 @@ void CRenderTarget::phase_luminance()
 
         // Fill vertex buffer
         v_filter* pv = (v_filter*)RCache.Vertex.Lock(4, g_bloom_filter->vb_stride, Offset);
-        pv->p.set(eps, eps, eps, 1.f);
-        for (int t = 0; t < 8; t++) pv->uv[t].set(a[t].x, a[t].y, a[t + 8].y, a[t + 8].x); // xy/yx	- left+up
-        pv++;
         pv->p.set(eps, float(_ts + eps), eps, 1.f);
-        for (int t = 0; t < 8; t++) pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
         pv++;
-        pv->p.set(float(_ts + eps), eps, eps, 1.f);
-        for (int t = 0; t < 8; t++) pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
+        pv->p.set(eps, eps, eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(a[t].x, a[t].y, a[t + 8].y, a[t + 8].x); // xy/yx	- left+up
         pv++;
         pv->p.set(float(_ts + eps), float(_ts + eps), eps, 1.f);
-        for (int t = 0; t < 8; t++) pv->uv[t].set(b[t].x, b[t].y, b[t + 8].y, b[t + 8].x); // xy/yx	- right+down
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(b[t].x, b[t].y, b[t + 8].y, b[t + 8].x); // xy/yx	- right+down
+        pv++;
+        pv->p.set(float(_ts + eps), eps, eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
         pv++;
         RCache.Vertex.Unlock(4, g_bloom_filter->vb_stride);
         RCache.set_Element(s_luminance->E[1]);
@@ -118,8 +122,7 @@ void CRenderTarget::phase_luminance()
 
     // 222: Perform LUM-SAT, pass 2, 8x8 => 1x1
     u32 gpu_id = Device.dwFrame % HW.Caps.iGPUNum;
-    u_setrt(rt_LUM_pool[gpu_id * 2 + 1], 0, 0, 0);
-    //RImplementation.rmNormal();
+    u_setrt(rt_LUM_pool[gpu_id * 2 + 1], NULL, NULL, NULL);
     {
         // Build filter-kernel
         float _ts = 1;
@@ -135,17 +138,21 @@ void CRenderTarget::phase_luminance()
 
         // Fill vertex buffer
         v_filter* pv = (v_filter*)RCache.Vertex.Lock(4, g_bloom_filter->vb_stride, Offset);
-        pv->p.set(eps, eps, eps, 1.f);
-        for (int t = 0; t < 8; t++) pv->uv[t].set(a[t].x, a[t].y, a[t + 8].y, a[t + 8].x); // xy/yx	- left+up
-        pv++;
         pv->p.set(eps, float(_ts + eps), eps, 1.f);
-        for (int t = 0; t < 8; t++) pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
         pv++;
-        pv->p.set(float(_ts + eps), eps, eps, 1.f);
-        for (int t = 0; t < 8; t++) pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
+        pv->p.set(eps, eps, eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(a[t].x, a[t].y, a[t + 8].y, a[t + 8].x); // xy/yx	- left+up
         pv++;
         pv->p.set(float(_ts + eps), float(_ts + eps), eps, 1.f);
-        for (int t = 0; t < 8; t++) pv->uv[t].set(b[t].x, b[t].y, b[t + 8].y, b[t + 8].x); // xy/yx	- right+down
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(b[t].x, b[t].y, b[t + 8].y, b[t + 8].x); // xy/yx	- right+down
+        pv++;
+        pv->p.set(float(_ts + eps), eps, eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
         pv++;
         RCache.Vertex.Unlock(4, g_bloom_filter->vb_stride);
 
@@ -163,6 +170,5 @@ void CRenderTarget::phase_luminance()
     }
 
     // Cleanup states
-    //CHK_DX		(HW.pDevice->SetRenderState(D3DRS_ZENABLE,TRUE));
-    RCache.set_Z(TRUE);
+    RCache.set_Z(true);
 }

src/Layers/xrRenderPC_R2/r2_rendertarget_phase_luminance.cpp
@@ -1,170 +1,0 @@
-#include "stdafx.h"
-
-#pragma pack(push, 4)
-struct v_build
-{
-    Fvector4 p;
-    Fvector2 uv0;
-    Fvector2 uv1;
-    Fvector2 uv2;
-    Fvector2 uv3;
-};
-
-struct v_filter
-{
-    Fvector4 p;
-    Fvector4 uv[8];
-};
-#pragma pack(pop)
-
-void CRenderTarget::phase_luminance()
-{
-    u32 Offset = 0;
-    float eps = EPS_S;
-
-    // Targets
-    RCache.set_Stencil(FALSE);
-    RCache.set_CullMode(CULL_NONE);
-    RCache.set_ColorWriteEnable();
-    RCache.set_Z(false);
-
-    // 000: Perform LUM-SAT, pass 0, 256x256 => 64x64
-    u_setrt(rt_LUM_64, NULL, NULL, NULL);
-    {
-        float ts = 64;
-        float _w = float(BLOOM_size_X);
-        float _h = float(BLOOM_size_Y);
-        Fvector2 one = {2.f / _w, 2.f / _h}; // two, infact
-        Fvector2 half = {1.f / _w, 1.f / _h}; // one, infact
-        Fvector2 a_0 = {half.x + 0, half.y + 0};
-        Fvector2 a_1 = {half.x + one.x, half.y + 0};
-        Fvector2 a_2 = {half.x + 0, half.y + one.y};
-        Fvector2 a_3 = {half.x + one.x, half.y + one.y};
-        Fvector2 b_0 = {1 + a_0.x, 1 + a_0.y};
-        Fvector2 b_1 = {1 + a_1.x, 1 + a_1.y};
-        Fvector2 b_2 = {1 + a_2.x, 1 + a_2.y};
-        Fvector2 b_3 = {1 + a_3.x, 1 + a_3.y};
-
-        // Fill vertex buffer
-        v_build* pv = (v_build*)RCache.Vertex.Lock(4, g_bloom_build->vb_stride, Offset);
-        pv->p.set(eps, float(ts + eps), eps, 1.f);
-        pv->uv0.set(a_0.x, b_0.y);
-        pv->uv1.set(a_1.x, b_1.y);
-        pv->uv2.set(a_2.x, b_2.y);
-        pv->uv3.set(a_3.x, b_3.y);
-        pv++;
-        pv->p.set(eps, eps, eps, 1.f);
-        pv->uv0.set(a_0.x, a_0.y);
-        pv->uv1.set(a_1.x, a_1.y);
-        pv->uv2.set(a_2.x, a_2.y);
-        pv->uv3.set(a_3.x, a_3.y);
-        pv++;
-        pv->p.set(float(ts + eps), float(ts + eps), eps, 1.f);
-        pv->uv0.set(b_0.x, b_0.y);
-        pv->uv1.set(b_1.x, b_1.y);
-        pv->uv2.set(b_2.x, b_2.y);
-        pv->uv3.set(b_3.x, b_3.y);
-        pv++;
-        pv->p.set(float(ts + eps), eps, eps, 1.f);
-        pv->uv0.set(b_0.x, a_0.y);
-        pv->uv1.set(b_1.x, a_1.y);
-        pv->uv2.set(b_2.x, a_2.y);
-        pv->uv3.set(b_3.x, a_3.y);
-        pv++;
-        RCache.Vertex.Unlock(4, g_bloom_build->vb_stride);
-        RCache.set_Element(s_luminance->E[0]);
-        RCache.set_Geometry(g_bloom_build);
-        RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-    }
-
-    // 111: Perform LUM-SAT, pass 1, 64x64 => 8x8
-    u_setrt(rt_LUM_8, NULL, NULL, NULL);
-    {
-        // Build filter-kernel
-        float _ts = 8;
-        float _src = float(64);
-        Fvector2 a[16], b[16];
-        for (int k = 0; k < 16; k++)
-        {
-            int _x = (k * 2 + 1) % 8; // 1,3,5,7
-            int _y = ((k / 4) * 2 + 1); // 1,1,1,1 ~ 3,3,3,3 ~...etc...
-            a[k].set(_x, _y).div(_src);
-            b[k].set(a[k]).add(1);
-        }
-
-        // Fill vertex buffer
-        v_filter* pv = (v_filter*)RCache.Vertex.Lock(4, g_bloom_filter->vb_stride, Offset);
-        pv->p.set(eps, float(_ts + eps), eps, 1.f);
-        for (int t = 0; t < 8; t++)
-            pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
-        pv++;
-        pv->p.set(eps, eps, eps, 1.f);
-        for (int t = 0; t < 8; t++)
-            pv->uv[t].set(a[t].x, a[t].y, a[t + 8].y, a[t + 8].x); // xy/yx	- left+up
-        pv++;
-        pv->p.set(float(_ts + eps), float(_ts + eps), eps, 1.f);
-        for (int t = 0; t < 8; t++)
-            pv->uv[t].set(b[t].x, b[t].y, b[t + 8].y, b[t + 8].x); // xy/yx	- right+down
-        pv++;
-        pv->p.set(float(_ts + eps), eps, eps, 1.f);
-        for (int t = 0; t < 8; t++)
-            pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
-        pv++;
-        RCache.Vertex.Unlock(4, g_bloom_filter->vb_stride);
-        RCache.set_Element(s_luminance->E[1]);
-        RCache.set_Geometry(g_bloom_filter);
-        RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-    }
-
-    // 222: Perform LUM-SAT, pass 2, 8x8 => 1x1
-    u32 gpu_id = Device.dwFrame % HW.Caps.iGPUNum;
-    u_setrt(rt_LUM_pool[gpu_id * 2 + 1], NULL, NULL, NULL);
-    {
-        // Build filter-kernel
-        float _ts = 1;
-        float _src = float(8);
-        Fvector2 a[16], b[16];
-        for (int k = 0; k < 16; k++)
-        {
-            int _x = (k * 2 + 1) % 8; // 1,3,5,7
-            int _y = ((k / 4) * 2 + 1); // 1,1,1,1 ~ 3,3,3,3 ~...etc...
-            a[k].set(_x, _y).div(_src);
-            b[k].set(a[k]).add(1);
-        }
-
-        // Fill vertex buffer
-        v_filter* pv = (v_filter*)RCache.Vertex.Lock(4, g_bloom_filter->vb_stride, Offset);
-        pv->p.set(eps, float(_ts + eps), eps, 1.f);
-        for (int t = 0; t < 8; t++)
-            pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
-        pv++;
-        pv->p.set(eps, eps, eps, 1.f);
-        for (int t = 0; t < 8; t++)
-            pv->uv[t].set(a[t].x, a[t].y, a[t + 8].y, a[t + 8].x); // xy/yx	- left+up
-        pv++;
-        pv->p.set(float(_ts + eps), float(_ts + eps), eps, 1.f);
-        for (int t = 0; t < 8; t++)
-            pv->uv[t].set(b[t].x, b[t].y, b[t + 8].y, b[t + 8].x); // xy/yx	- right+down
-        pv++;
-        pv->p.set(float(_ts + eps), eps, eps, 1.f);
-        for (int t = 0; t < 8; t++)
-            pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
-        pv++;
-        RCache.Vertex.Unlock(4, g_bloom_filter->vb_stride);
-
-        f_luminance_adapt = .9f * f_luminance_adapt + .1f * Device.fTimeDelta * ps_r2_tonemap_adaptation;
-        float amount = ps_r2_ls_flags.test(R2FLAG_TONEMAP) ? ps_r2_tonemap_amount : 0;
-        Fvector3 _none, _full, _result;
-        _none.set(1, 0, 1);
-        _full.set(ps_r2_tonemap_middlegray, 1.f, ps_r2_tonemap_low_lum);
-        _result.lerp(_none, _full, amount);
-
-        RCache.set_Element(s_luminance->E[2]);
-        RCache.set_Geometry(g_bloom_filter);
-        RCache.set_c("MiddleGray", _result.x, _result.y, _result.z, f_luminance_adapt);
-        RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-    }
-
-    // Cleanup states
-    RCache.set_Z(true);
-}

src/Layers/xrRenderPC_GL/gl_rendertarget.h
@@ -239,9 +239,9 @@ public:
     void u_setrt(u32 W, u32 H, GLuint _1, GLuint _2, GLuint _3, GLuint zb);
     void u_calc_tc_noise(Fvector2& p0, Fvector2& p1);
     void u_calc_tc_duality_ss(Fvector2& r0, Fvector2& r1, Fvector2& l0, Fvector2& l1);
-    BOOL u_need_PP();
+    bool u_need_PP();
     bool u_need_CM();
-    BOOL u_DBT_enable(float zMin, float zMax);
+    bool u_DBT_enable(float zMin, float zMax);
     void u_DBT_disable();
 
     void phase_scene_prepare();
@@ -271,7 +271,7 @@ public:
     bool need_to_render_sunshafts();
     bool use_minmax_sm_this_frame();
 
-    BOOL enable_scissor(light* L); // true if intersects near plane
+    bool enable_scissor(light* L); // true if intersects near plane
     void enable_dbt_bounds(light* L);
 
     void disable_aniso();

src/Layers/xrRenderPC_GL/gl_rendertarget_accum_omnipart_geom.cpp
@@ -1,39 +1,0 @@
-#include "stdafx.h"
-#include "../xrRender/du_sphere_part.h"
-
-void CRenderTarget::accum_omnip_geom_create()
-{
-    GLenum dwUsage = GL_STATIC_DRAW;
-
-    // vertices
-    {
-        u32 vCount = DU_SPHERE_PART_NUMVERTEX;
-        u32 vSize = 3 * 4;
-        g_accum_omnip_vb.Create(vCount * vSize);
-        u8* pData = static_cast<u8*>(g_accum_omnip_vb.Map());
-        CopyMemory(pData, du_sphere_part_vertices, vCount * vSize);
-        g_accum_omnip_vb.Unmap(true);
-    }
-
-    // Indices
-    {
-        u32 iCount = DU_SPHERE_PART_NUMFACES * 3;
-        g_accum_omnip_ib.Create(iCount * 2);
-        u8* pData = static_cast<u8*>(g_accum_omnip_ib.Map());
-        CopyMemory(pData, du_sphere_part_faces, iCount * 2);
-        g_accum_omnip_ib.Unmap(true); // upload index data
-    }
-}
-
-void CRenderTarget::accum_omnip_geom_destroy()
-{
-#ifdef DEBUG
-    _SHOW_REF("g_accum_omnip_ib", &g_accum_omnip_ib);
-#endif
-    g_accum_omnip_ib.Release();
-
-#ifdef DEBUG
-    _SHOW_REF("g_accum_omnip_vb", &g_accum_omnip_vb);
-#endif
-    g_accum_omnip_vb.Release();
-}

src/Layers/xrRenderPC_GL/gl_rendertarget_accum_point_geom.cpp
@@ -1,319 +1,0 @@
-#include "stdafx.h"
-
-#include "../xrRender/du_sphere.h"
-
-/*
-Fvector du_sphere_vertices[DU_SPHERE_NUMVERTEX]=
-{
-	0.0000f,	1.0000f, 	0.0000f,
-		0.8944f,	0.4472f, 	0.0000f,
-		0.2764f,	0.4472f, 	0.8507f,
-		-0.7236f,	0.4472f, 	0.5257f,
-		-0.7236f,	0.4472f, 	-0.5257f,
-		0.2764f,	0.4472f, 	-0.8507f,
-		0.7236f,	-0.4472f,	0.5257f,
-		-0.2764f,	-0.4472f,	0.8507f,
-		-0.8944f,	-0.4472f,	-0.0000f,
-		-0.2764f,	-0.4472f,	-0.8507f,
-		0.7236f,	-0.4472f,	-0.5257f,
-		0.0000f,	-1.0000f,	0.0000f,
-		0.3607f,	0.9327f, 	0.0000f,
-		0.6729f,	0.7397f, 	0.0000f,
-		0.1115f,	0.9327f, 	0.3431f,
-		0.2079f,	0.7397f, 	0.6399f,
-		-0.2918f,	0.9327f, 	0.2120f,
-		-0.5444f,	0.7397f, 	0.3955f,
-		-0.2918f,	0.9327f, 	-0.2120f,
-		-0.5444f,	0.7397f, 	-0.3955f,
-		0.1115f,	0.9327f, 	-0.3431f,
-		0.2079f,	0.7397f, 	-0.6399f,
-		0.7844f,	0.5168f, 	0.3431f,
-		0.5687f,	0.5168f, 	0.6399f,
-		-0.0839f,	0.5168f, 	0.8520f,
-		-0.4329f,	0.5168f, 	0.7386f,
-		-0.8362f,	0.5168f, 	0.1835f,
-		-0.8362f,	0.5168f, 	-0.1835f,
-		-0.4329f,	0.5168f, 	-0.7386f,
-		-0.0839f,	0.5168f, 	-0.8520f,
-		0.5687f,	0.5168f, 	-0.6399f,
-		0.7844f,	0.5168f, 	-0.3431f,
-		0.9647f,	0.1561f, 	0.2120f,
-		0.9051f,	-0.1561f,	0.3955f,
-		0.0965f,	0.1561f, 	0.9830f,
-		-0.0965f,	-0.1561f,	0.9830f,
-		-0.9051f,	0.1561f, 	0.3955f,
-		-0.9647f,	-0.1561f,	0.2120f,
-		-0.6558f,	0.1561f, 	-0.7386f,
-		-0.4998f,	-0.1561f,	-0.8520f,
-		0.4998f,	0.1561f, 	-0.8520f,
-		0.6558f,	-0.1561f,	-0.7386f,
-		0.9647f,	0.1561f, 	-0.2120f,
-		0.9051f,	-0.1561f,	-0.3955f,
-		0.4998f,	0.1561f, 	0.8520f,
-		0.6558f,	-0.1561f,	0.7386f,
-		-0.6558f,	0.1561f, 	0.7386f,
-		-0.4998f,	-0.1561f,	0.8520f,
-		-0.9051f,	0.1561f, 	-0.3955f,
-		-0.9647f,	-0.1561f,	-0.2120f,
-		0.0965f,	0.1561f, 	-0.9830f,
-		-0.0965f,	-0.1561f,	-0.9830f,
-		0.4329f,	-0.5168f,	0.7386f,
-		0.0839f,	-0.5168f,	0.8520f,
-		-0.5687f,	-0.5168f,	0.6399f,
-		-0.7844f,	-0.5168f,	0.3431f,
-		-0.7844f,	-0.5168f,	-0.3431f,
-		-0.5687f,	-0.5168f,	-0.6399f,
-		0.0839f,	-0.5168f,	-0.8520f,
-		0.4329f,	-0.5168f,	-0.7386f,
-		0.8362f,	-0.5168f,	-0.1835f,
-		0.8362f,	-0.5168f,	0.1835f,
-		0.2918f,	-0.9327f,	0.2120f,
-		0.5444f,	-0.7397f,	0.3955f,
-		-0.1115f,	-0.9327f,	0.3431f,
-		-0.2079f,	-0.7397f,	0.6399f,
-		-0.3607f,	-0.9327f,	-0.0000f,
-		-0.6729f,	-0.7397f,	-0.0000f,
-		-0.1115f,	-0.9327f,	-0.3431f,
-		-0.2079f,	-0.7397f,	-0.6399f,
-		0.2918f,   	-0.9327f,	-0.2120f,
-		0.5444f,	-0.7397f,	-0.3955f,
-		0.4795f,	0.8054f,   	0.3484f,
-		-0.1832f,	0.8054f,   	0.5637f,
-		-0.5927f,	0.8054f,   	-0.0000f,
-		-0.1832f,	0.8054f,   	-0.5637f,
-		0.4795f,	0.8054f,   	-0.3484f,
-		0.9855f,	-0.1699f,	0.0000f,
-		0.3045f,	-0.1699f,	0.9372f,
-		-0.7973f,	-0.1699f,	0.5792f,
-		-0.7973f,	-0.1699f,	-0.5792f,
-		0.3045f,	-0.1699f,	-0.9372f,
-		0.7973f,	0.1699f,   	0.5792f,
-		-0.3045f,	0.1699f,   	0.9372f,
-		-0.9855f,	0.1699f,   	-0.0000f,
-		-0.3045f,	0.1699f,   	-0.9372f,
-		0.7973f,	0.1699f,   	-0.5792f,
-		0.1832f,	-0.8054f,	0.5637f,
-		-0.4795f,	-0.8054f,	0.3484f,
-		-0.4795f,	-0.8054f,	-0.3484f,
-		0.1832f,	-0.8054f,	-0.5637f,
-		0.5927f,	-0.8054f,	0.0000
-};
-
-u16 du_sphere_faces[DU_SPHERE_NUMFACES*3]={
-	14,	12,    0,
-		72,   13,   12,
-		14,   72,   12,
-		15,   72,   14,
-		22,    1,   13,
-		72,   22,   13,
-		23,   22,   72,
-		15,   23,   72,
-		2,   23,   15,
-		16,   14,    0,
-		73,   15,   14,
-		16,   73,   14,
-		17,   73,   16,
-		24,    2,   15,
-		73,   24,   15,
-		25,   24,   73,
-		17,   25,   73,
-		3,   25,   17,
-		18,   16,    0,
-		74,   17,   16,
-		18,   74,   16,
-		19,   74,   18,
-		26,    3,   17,
-		74,   26,   17,
-		27,   26,   74,
-		19,   27,   74,
-		4,   27,   19,
-		20,   18,    0,
-		75,   19,   18,
-		20,   75,   18,
-		21,   75,   20,
-		28,    4,   19,
-		75,   28,   19,
-		29,   28,   75,
-		21,   29,   75,
-		5,   29,   21,
-		12,   20,    0,
-		76,   21,   20,
-		12,   76,   20,
-		13,   76,   12,
-		30,    5,   21,
-		76,   30,   21,
-		31,   30,   76,
-		13,   31,   76,
-		1,   31,   13,
-		32,   42,    1,
-		77,   43,   42,
-		32,   77,   42,
-		33,   77,   32,
-		60,   10,   43,
-		77,   60,   43,
-		61,   60,   77,
-		33,   61,   77,
-		6,   61,   33,
-		34,   44,    2,
-		78,   45,   44,
-		34,   78,   44,
-		35,   78,   34,
-		52,    6,   45,
-		78,   52,   45,
-		53,   52,   78,
-		35,   53,   78,
-		7,   53,   35,
-		36,   46,    3,
-		79,   47,   46,
-		36,   79,   46,
-		37,   79,   36,
-		54,    7,   47,
-		79,   54,   47,
-		55,   54,   79,
-		37,   55,   79,
-		8,   55,   37,
-		38,   48,    4,
-		80,   49,   48,
-		38,   80,   48,
-		39,   80,   38,
-		56,    8,   49,
-		80,   56,   49,
-		57,   56,   80,
-		39,   57,   80,
-		9,   57,   39,
-		40,   50,    5,
-		81,   51,   50,
-		40,   81,   50,
-		41,   81,   40,
-		58,    9,   51,
-		81,   58,   51,
-		59,   58,   81,
-		41,   59,   81,
-		10,   59,   41,
-		33,   45,    6,
-		82,   44,   45,
-		33,   82,   45,
-		32,   82,   33,
-		23,    2,   44,
-		82,   23,   44,
-		22,   23,   82,
-		32,   22,   82,
-		1,   22,   32,
-		35,   47,    7,
-		83,   46,   47,
-		35,   83,   47,
-		34,   83,   35,
-		25,    3,   46,
-		83,   25,   46,
-		24,   25,   83,
-		34,   24,   83,
-		2,   24,   34,
-		37,   49,    8,
-		84,   48,   49,
-		37,   84,   49,
-		36,   84,   37,
-		27,    4,   48,
-		84,   27,   48,
-		26,   27,   84,
-		36,   26,   84,
-		3,   26,   36,
-		39,   51,    9,
-		85,   50,   51,
-		39,   85,   51,
-		38,   85,   39,
-		29,    5,   50,
-		85,   29,   50,
-		28,   29,   85,
-		38,   28,   85,
-		4,   28,   38,
-		41,   43,   10,
-		86,   42,   43,
-		41,   86,   43,
-		40,   86,   41,
-		31,    1,   42,
-		86,   31,   42,
-		30,   31,   86,
-		40,   30,   86,
-		5,   30,   40,
-		62,   64,   11,
-		87,   65,   64,
-		62,   87,   64,
-		63,   87,   62,
-		53,    7,   65,
-		87,   53,   65,
-		52,   53,   87,
-		63,   52,   87,
-		6,   52,   63,
-		64,   66,   11,
-		88,   67,   66,
-		64,   88,   66,
-		65,   88,   64,
-		55,    8,   67,
-		88,   55,   67,
-		54,   55,   88,
-		65,   54,   88,
-		7,   54,   65,
-		66,   68,   11,
-		89,   69,   68,
-		66,   89,   68,
-		67,   89,   66,
-		57,    9,   69,
-		89,   57,   69,
-		56,   57,   89,
-		67,   56,   89,
-		8,   56,   67,
-		68,   70,   11,
-		90,   71,   70,
-		68,   90,   70,
-		69,   90,   68,
-		59,   10,   71,
-		90,   59,   71,
-		58,   59,   90,
-		69,   58,   90,
-		9,   58,   69,
-		70,   62,   11,
-		91,   63,   62,
-		70,   91,   62,
-		71,   91,   70,
-		61,    6,   63,
-		91,   61,   63,
-		60,   61,   91,
-		71,   60,   91,
-		10,   60,   71,
-};
-*/
-
-void CRenderTarget::accum_point_geom_create()
-{
-    // vertices
-    {
-        u32 vCount = DU_SPHERE_NUMVERTEX;
-        u32 vSize = 3 * 4;
-        g_accum_point_vb.Create(vCount * vSize);
-        u8* pData = static_cast<u8*>(g_accum_point_vb.Map());
-        CopyMemory(pData, du_sphere_vertices, vCount * vSize);
-        g_accum_point_vb.Unmap(true);
-    }
-
-    // Indices
-    {
-        u32 iCount = DU_SPHERE_NUMFACES * 3;
-        g_accum_point_ib.Create(iCount * 2);
-        u8* pData = static_cast<u8*>(g_accum_point_ib.Map());
-        CopyMemory(pData, du_sphere_faces, iCount * 2);
-        g_accum_point_ib.Unmap(true);
-    }
-}
-
-void CRenderTarget::accum_point_geom_destroy()
-{
-#ifdef DEBUG
-    _SHOW_REF("g_accum_point_ib", &g_accum_point_ib);
-#endif
-    g_accum_point_ib.Release();
-
-#ifdef DEBUG
-    _SHOW_REF("g_accum_point_vb", &g_accum_point_vb);
-#endif
-    g_accum_point_vb.Release();
-}

commit 1052b75bc94d4fff83bc642101c0aaf72248c98b

Fixed broken luminance on OpenGL render
Was broken in 141609ba14562d3a8dc056d0cb0800a38c5770ab

src/Layers/xrRender_R2/r2_rendertarget_phase_luminance.cpp
@@ -33,7 +33,7 @@ void CRenderTarget::phase_luminance()
     RCache.set_Z(false);
 
     // 000: Perform LUM-SAT, pass 0, 256x256 => 64x64
-    u_setrt(rt_LUM_64, NULL, NULL, NULL);
+    u_setrt(rt_LUM_64, 0, 0, 0);
     {
         float ts = 64;
         float _w = float(BLOOM_size_X);
@@ -51,6 +51,32 @@ void CRenderTarget::phase_luminance()
 
         // Fill vertex buffer
         v_build* pv = (v_build*)RCache.Vertex.Lock(4, g_bloom_build->vb_stride, Offset);
+#ifdef USE_OGL
+        pv->p.set(eps, eps, eps, 1.f);
+        pv->uv0.set(a_0.x, a_0.y);
+        pv->uv1.set(a_1.x, a_1.y);
+        pv->uv2.set(a_2.x, a_2.y);
+        pv->uv3.set(a_3.x, a_3.y);
+        pv++;
+        pv->p.set(eps, float(ts + eps), eps, 1.f);
+        pv->uv0.set(a_0.x, b_0.y);
+        pv->uv1.set(a_1.x, b_1.y);
+        pv->uv2.set(a_2.x, b_2.y);
+        pv->uv3.set(a_3.x, b_3.y);
+        pv++;
+        pv->p.set(float(ts + eps), eps, eps, 1.f);
+        pv->uv0.set(b_0.x, a_0.y);
+        pv->uv1.set(b_1.x, a_1.y);
+        pv->uv2.set(b_2.x, a_2.y);
+        pv->uv3.set(b_3.x, a_3.y);
+        pv++;
+        pv->p.set(float(ts + eps), float(ts + eps), eps, 1.f);
+        pv->uv0.set(b_0.x, b_0.y);
+        pv->uv1.set(b_1.x, b_1.y);
+        pv->uv2.set(b_2.x, b_2.y);
+        pv->uv3.set(b_3.x, b_3.y);
+        pv++;
+#else
         pv->p.set(eps, float(ts + eps), eps, 1.f);
         pv->uv0.set(a_0.x, b_0.y);
         pv->uv1.set(a_1.x, b_1.y);
@@ -75,6 +101,7 @@ void CRenderTarget::phase_luminance()
         pv->uv2.set(b_2.x, a_2.y);
         pv->uv3.set(b_3.x, a_3.y);
         pv++;
+#endif // USE_OGL
         RCache.Vertex.Unlock(4, g_bloom_build->vb_stride);
         RCache.set_Element(s_luminance->E[0]);
         RCache.set_Geometry(g_bloom_build);
@@ -82,7 +109,7 @@ void CRenderTarget::phase_luminance()
     }
 
     // 111: Perform LUM-SAT, pass 1, 64x64 => 8x8
-    u_setrt(rt_LUM_8, NULL, NULL, NULL);
+    u_setrt(rt_LUM_8, 0, 0, 0);
     {
         // Build filter-kernel
         float _ts = 8;
@@ -98,6 +125,24 @@ void CRenderTarget::phase_luminance()
 
         // Fill vertex buffer
         v_filter* pv = (v_filter*)RCache.Vertex.Lock(4, g_bloom_filter->vb_stride, Offset);
+#ifdef USE_OGL
+        pv->p.set(eps, eps, eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(a[t].x, a[t].y, a[t + 8].y, a[t + 8].x); // xy/yx	- left+up
+        pv++;
+        pv->p.set(eps, float(_ts + eps), eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
+        pv++;
+        pv->p.set(float(_ts + eps), eps, eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
+        pv++;
+        pv->p.set(float(_ts + eps), float(_ts + eps), eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(b[t].x, b[t].y, b[t + 8].y, b[t + 8].x); // xy/yx	- right+down
+        pv++;
+#else
         pv->p.set(eps, float(_ts + eps), eps, 1.f);
         for (int t = 0; t < 8; t++)
             pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
@@ -114,6 +159,7 @@ void CRenderTarget::phase_luminance()
         for (int t = 0; t < 8; t++)
             pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
         pv++;
+#endif // USE_OGL
         RCache.Vertex.Unlock(4, g_bloom_filter->vb_stride);
         RCache.set_Element(s_luminance->E[1]);
         RCache.set_Geometry(g_bloom_filter);
@@ -122,7 +168,7 @@ void CRenderTarget::phase_luminance()
 
     // 222: Perform LUM-SAT, pass 2, 8x8 => 1x1
     u32 gpu_id = Device.dwFrame % HW.Caps.iGPUNum;
-    u_setrt(rt_LUM_pool[gpu_id * 2 + 1], NULL, NULL, NULL);
+    u_setrt(rt_LUM_pool[gpu_id * 2 + 1], 0, 0, 0);
     {
         // Build filter-kernel
         float _ts = 1;
@@ -138,6 +184,24 @@ void CRenderTarget::phase_luminance()
 
         // Fill vertex buffer
         v_filter* pv = (v_filter*)RCache.Vertex.Lock(4, g_bloom_filter->vb_stride, Offset);
+#ifdef USE_OGL
+        pv->p.set(eps, eps, eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(a[t].x, a[t].y, a[t + 8].y, a[t + 8].x); // xy/yx	- left+up
+        pv++;
+        pv->p.set(eps, float(_ts + eps), eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
+        pv++;
+        pv->p.set(float(_ts + eps), eps, eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
+        pv++;
+        pv->p.set(float(_ts + eps), float(_ts + eps), eps, 1.f);
+        for (int t = 0; t < 8; t++)
+            pv->uv[t].set(b[t].x, b[t].y, b[t + 8].y, b[t + 8].x); // xy/yx	- right+down
+        pv++;
+#else
         pv->p.set(eps, float(_ts + eps), eps, 1.f);
         for (int t = 0; t < 8; t++)
             pv->uv[t].set(a[t].x, b[t].y, b[t + 8].y, a[t + 8].x); // xy/yx	- left+down
@@ -154,6 +218,7 @@ void CRenderTarget::phase_luminance()
         for (int t = 0; t < 8; t++)
             pv->uv[t].set(b[t].x, a[t].y, a[t + 8].y, b[t + 8].x); // xy/yx	- right+up
         pv++;
+#endif // USE_OGL
         RCache.Vertex.Unlock(4, g_bloom_filter->vb_stride);
 
         f_luminance_adapt = .9f * f_luminance_adapt + .1f * Device.fTimeDelta * ps_r2_tonemap_adaptation;