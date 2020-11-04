OpenXRay/xray-16

commit d65f87c485688b0bb4fc4654fc983a4728009d57

xrGame/string_table.cpp: insert unknown actions as is
Now, if engine will find any unknown action (eg.
$$ACTION_SOME_UNKNOWN_ACTION$$) in the localization it will insert it as
is.

Addition to 667c81d053b1a2aa0c5aa7e8e13dbc92a55a4c33 and #639

src/xrGame/string_table.cpp
@@ -221,8 +221,15 @@ STRING_VALUE CStringTable::ParseLine(LPCSTR str, LPCSTR skey, bool bFirst)
 
         strncpy_s(srcbuff, b + LEN, len);
         srcbuff[len] = 0;
-        GetActionAllBinding(srcbuff, buff, sizeof(buff));
-        res.append(buff, xr_strlen(buff));
+        if (ActionNameToPtr(srcbuff)) // if exist, get bindings
+        {
+            VERIFY(GetActionAllBinding(srcbuff, buff, sizeof(buff)));
+            res.append(buff, xr_strlen(buff));
+        }
+        else // doesn't exist, insert as is
+        {
+            res.append(b, LEN + len + 2);
+        }
 
         k = (int)(b - str);
         k += len;

commit 2febb0b57fbfb6e18ac6fc2883f717b72b5a819b

xrGame/string_table: fixed missing text about key binded to action
Bug was introduced in d65f87c485688b0bb4fc4654fc983a4728009d57

src/xrGame/string_table.cpp
@@ -225,7 +225,9 @@ STRING_VALUE CStringTable::ParseLine(LPCSTR str, LPCSTR skey, bool bFirst)
         srcbuff[len] = 0;
         if (ActionNameToPtr(srcbuff)) // if exist, get bindings
         {
-            VERIFY(GetActionAllBinding(srcbuff, buff, sizeof(buff)));
+            [[maybe_unused]] const bool result =
+                GetActionAllBinding(srcbuff, buff, sizeof(buff));
+            VERIFY(result);
             res.append(buff, xr_strlen(buff));
         }
         else // doesn't exist, insert as is