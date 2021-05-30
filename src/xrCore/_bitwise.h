daed13f56ba88a6ab0d049275d8028adbca548c5
Xottab-DUTY <xottab-duty@yandex.ru>
2021-05-30 17:31:10 +0300

    xrCore/_bitwise.h: swapped x64 and x86 specific code
    Now x86 is a specific case and 'else' case will be used for all other
    architectures, making this code robust.
    
    Thanks @vTurbine for spotting this.

--- src/xrCore/_bitwise.h
+++ src/xrCore/_bitwise.h
@@ -19,16 +19,16 @@
 #define fdRLE10 0x03ede5bdb // 1/ln10
 
 // integer math on floats
-#ifdef _M_AMD64
-IC bool negative(const float f) { return std::signbit(f); }
-IC bool positive(const float f) { return !negative(f); }
-IC void set_negative(float& f) { f = -fabsf(f); }
-IC void set_positive(float& f) { f = fabsf(f); }
-#else
+#ifdef XR_ARCHITECTURE_X86
 IC bool negative(const float& f) { return *(unsigned*)&f & fdSGN; }
 IC bool positive(const float& f) { return (*(unsigned*)&f & fdSGN) == 0; }
 IC void set_negative(float& f) { *(unsigned*)&f |= fdSGN; }
 IC void set_positive(float& f) { *(unsigned*)&f &= ~fdSGN; }
+#else
+IC bool negative(const float f) { return std::signbit(f); }
+IC bool positive(const float f) { return !negative(f); }
+IC void set_negative(float& f) { f = -fabsf(f); }
+IC void set_positive(float& f) { f = fabsf(f); }
 #endif
 
 /*
