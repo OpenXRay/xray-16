b3ef0b08576720ba13e2407d8ac19863f559ff41
Chugunov Roman <Zebs-BMK@yandex.ru>
2021-06-01 13:13:33 +0300

    fix: check luabind submodule existing. Close #812

--- Externals/submodule_check.hpp
+++ Externals/submodule_check.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#if !__has_include(<luabind/luabind.hpp>)
+#if !__has_include(<luabind/luabind/luabind.hpp>)
 #error "You probably have downloaded OpenXRay sources as ZIP archive, you can't do that. Use git to clone the repository.\
     Read the build instructions: https://github.com/OpenXRay/xray-16/wiki"
 #endif
