363cc842474e255d1e22b17373b6083979244e6f
Степан <stepadunaevskij@gmail.com>
2021-07-07 08:30:39 +0400

    реформат

--- src/xrSound/SoundRender_CoreD.h
+++ src/xrSound/SoundRender_CoreD.h
@@ -5,50 +5,60 @@
 #include "SoundRender_Core.h"
 
 #pragma warning(push)
-#pragma warning(disable:4995)
+#pragma warning(disable : 4995)
 #include <dsound.h>
 #pragma warning(pop)
 
-#define _RELEASE(x)		{ if(x) { (x)->Release();       (x)=NULL; } }
+#define _RELEASE(x)         \
+    {                       \
+        if (x)              \
+        {                   \
+            (x)->Release(); \
+            (x) = NULL;     \
+        }                   \
+    }
 
-class CSoundRender_CoreD: public CSoundRender_Core
+class CSoundRender_CoreD : public CSoundRender_Core
 {
-	typedef CSoundRender_Core inherited;
-	struct SListener 
-	{
-		u32				dwSize;
-		Fvector			vPosition;
-		Fvector			vVelocity;
-		Fvector			vOrientFront; 
-		Fvector			vOrientTop; 
-		float			fDistanceFactor;
-		float			fRolloffFactor;
-		float			fDopplerFactor;
-	};
-    BOOL 				EAXQuerySupport			(const GUID* guid, u32 prop);
-	BOOL 				EAXTestSupport			(BOOL bDeferred);
+    typedef CSoundRender_Core inherited;
+    struct SListener
+    {
+        u32 dwSize;
+        Fvector vPosition;
+        Fvector vVelocity;
+        Fvector vOrientFront;
+        Fvector vOrientTop;
+        float fDistanceFactor;
+        float fRolloffFactor;
+        float fDopplerFactor;
+    };
+    BOOL EAXQuerySupport(const GUID* guid, u32 prop);
+    BOOL EAXTestSupport(BOOL bDeferred);
+
 public:
-	// DSound interface
-	IDirectSound8*				pDevice;		// The device itself
-	IDirectSoundBuffer*			pBuffer;		// The primary buffer (mixer destination)
-	IDirectSound3DListener8*	pListener;
-	LPKSPROPERTYSET				pExtensions;
-	DSCAPS						dsCaps;
-	SListener					Listener;
+    // DSound interface
+    IDirectSound8* pDevice; // The device itself
+    IDirectSoundBuffer* pBuffer; // The primary buffer (mixer destination)
+    IDirectSound3DListener8* pListener;
+    LPKSPROPERTYSET pExtensions;
+    DSCAPS dsCaps;
+    SListener Listener;
+
 private:
-	virtual void			update_listener			(const Fvector& P, const Fvector& D, const Fvector& N, float dt);
-	virtual void			i_eax_set				(const GUID* guid, u32 prop, void* val, u32 sz);
-	virtual void			i_eax_get				(const GUID* guid, u32 prop, void* val, u32 sz);
+    virtual void update_listener(const Fvector& P, const Fvector& D, const Fvector& N, float dt);
+    virtual void i_eax_set(const GUID* guid, u32 prop, void* val, u32 sz);
+    virtual void i_eax_get(const GUID* guid, u32 prop, void* val, u32 sz);
+
 public:
-							CSoundRender_CoreD		();
-    virtual					~CSoundRender_CoreD		();
+    CSoundRender_CoreD();
+    virtual ~CSoundRender_CoreD();
 
-	virtual void			_initialize				( u64 window	);
-	virtual void			_clear					( );
+    virtual void _initialize(u64 window);
+    virtual void _clear();
 
-	virtual void			set_master_volume		( float f		);
+    virtual void set_master_volume(float f);
 
-	virtual const Fvector&	listener_position		( )				{ return Listener.vPosition; }
+    virtual const Fvector& listener_position() { return Listener.vPosition; }
 };
 extern CSoundRender_CoreD* SoundRenderD;
 #endif
--- src/xrSound/SoundRender_TargetD.h
+++ src/xrSound/SoundRender_TargetD.h
@@ -5,37 +5,38 @@
 #include "soundrender_Target.h"
 #include "soundrender_CoreD.h"
 
-class CSoundRender_TargetD: public CSoundRender_Target
+class CSoundRender_TargetD : public CSoundRender_Target
 {
-	typedef CSoundRender_Target	inherited;
+    typedef CSoundRender_Target inherited;
 
-	IDirectSoundBuffer*			pBuffer_base;
-	IDirectSoundBuffer8*		pBuffer;
-	IDirectSound3DBuffer8*		pControl;
+    IDirectSoundBuffer* pBuffer_base;
+    IDirectSoundBuffer8* pBuffer;
+    IDirectSound3DBuffer8* pControl;
 
-	BOOL						bDX7;
+    BOOL bDX7;
 
-	u32							buf_size;		// bytes
-	u32							buf_block;
+    u32 buf_size; // bytes
+    u32 buf_block;
 
-	s32							cache_hw_volume;
-	s32							cache_hw_freq;
-	u32							pos_write;		// bytes
+    s32 cache_hw_volume;
+    s32 cache_hw_freq;
+    u32 pos_write; // bytes
 private:
-	void						fill_block				();
-	u32							calc_interval			(u32 ptr);
+    void fill_block();
+    u32 calc_interval(u32 ptr);
+
 public:
-								CSoundRender_TargetD	();
-	virtual 					~CSoundRender_TargetD	();
+    CSoundRender_TargetD();
+    virtual ~CSoundRender_TargetD();
 
-	virtual BOOL				_initialize				();
-	virtual void				_destroy				();
+    virtual BOOL _initialize();
+    virtual void _destroy();
 
-	virtual void				start					(CSoundRender_Emitter* E);
-	virtual void				render					();
-	virtual void				rewind					();
-	virtual void				stop					();
-	virtual void				update					();
-	virtual void				fill_parameters			();
+    virtual void start(CSoundRender_Emitter* E);
+    virtual void render();
+    virtual void rewind();
+    virtual void stop();
+    virtual void update();
+    virtual void fill_parameters();
 };
 #endif
