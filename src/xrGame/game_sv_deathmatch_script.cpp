bce79a2fa57dd852d8ad3f9a08545d1530d8342b
@Bi_Stupid <64709799+TheGreatGman@users.noreply.github.com>
2021-07-20 23:41:39 +0300

    Cmake-fix for full compilation. Help for Xottab and r-a-sattarov (#850)
    Co-authored-by: Xottab-DUTY <xottab-duty@yandex.ru>

--- src/xrGame/game_sv_deathmatch_script.cpp
+++ src/xrGame/game_sv_deathmatch_script.cpp
@@ -3,27 +3,27 @@
 #include "xrServer_script_macroses.h"
 #include "xrServer.h"
 
-using namespace luabind;
-
 template <typename T>
-struct CWrapperBase : public T, public luabind::wrap_base
+struct CWrapperBase : T, luabind::wrap_base
 {
-    typedef T inherited;
-    typedef CWrapperBase<T> self_type;
-    DEFINE_LUA_WRAPPER_CONST_METHOD_0(type_name, LPCSTR)
+    using inherited = T;
+    using self_type = CWrapperBase<T>;
+    DEFINE_LUA_WRAPPER_CONST_METHOD_0(type_name, pcstr)
     //	DEFINE_LUA_WRAPPER_METHOD_1(Money_SetStart, void, u32)
 };
 
-#pragma optimize("s", on)
-void game_sv_Deathmatch::script_register(lua_State* L)
+SCRIPT_EXPORT(game_sv_Deathmatch, (game_sv_GameState),
 {
-    typedef CWrapperBase<game_sv_Deathmatch> WrapType;
-    module(L)[luabind::class_<game_sv_Deathmatch, WrapType, game_sv_GameState>("game_sv_Deathmatch")
-                  .def(constructor<>())
-                  .def("GetTeamData", &game_sv_Deathmatch::GetTeamData)
+    using WrapType = CWrapperBase<game_sv_Deathmatch>;
+    using namespace luabind;
 
-                  .def("type_name", &WrapType::type_name, &WrapType::type_name_static)
-        //			.def("Money_SetStart",		&CWrapperBase<game_sv_Deathmatch>::Money_SetStart,
-        //&CWrapperBase<game_sv_Deathmatch>::Money_SetStart_static)
+    module(luaState)
+    [
+        class_<game_sv_Deathmatch, game_sv_GameState, default_holder, WrapType>("game_sv_Deathmatch")
+            .def(constructor<>())
+            .def("GetTeamData", &game_sv_Deathmatch::GetTeamData)
+
+            .def("type_name", &game_sv_Deathmatch::type_name, &WrapType::type_name_static)
+        //			.def("Money_SetStart",		&CWrapperBase<game_sv_Deathmatch>::Money_SetStart,		&CWrapperBase<game_sv_Deathmatch>::Money_SetStart_static)
     ];
-}
+});
