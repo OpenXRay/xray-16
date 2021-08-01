OpenXRay/xray-16

commit 685e47dfe57e433b99b5285cd07964a0622b2ca7

Preparations for UI restart (Part II)
Thanks @SkyLoaderr

First part: d47af9c7dcb867bfb66c2820f7170d2bdda5a7bb

src/xrGame/MainMenu.cpp
@@ -145,15 +145,18 @@ CMainMenu::CMainMenu()
     }
 
     Device.seqFrame.Add(this, REG_PRIORITY_LOW - 1000);
+    Device.seqResolutionChanged.Add(this);
 }
 
 CMainMenu::~CMainMenu()
 {
+    Device.seqResolutionChanged.Remove(this);
     Device.seqFrame.Remove(this);
+
     xr_delete(g_btnHint);
     xr_delete(g_statHint);
     xr_delete(m_startDialog);
-    g_pGamePersistent->m_pMainMenu = NULL;
+    g_pGamePersistent->m_pMainMenu = nullptr;
 
 #ifdef WINDOWS
     xr_delete(m_account_mngr);
@@ -756,6 +759,12 @@ void CMainMenu::OnDeviceReset()
 {
     if (IsActive() && g_pGameLevel)
         SetNeedVidRestart();
+}
+
+void CMainMenu::OnScreenResolutionChanged()
+{
+    ReadTextureInfo();
+    ReloadUI();
 }
 
 // -------------------------------------------------------------------------------------------------

src/xrGame/MainMenu.h
@@ -49,7 +49,8 @@ class CMainMenu : public IMainMenu,
                   public pureRender,
                   public CDialogHolder,
                   public CUIWndCallback,
-                  public CDeviceResetNotifier
+                  public CDeviceResetNotifier,
+                  public pureScreenResolutionChanged
 {
     CUIDialogWnd* m_startDialog;
 
@@ -188,6 +189,7 @@ public:
     void Hide_CTMS_Dialog();
     void SetNeedVidRestart();
     virtual void OnDeviceReset();
+    void OnScreenResolutionChanged() override;
     LPCSTR GetGSVer();
 
     bool IsCDKeyIsValid();

src/xrGame/UIGameCustom.h
@@ -114,6 +114,7 @@ public:
     void ShowCrosshair(bool show) { psHUD_Flags.set(HUD_CROSSHAIR_RT, show); }
     bool CrosshairShown() { return !!psHUD_Flags.test(HUD_CROSSHAIR_RT); }
     virtual void HideShownDialogs() {}
+    virtual void ReinitDialogs() {}
     StaticDrawableWrapper* AddCustomStatic(const char* id, bool singleInstance);
     StaticDrawableWrapper* GetCustomStatic(const char* id);
     void RemoveCustomStatic(const char* id);

src/xrGame/UIGameSP.cpp
@@ -45,6 +45,14 @@ void CUIGameSP::HideShownDialogs()
     }
 }
 
+void CUIGameSP::ReinitDialogs()
+{
+    delete_data(TalkMenu);
+    TalkMenu = new CUITalkWnd();
+    delete_data(UIChangeLevelWnd);
+    UIChangeLevelWnd = new CChangeLevelWnd();
+}
+
 void CUIGameSP::SetClGame(game_cl_GameState* g)
 {
     inherited::SetClGame(g);

src/xrGame/UIGameSP.h
@@ -35,7 +35,9 @@ public:
     void ChangeLevel(GameGraph::_GRAPH_ID game_vert_id, u32 level_vert_id, Fvector pos, Fvector ang, Fvector pos2,
         Fvector ang2, bool b, const shared_str& message, bool b_allow_change_level);
 
-    virtual void HideShownDialogs();
+    void HideShownDialogs() override;
+    void ReinitDialogs() override;
+
 #ifdef DEBUG
     virtual void Render();
 #endif

src/xrGame/console_commands.cpp
@@ -1325,6 +1325,9 @@ public:
 
     void Execute(pcstr /*args*/) override
     {
+        Device.seqResolutionChanged.Process();
+        if (g_pGamePersistent && g_pGameLevel && Level().game)
+            CurrentGameUI()->ReinitDialogs();
     }
 };
 

src/xrUICore/Options/UIOptionsManager.h
@@ -21,8 +21,10 @@ public:
     void DoUIRestart();
     void DoSystemRestart();
 
-    bool NeedSystemRestart() { return 0 != (m_restart_flags & e_system_restart); }
-    bool NeedVidRestart() { return 0 != (m_restart_flags & e_vid_restart); }
+    bool NeedSystemRestart() const { return 0 != (m_restart_flags & e_system_restart); }
+    bool NeedVidRestart() const { return 0 != (m_restart_flags & e_vid_restart); }
+    bool NeedSndRestart() const { return 0 != (m_restart_flags & e_snd_restart); }
+    bool NeedUIRestart() const { return 0 != (m_restart_flags & e_ui_restart); }
     void SendMessage2Group(const shared_str& group, const char* message);
 
     void RegisterItem(CUIOptionsItem* item, const shared_str& group);

commit 147a0752f4f5e9483d37a5e549156484d935a2b1

Recreate main menu only after we have exited from it's Lua script (fixes #715)
Restore original logic: recreate in OnFrame function.

Bug was introduced in 685e47dfe57e433b99b5285cd07964a0622b2ca7

src/xrGame/MainMenu.cpp
@@ -571,9 +571,10 @@ void CMainMenu::OnFrame()
     if (IsActive())
     {
         CheckForErrorDlg();
-        if (m_wasForceReloaded)
+        if (m_Flags.test(flNeedUIRestart))
         {
-            m_wasForceReloaded = false;
+            m_Flags.set(flNeedUIRestart, false);
+            ReloadUI();
             m_startDialog->SendMessage(m_startDialog, MAIN_MENU_RELOADED, NULL);
         }
     }
@@ -772,12 +773,9 @@ void CMainMenu::OnDeviceReset()
 
 void CMainMenu::OnUIReset()
 {
-    const bool main_menu_is_active = IsActive();
-    VERIFY2(main_menu_is_active, "Trying to reload main menu while it's inactive. That's unsupported.");
-    if (!main_menu_is_active)
-        return;
-    ReloadUI();
-    m_wasForceReloaded = true;
+    // At this point, we may still be executing Lua script.
+    // It is not safe to recreate the menu here.
+    m_Flags.set(flNeedUIRestart, true);
 }
 
 // -------------------------------------------------------------------------------------------------

src/xrGame/MainMenu.h
@@ -64,6 +64,7 @@ class CMainMenu : public IMainMenu,
         flRestoreCursor = (1 << 5),
         flGameSaveScreenshot = (1 << 6),
         flNeedVidRestart = (1 << 7),
+        flNeedUIRestart = (1 << 8),
     };
     Flags16 m_Flags;
     string_path m_screenshot_name;
@@ -134,7 +135,6 @@ protected:
 
 public:
     u32 m_deactivated_frame;
-    bool m_wasForceReloaded;
     void DestroyInternal(bool bForce) override;
 
     CMainMenu();