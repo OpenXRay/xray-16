f86e084826fd370fbf6b4b89a998071d8e916a92
betstick <61669265+betstick@users.noreply.github.com>
2021-11-21 12:14:57 -0500

    Apply suggestions from code review
    Co-authored-by: Xottab_DUTY <Xottab-DUTY@users.noreply.github.com>

--- src/Layers/xrRender/stats_manager.h
+++ src/Layers/xrRender/stats_manager.h
@@ -51,7 +51,7 @@ private:
 
 u32 get_format_pixel_size(D3DFORMAT format);
 
-#if defined(USE_DX11) || defined(USE_OGL)
+#if defined(USE_DX11)
 u32 get_format_pixel_size(DXGI_FORMAT format);
 #endif
 
