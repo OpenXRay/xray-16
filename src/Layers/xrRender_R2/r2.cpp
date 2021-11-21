10730a51f166a7cdbfe51cdc702c4f05b3479263
betstick <61669265+betstick@users.noreply.github.com>
2021-11-21 12:15:34 -0500

    Update src/Layers/xrRender_R2/r2.cpp
    Co-authored-by: Xottab_DUTY <Xottab-DUTY@users.noreply.github.com>

--- src/Layers/xrRender_R2/r2.cpp
+++ src/Layers/xrRender_R2/r2.cpp
@@ -547,7 +547,7 @@ void CRender::create()
     o.dx10_minmax_sm = ps_r3_minmax_sm;
     o.dx10_minmax_sm_screenarea_threshold = 1600 * 1200;
 
-#if defined(USE_DX9) || defined(USE_DX11)
+#if defined(USE_DX11)
     o.dx11_enable_tessellation =
         HW.FeatureLevel >= D3D_FEATURE_LEVEL_11_0 && ps_r2_ls_flags_ext.test(R2FLAGEXT_ENABLE_TESSELLATION);
 #endif
