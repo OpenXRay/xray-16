584393825dab367796a28990ccd89a5f51efa2c7
Xottab-DUTY <xottab-duty@yandex.ru>
2021-12-16 09:58:21 +0300

    xrSound/SoundRender_Source_loader.cpp: don't show log about missing ogg-comment on MASTER_GOLD

--- src/xrSound/SoundRender_Source_loader.cpp
+++ src/xrSound/SoundRender_Source_loader.cpp
@@ -76,7 +76,7 @@ bool CSoundRender_Source::LoadWave(pcstr pName, bool crashOnError)
 #ifdef DEBUG
     if (ovi->channels == 2)
         Msg("stereo sound source [%s]", pName);
-#endif // #ifdef DEBUG
+#endif
 
     ZeroMemory(&m_wformat, sizeof(WAVEFORMATEX));
 
@@ -122,10 +122,18 @@ bool CSoundRender_Source::LoadWave(pcstr pName, bool crashOnError)
             m_fMaxAIDist = F.r_float();
         }
         else
+        {
+#ifndef MASTER_GOLD
             Log("! Invalid ogg-comment version, file: ", pName);
+#endif
+        }
     }
     else
+    {
+#ifndef MASTER_GOLD
         Log("! Missing ogg-comment, file: ", pName);
+#endif
+    }
 
     R_ASSERT3_CURE(m_fMaxAIDist >= 0.1f && m_fMaxDist >= 0.1f, "Invalid max distance.", pName, !crashOnError,
     {
