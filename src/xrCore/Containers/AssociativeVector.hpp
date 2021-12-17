9ad3a1a231a8d88c18da13040a3eb175a24b4003
Dino8890 <dino8890@protonmail.com>
2021-12-17 15:25:32 +0100

    Added emplace metod to AssociativeVector.

--- src/xrCore/Containers/AssociativeVector.hpp
+++ src/xrCore/Containers/AssociativeVector.hpp
@@ -63,6 +63,7 @@ public:
     IC iterator insert(iterator where, const value_type& value);
     template <class TIterator>
     IC void insert(TIterator first, TIterator last);
+    IC insert_result emplace(key_type key, mapped_type value);
     IC void erase(iterator element);
     IC void erase(iterator first, iterator last);
     IC size_type erase(const key_type& key);
@@ -300,6 +301,11 @@ IC void _associative_vector::insert(TIterator first, TIterator last)
     }
     inherited::insert(end(), first, last);
     std::sort(begin(), end(), static_cast<TComparer&>(*this));
+}
+
+TEMPLATE_SPECIALIZATION
+IC typename _associative_vector::insert_result _associative_vector::emplace(key_type key, mapped_type value) {
+    return insert(std::make_pair(key, value));
 }
 
 TEMPLATE_SPECIALIZATION
