OpenXRay/xray-16

commit ad2270c4916fcb6f7c7ee93c9c277dbce4f22b49

Removed GetKeysBindedTo(), use better function instead

src/xrEngine/XR_IOConsole.h
@@ -6,6 +6,7 @@
 
 #include "Include/xrRender/FactoryPtr.h"
 #include "Include/xrRender/UIShader.h"
+#include "xr_level_controller.h" // XXX: only for bindtypes_count, better to remove
 
 // refs
 class ENGINE_API CGameFont;
@@ -88,7 +89,7 @@ protected:
     bool m_disable_tips;
 
 private:
-    int lastBindedKeys[2];
+    int lastBindedKeys[bindtypes_count];
 
     vecHistory m_cmd_history;
     u32 m_cmd_history_max;

src/xrEngine/XR_IOConsole.cpp
@@ -613,15 +613,14 @@ void CConsole::Show()
     reset_selected_tip();
     update_tips();
 
-    auto [key1, key2] = GetKeysBindedTo(kCONSOLE);
-
-    if (key1 > -1 && key1 < CInput::COUNT_KB_BUTTONS)
-        ec().assign_callback(key1, text_editor::ks_free, Callback(this, &CConsole::Hide_cmd));
-    if (key2 > -1 && key2 < CInput::COUNT_KB_BUTTONS)
-        ec().assign_callback(key2, text_editor::ks_free, Callback(this, &CConsole::Hide_cmd));
-
-    lastBindedKeys[0] = key1;
-    lastBindedKeys[1] = key2;
+    ForAllActionKeys(kCONSOLE, [&](size_t keyboard_index, int key)
+    {
+        if (key < CInput::COUNT_KB_BUTTONS)
+        {
+            ec().assign_callback(key, text_editor::ks_free, Callback(this, &CConsole::Hide_cmd));
+            lastBindedKeys[keyboard_index] = key;
+        }
+    });
 
     m_editor->IR_Capture();
     Device.seqRender.Add(this, 1);
@@ -647,8 +646,12 @@ void CConsole::Hide()
     reset_selected_tip();
     update_tips();
 
-    ec().remove_callback(lastBindedKeys[0]);
-    ec().remove_callback(lastBindedKeys[1]);
+    for (int key : lastBindedKeys)
+    {
+        if (key)
+            ec().remove_callback(key);
+    }
+    ZeroMemory(lastBindedKeys, sizeof(lastBindedKeys));
 
     Device.seqFrame.Remove(this);
     Device.seqRender.Remove(this);

src/xrEngine/line_edit_control.cpp
@@ -259,7 +259,7 @@ void line_edit_control::assign_callback(int const dik, key_state state, Callback
 void line_edit_control::remove_callback(int dik)
 {
     VERIFY(dik < CInput::COUNT_KB_BUTTONS);
-    if (dik > -1 && dik < CInput::COUNT_KB_BUTTONS)
+    if (dik < CInput::COUNT_KB_BUTTONS)
         xr_delete(m_actions[dik]);
 }
 

src/xrEngine/xr_level_controller.cpp
@@ -706,18 +706,6 @@ bool GetActionAllBinding(pcstr action, char* dst_buff, int dst_buff_sz)
     return true;
 }
 
-#pragma todo("Artur to All: Gamepads has own bindings. Add m_keyboard[2]")
-
-std::pair<int, int> GetKeysBindedTo(EGameActions action_id)
-{
-    const key_binding& binding = g_key_bindings[action_id];
-    return
-    {
-        binding.m_keyboard[0] ? binding.m_keyboard[0]->dik : -1,
-        binding.m_keyboard[1] ? binding.m_keyboard[1]->dik : -1
-    };
-}
-
 ConsoleBindCmds g_consoleBindCmds;
 BOOL g_remapped = false;
 

src/xrEngine/xr_level_controller.h
@@ -189,7 +189,27 @@ ENGINE_API EGameActions GetBindedAction(int dik);
 
 ENGINE_API bool GetActionAllBinding(pcstr action, char* dst_buff, int dst_buff_sz);
 
-ENGINE_API std::pair<int, int> GetKeysBindedTo(EGameActions action_id);
+template <typename Invocable>
+void ForAllActionKeys(EGameActions action_id, Invocable&& invocable)
+{
+    for (size_t i = 0; i < bindtypes_count; ++i)
+    {
+        const int key = GetActionDik(action_id, i);
+        if (key == SDL_SCANCODE_UNKNOWN)
+            continue;
+
+        if constexpr (std::is_invocable_r_v<bool, Invocable, size_t, int>)
+        {
+            if (invocable(i, key))
+                break;
+        }
+        else
+        {
+            static_assert(std::is_invocable_v<Invocable, size_t, int>);
+            invocable(i, key);
+        }
+    }
+}
 
 extern ENGINE_API void CCC_RegisterInput();
 

src/xrGame/CameraLook.cpp
@@ -89,13 +89,21 @@ Fvector CCameraLook2::m_cam_offset;
 void CCameraLook2::OnActivate(CCameraBase* old_cam) { CCameraLook::OnActivate(old_cam); }
 void CCameraLook2::Update(Fvector& point, Fvector&)
 {
-    auto [key1, key2] = GetKeysBindedTo(kCAM_AUTOAIM);
-    const bool key1State = pInput->iGetAsyncKeyState(key1);
-    const bool key2State = pInput->iGetAsyncKeyState(key2);
+    bool keyPressed = false;
+    ForAllActionKeys(kCAM_AUTOAIM, [&](size_t /*keyboard_index*/, int key)
+    {
+        if (pInput->iGetAsyncKeyState(key))
+        {
+            keyPressed = true;
+            return true;
+        }
+        return false;
+    });
+
     if (!m_locked_enemy)
     {
         // autoaim
-        if (key1State || key2State)
+        if (keyPressed)
         {
             const CVisualMemoryManager::VISIBLES& vVisibles = Actor()->memory().visual().objects();
             CVisualMemoryManager::VISIBLES::const_iterator v_it = vVisibles.begin();
@@ -125,7 +133,7 @@ void CCameraLook2::Update(Fvector& point, Fvector&)
     }
     else
     {
-        if (!key1State && !key2State)
+        if (!keyPressed)
         {
             m_locked_enemy = NULL;
             //.			Msg				("enemy is NILL");

commit 895dfd8ccbe9ec951cb5f85339b6ab31534fbbd4

Fixed crash on game start Introduced with ad2270c4916fcb6f7c7ee93c9c277dbce4f22b49

src/xrEngine/XR_IOConsole.h
@@ -89,7 +89,7 @@ protected:
     bool m_disable_tips;
 
 private:
-    int lastBindedKeys[bindtypes_count];
+    int lastBindedKeys[bindtypes_count]{};
 
     vecHistory m_cmd_history;
     u32 m_cmd_history_max;