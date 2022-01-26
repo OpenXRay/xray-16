OpenXRay/xray-16

commit 34924178158a198267a312e8cbc73d551d06779a

xrRender_DX9: removed D3DXGetXXXShaderProfile usage Use own implementation instead

src/Layers/xrRender/ShaderResourceTraits.h
@@ -92,9 +92,7 @@ struct ShaderTypeTraits<SVS>
     static inline const char* GetCompilationTarget()
     {
 #ifdef USE_DX9
-#   if RENDER == R_R2
-        return D3DXGetVertexShaderProfile(HW.pDevice); // vertex "vs_2_a";
-#   endif
+        return HW.Caps.geometry_profile;
 #elif defined(USE_DX11)
         switch (HW.FeatureLevel)
         {
@@ -111,10 +109,8 @@ struct ShaderTypeTraits<SVS>
             return "vs_5_0";
         }
 #endif
-        if (HW.Caps.geometry_major >= 2)
-            return "vs_2_0";
-
-        return "vs_1_1";
+        NODEFAULT;
+        return nullptr;
     }
 
     static void GetCompilationTarget(const char*& target, const char*& entry, const char* data)
@@ -191,9 +187,7 @@ struct ShaderTypeTraits<SPS>
     static inline const char* GetCompilationTarget()
     {
 #ifdef USE_DX9
-#   if RENDER == R_R2
-        return D3DXGetPixelShaderProfile(HW.pDevice); // pixel "ps_2_a";
-#   endif
+        return HW.Caps.raster_profile;
 #elif defined(USE_DX11)
         switch (HW.FeatureLevel)
         {
@@ -210,8 +204,8 @@ struct ShaderTypeTraits<SPS>
             return "ps_5_0";
         }
 #endif
-
-        return "ps_2_0";
+        NODEFAULT;
+        return nullptr;
     }
 
     static void GetCompilationTarget(const char*& target, const char*& entry, const char* data)

src/Layers/xrRenderDX10/dx10HWCaps.cpp
@@ -146,6 +146,7 @@ void CHWCaps::Update()
     // ***************** GEOMETRY
     geometry_major = 4;
     geometry_minor = 0;
+    geometry_profile = "vs_4_0";
     geometry.bSoftware = FALSE;
     geometry.bPointSprites = FALSE;
     geometry.bNPatches = FALSE;
@@ -159,6 +160,7 @@ void CHWCaps::Update()
     // ***************** PIXEL processing
     raster_major = 4;
     raster_minor = 0;
+    raster_profile = "ps_4_0";
     // XXX: review this
     raster.dwStages = 15; // Previuos value is 16, but it's out of bounds
     raster.bNonPow2 = TRUE;

src/Layers/xrRender/HWCaps.h
@@ -52,9 +52,11 @@ public:
     // caps itself
     u16 geometry_major;
     u16 geometry_minor;
+    pcstr geometry_profile;
     caps_Geometry geometry;
     u16 raster_major;
     u16 raster_minor;
+    pcstr raster_profile;
     caps_Raster raster;
 
     u32 id_vendor;
@@ -71,7 +73,7 @@ public:
     bool hasFixedPipeline{ false };     // device has support for fixed pipeline
     bool useCombinedSamplers{ false };  // texture and sampler settings represented as single object
 
+public:
     void Update(void);
 };
-
 #endif

src/Layers/xrRenderDX9/dx9HWCaps.cpp
@@ -104,6 +104,64 @@ u32 GetGpuNum() { return 1; }
 #endif
 }
 
+static pcstr GetVertexShaderProfile(const D3DCAPS9& caps)
+{
+    switch (caps.VertexShaderVersion)
+    {
+#if RENDER != R_R1
+    case D3DVS_VERSION(3, 0): return "vs_3_0";
+
+    case D3DVS_VERSION(2, 0):
+        if (caps.VS20Caps.NumTemps >= 13                &&
+            caps.VS20Caps.DynamicFlowControlDepth == 24 &&
+            caps.VS20Caps.Caps & D3DPS20CAPS_PREDICATION)
+        {
+            return "vs_2_a";
+        }
+        [[fallthrough]];
+#endif
+    default:                  return "vs_2_0";
+
+    case D3DVS_VERSION(1, 1): return "vs_1_1";
+    }
+}
+
+static pcstr GetPixelShaderProfile(const D3DCAPS9& caps)
+{
+#if RENDER == R_R1
+    return "ps_2_0";
+#else // R2
+    switch (caps.PixelShaderVersion)
+    {
+    case D3DPS_VERSION(3, 0): return "ps_3_0";
+
+    case D3DPS_VERSION(2, 0):
+        if (caps.PS20Caps.NumTemps >= 22                          &&
+            caps.PS20Caps.Caps & D3DPS20CAPS_ARBITRARYSWIZZLE     &&
+            caps.PS20Caps.Caps & D3DPS20CAPS_GRADIENTINSTRUCTIONS &&
+            caps.PS20Caps.Caps & D3DPS20CAPS_PREDICATION          &&
+            caps.PS20Caps.Caps & D3DPS20CAPS_NODEPENDENTREADLIMIT &&
+            caps.PS20Caps.Caps & D3DPS20CAPS_NOTEXINSTRUCTIONLIMIT)
+        {
+            return "ps_2_a";
+        }
+        if (caps.PS20Caps.NumTemps >= 32                          &&
+            caps.PS20Caps.Caps&D3DPS20CAPS_NOTEXINSTRUCTIONLIMIT)
+        {
+            return "ps_2_b";
+        }
+        [[fallthrough]];
+
+    default:                  return "ps_2_0";
+
+    case D3DPS_VERSION(1, 4): return "ps_1_4";
+    case D3DPS_VERSION(1, 3): return "ps_1_3";
+    case D3DPS_VERSION(1, 2): return "ps_1_2";
+    case D3DPS_VERSION(1, 1): return "ps_1_1";
+    }
+#endif
+}
+
 void CHWCaps::Update()
 {
     D3DCAPS9 caps;
@@ -112,6 +170,7 @@ void CHWCaps::Update()
     // ***************** GEOMETRY
     geometry_major = u16((u32(caps.VertexShaderVersion) & (0xf << 8ul)) >> 8);
     geometry_minor = u16((u32(caps.VertexShaderVersion) & 0xf));
+    geometry_profile = GetVertexShaderProfile(caps);
     geometry.bSoftware = (caps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT) == 0;
     geometry.bPointSprites = FALSE;
     geometry.bNPatches = (caps.DevCaps & D3DDEVCAPS_NPATCHES) != 0;
@@ -125,6 +184,7 @@ void CHWCaps::Update()
     // ***************** PIXEL processing
     raster_major = u16(u32(u32(caps.PixelShaderVersion) & u32(0xf << 8ul)) >> 8);
     raster_minor = u16(u32(u32(caps.PixelShaderVersion) & u32(0xf)));
+    raster_profile = GetPixelShaderProfile(caps);
     raster.dwStages = caps.MaxSimultaneousTextures;
     raster.bNonPow2 = ((caps.TextureCaps & D3DPTEXTURECAPS_NONPOW2CONDITIONAL) != 0) ||
         ((caps.TextureCaps & D3DPTEXTURECAPS_POW2) == 0);

src/Layers/xrRenderGL/glHWCaps.cpp
@@ -17,6 +17,7 @@ void CHWCaps::Update()
     // ***************** GEOMETRY
     geometry_major = 4;
     geometry_minor = 0;
+    geometry_profile = "vs_4_0";
     geometry.bSoftware = FALSE;
     geometry.bPointSprites = FALSE;
     geometry.bNPatches = FALSE;
@@ -32,6 +33,7 @@ void CHWCaps::Update()
     // ***************** PIXEL processing
     raster_major = 4;
     raster_minor = 0;
+    raster_profile = "ps_4_0";
     // XXX: review this
     raster.dwStages = 15; // Previuos value is 16, but it's out of bounds
     raster.bNonPow2 = TRUE;

commit b3118860ec5b5d2f48c3fceaa11da763a45f7690

Fixed OpenGL renderer crash on launch Moved DX11 vertex/pixel shader profile processing to dx10HWCaps, now all renderers will use HWCaps for vertex/pixel profile.
Crash was introduced in 34924178158a198267a312e8cbc73d551d06779a

src/Layers/xrRender/ShaderResourceTraits.h
@@ -91,26 +91,7 @@ struct ShaderTypeTraits<SVS>
 
     static inline const char* GetCompilationTarget()
     {
-#ifdef USE_DX9
         return HW.Caps.geometry_profile;
-#elif defined(USE_DX11)
-        switch (HW.FeatureLevel)
-        {
-        case D3D_FEATURE_LEVEL_10_0:
-            return "vs_4_0";
-        case D3D_FEATURE_LEVEL_10_1:
-            return "vs_4_1";
-        case D3D_FEATURE_LEVEL_11_0:
-        case D3D_FEATURE_LEVEL_11_1:
-#ifdef HAS_DX11_3
-        case D3D_FEATURE_LEVEL_12_0:
-        case D3D_FEATURE_LEVEL_12_1:
-#endif
-            return "vs_5_0";
-        }
-#endif
-        NODEFAULT;
-        return nullptr;
     }
 
     static void GetCompilationTarget(const char*& target, const char*& entry, const char* data)
@@ -186,26 +167,7 @@ struct ShaderTypeTraits<SPS>
 
     static inline const char* GetCompilationTarget()
     {
-#ifdef USE_DX9
         return HW.Caps.raster_profile;
-#elif defined(USE_DX11)
-        switch (HW.FeatureLevel)
-        {
-        case D3D_FEATURE_LEVEL_10_0:
-            return "ps_4_0";
-        case D3D_FEATURE_LEVEL_10_1:
-            return "ps_4_1";
-        case D3D_FEATURE_LEVEL_11_0:
-        case D3D_FEATURE_LEVEL_11_1:
-#ifdef HAS_DX11_3
-        case D3D_FEATURE_LEVEL_12_0:
-        case D3D_FEATURE_LEVEL_12_1:
-#endif
-            return "ps_5_0";
-        }
-#endif
-        NODEFAULT;
-        return nullptr;
     }
 
     static void GetCompilationTarget(const char*& target, const char*& entry, const char* data)

src/Layers/xrRenderDX10/dx10HWCaps.cpp
@@ -144,9 +144,30 @@ u32 GetGpuNum() { return 1; }
 void CHWCaps::Update()
 {
     // ***************** GEOMETRY
-    geometry_major = 4;
-    geometry_minor = 0;
-    geometry_profile = "vs_4_0";
+    switch (HW.FeatureLevel)
+    {
+    case D3D_FEATURE_LEVEL_10_0:
+        geometry_profile = "vs_4_0";
+        geometry_major = 4;
+        geometry_minor = 0;
+        break;
+    case D3D_FEATURE_LEVEL_10_1:
+        geometry_profile = "vs_4_1";
+        geometry_major = 4;
+        geometry_minor = 1;
+        break;
+    case D3D_FEATURE_LEVEL_11_0:
+    case D3D_FEATURE_LEVEL_11_1:
+#ifdef HAS_DX11_3
+    case D3D_FEATURE_LEVEL_12_0:
+    case D3D_FEATURE_LEVEL_12_1:
+#endif
+        geometry_profile = "vs_5_0";
+        geometry_major = 5;
+        geometry_minor = 0;
+        break;
+    default: NODEFAULT;
+    }
     geometry.bSoftware = FALSE;
     geometry.bPointSprites = FALSE;
     geometry.bNPatches = FALSE;
@@ -158,9 +179,30 @@ void CHWCaps::Update()
     geometry.bVTF = TRUE;
 
     // ***************** PIXEL processing
-    raster_major = 4;
-    raster_minor = 0;
-    raster_profile = "ps_4_0";
+    switch (HW.FeatureLevel)
+    {
+    case D3D_FEATURE_LEVEL_10_0:
+        raster_profile = "ps_4_0";
+        raster_major = 4;
+        raster_minor = 0;
+        break;
+    case D3D_FEATURE_LEVEL_10_1:
+        raster_profile = "ps_4_1";
+        raster_major = 4;
+        raster_minor = 1;
+        break;
+    case D3D_FEATURE_LEVEL_11_0:
+    case D3D_FEATURE_LEVEL_11_1:
+#ifdef HAS_DX11_3
+    case D3D_FEATURE_LEVEL_12_0:
+    case D3D_FEATURE_LEVEL_12_1:
+#endif
+        raster_profile = "ps_5_0";
+        raster_major = 5;
+        raster_minor = 0;
+        break;
+    default: NODEFAULT;
+    }
     // XXX: review this
     raster.dwStages = 15; // Previuos value is 16, but it's out of bounds
     raster.bNonPow2 = TRUE;
@@ -178,10 +220,6 @@ void CHWCaps::Update()
         CAP_VERSION(raster_major, raster_minor));
     // *******1********** Vertex cache
     Msg("* GPU vertex cache: %s, %d", "unrecognized", u32(geometry.dwVertexCache));
-
-    // *******1********** Compatibility : vertex shader
-    if (0 == raster_major)
-        geometry_major = 0; // Disable VS if no PS
 
     //
     bTableFog = FALSE; // BOOL	(caps.RasterCaps&D3DPRASTERCAPS_FOGTABLE);