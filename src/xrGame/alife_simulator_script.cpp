9f8d195c279c51f64e783a303cc70469290fef28
Chugunov Roman <Zebs-BMK@yandex.ru>
2022-05-13 21:02:22 +0300

    debug: fix alife simulator script

--- src/xrGame/alife_simulator_script.cpp
+++ src/xrGame/alife_simulator_script.cpp
@@ -361,10 +361,10 @@ CSE_Abstract* try_to_clone_object(CALifeSimulator* self, CSE_Abstract* object, p
     clone->wpn_flags = wpnmag->wpn_flags;
     clone->m_addon_flags = wpnmag->m_addon_flags;
     clone->m_fCondition = wpnmag->m_fCondition;
-    clone->ammo_type.data = wpnmag->ammo_type.data;
+    clone->ammo_type = wpnmag->ammo_type;
     clone->m_upgrades = wpnmag->m_upgrades;
-    clone->a_elapsed.data = wpnmag->a_elapsed.data;
-    clone->a_current_addon.data = wpnmag->a_current_addon.data;
+    clone->a_elapsed = wpnmag->a_elapsed;
+    clone->a_current = wpnmag->a_current;
 
     return bRegister ? reprocess_spawn(self, absClone) : absClone;
 }
