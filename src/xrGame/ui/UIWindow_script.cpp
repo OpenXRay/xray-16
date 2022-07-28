88d447f50f9327096f46044c1cdad0710b266d00
Roman <Zebs-BMK@yandex.ru>
2022-07-28 12:20:08 +0300

    Update src/xrGame/ui/UIWindow_script.cpp
    Co-authored-by: Xottab_DUTY <Xottab-DUTY@users.noreply.github.com>

--- src/xrGame/ui/UIWindow_script.cpp
+++ src/xrGame/ui/UIWindow_script.cpp
@@ -23,8 +23,9 @@ SCRIPT_EXPORT(CDialogHolder, (CUIWindow), {
     [
         class_<CDialogHolder>("CDialogHolder")
             .def("TopInputReceiver", &CDialogHolder::TopInputReceiver)
-            .def("SetMainInputReceiver", &CDialogHolder::SetMainInputReceiver)
             .def("MainInputReceiver", &CDialogHolder::TopInputReceiver)
+            .def("SetTopInputReceiver", &CDialogHolder::SetMainInputReceiver)
+            .def("SetMainInputReceiver", &CDialogHolder::SetMainInputReceiver)
             .def("start_stop_menu", &CDialogHolder::StartStopMenu)
             .def("AddDialogToRender", &CDialogHolder::AddDialogToRender)
             .def("RemoveDialogToRender", &CDialogHolder::RemoveDialogToRender)
