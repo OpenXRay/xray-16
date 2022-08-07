cb8d061c67a887499d009adf13f416f5eee20d88
Xottab_DUTY <xottab-duty@yandex.ru>
2022-08-08 01:09:12 +0500

    Use common r3_rendertarget_create_minmaxSM.cpp for R4 and RGL Also moved r2_rendertarget.cpp in CMakeLists.txt to in correct alphabetical order

--- src/Layers/xrRender_R2/r3_rendertarget_create_minmaxSM.cpp
+++ src/Layers/xrRender_R2/r3_rendertarget_create_minmaxSM.cpp
@@ -17,17 +17,17 @@ void CRenderTarget::create_minmax_SM()
     pv->set(1, 1, d_Z, d_W, C, 1, 0, 0, 0);
     pv++;
     RCache.Vertex.Unlock(4, g_combine_2UV->vb_stride);
-    // u_setrt	(rt_smap_depth_minmax_temp,NULL,NULL,NULL);
-    u_setrt(rt_smap_depth_minmax, NULL, NULL, NULL);
+    //u_setrt	(rt_smap_depth_minmax_temp,NULL,NULL,NULL);
+    u_setrt(rt_smap_depth_minmax, 0, 0, 0);
     RCache.set_Element(s_create_minmax_sm->E[0]);
     RCache.set_Geometry(g_combine_2UV);
-    StateManager.SetStencil(
-        FALSE, D3DCMP_ALWAYS, 0x80, 0xFF, 0x80, D3DSTENCILOP_KEEP, D3DSTENCILOP_REPLACE, D3DSTENCILOP_KEEP);
-    StateManager.SetColorWriteEnable(D3D_COLOR_WRITE_ENABLE_ALL);
-    StateManager.SetDepthFunc(D3DCMP_ALWAYS);
-    StateManager.SetDepthEnable(FALSE);
-    StateManager.SetCullMode(D3DCULL_NONE);
+    RCache.set_Stencil(FALSE, D3DCMP_ALWAYS, 0x80, 0xFF, 0x80,
+        D3DSTENCILOP_KEEP, D3DSTENCILOP_REPLACE, D3DSTENCILOP_KEEP);
+    RCache.set_ColorWriteEnable();
+    RCache.set_ZFunc(D3DCMP_ALWAYS);
+    RCache.set_Z(FALSE);
+    RCache.set_CullMode(D3DCULL_NONE);
     RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
 
-    StateManager.SetColorWriteEnable(D3D_COLOR_WRITE_ENABLE_ALL);
+    RCache.set_ColorWriteEnable();
 }
