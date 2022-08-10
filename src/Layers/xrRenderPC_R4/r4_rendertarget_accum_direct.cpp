db61d4f086caf47021865de3d90b57cbb680e921
Xottab_DUTY <xottab-duty@yandex.ru>
2022-08-11 03:34:54 +0500

    Restored sun shafts on R2 and R4 On RGL they were restored in 3f97e8cd18383985a7064afb5446ae86d0a704d8
    Now they should work like in Clear Sky, fully functional.

--- src/Layers/xrRenderPC_R4/r4_rendertarget_accum_direct.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_accum_direct.cpp
@@ -1220,8 +1220,10 @@ void CRenderTarget::accum_direct_volumetric(u32 sub_phase, const u32 Offset, con
         light* fuckingsun = (light*)RImplementation.Lights.sun._get();
 
         // Common constants (light-related)
-        Fvector L_clr;
+        Fvector L_dir, L_clr;
         L_clr.set(fuckingsun->color.r, fuckingsun->color.g, fuckingsun->color.b);
+        Device.mView.transform_dir(L_dir, fuckingsun->direction);
+        L_dir.normalize();
 
         //	Use g_combine_2UV that was set up by accum_direct
         //	RCache.set_Geometry			(g_combine_2UV);
@@ -1233,7 +1235,7 @@ void CRenderTarget::accum_direct_volumetric(u32 sub_phase, const u32 Offset, con
         {
             RCache.set_CullMode(CULL_CCW);
         }
-        //		RCache.set_c				("Ldynamic_dir",		L_dir.x,L_dir.y,L_dir.z,0 );
+        RCache.set_c("Ldynamic_dir", L_dir.x, L_dir.y, L_dir.z, 0.f);
         RCache.set_c("Ldynamic_color", L_clr.x, L_clr.y, L_clr.z, 0.f);
         RCache.set_c("m_shadow", mShadow);
         Fmatrix m_Texgen;
