OpenXRay/xray-16

commit ebe5426c3dc03b1f52fc9b095877ec8f98f89687

Moved InventoryUtilities shaders management into a manager (#549)

src/xrGame/ui/UIInventoryUtilities.cpp
@@ -45,6 +45,30 @@ CharInfoStrings* charInfoReputationStrings = NULL;
 CharInfoStrings* charInfoRankStrings = NULL;
 CharInfoStrings* charInfoGoodwillStrings = NULL;
 
+static class InventoryUtilitiesReset : public CUIResetNotifier, public pureAppStart, public pureAppEnd
+{
+public:
+    void OnAppStart() override
+    {
+        if (GEnv.isDedicatedServer)
+            return;
+        InventoryUtilities::CreateShaders();
+    }
+
+    void OnAppEnd() override
+    {
+        if (GEnv.isDedicatedServer)
+            return;
+        InventoryUtilities::DestroyShaders();
+    }
+
+    void OnUIReset() override
+    {
+        OnAppEnd();
+        OnAppStart();
+    }
+} s_inventory_utilities_reset;
+
 void InventoryUtilities::CreateShaders()
 {
     // Nothing here. All needed shaders will be created on demand

src/xrGame/xrgame_dll_detach.cpp
@@ -46,7 +46,6 @@ void init_game_globals()
         CInfoPortion::InitInternal(ShadowOfChernobylMode || ClearSkyMode);
         CEncyclopediaArticle::InitInternal(ShadowOfChernobylMode);
         CPhraseDialog::InitInternal();
-        InventoryUtilities::CreateShaders();
     };
     CCharacterInfo::InitInternal();
     CSpecificCharacter::InitInternal();
@@ -81,8 +80,6 @@ void clean_game_globals()
 
         CPhraseDialog::DeleteSharedData();
         CPhraseDialog::DeleteIdToIndexData();
-
-        InventoryUtilities::DestroyShaders();
     }
     CCharacterInfo::DeleteSharedData();
     CCharacterInfo::DeleteIdToIndexData();

commit c6fcc728b4cc7699ecc8f32948f5f6ce4e389cb0

Fixed resource leaks in renderers
This commit revisits ebe5426c3dc03b1f52fc9b095877ec8f98f89687, which introduced a memory leak in all renderers.

Also, leaks specific for DX11 renderer:
Fixed pAnnotation leak in dx11HW
Fixed texture surfaces reference counter not being decreased in CRT::resolve_into

src/Layers/xrRender/SH_Texture.cpp
@@ -38,6 +38,12 @@ CTexture::~CTexture()
 
 void CTexture::surface_set(ID3DBaseTexture* surf)
 {
+#if 0//def DEBUG
+    string_path msg_buff;
+    xr_sprintf(msg_buff, "* Changing texture surface [%s] current pSurface RefCount =", cName.c_str());
+    _SHOW_REF(msg_buff, pSurface);
+#endif // DEBUG
+
     if (surf)
         surf->AddRef();
 
@@ -289,7 +295,8 @@ void CTexture::Unload()
 {
 #ifdef DEBUG
     string_path msg_buff;
-    xr_sprintf(msg_buff, sizeof(msg_buff), "* Unloading texture [%s] pSurface RefCount=", cName.c_str());
+    xr_sprintf(msg_buff, "* Unloading texture [%s] pSurface RefCount =", cName.c_str());
+    _SHOW_REF(msg_buff, pSurface);
 #endif // DEBUG
 
     //.	if (flags.bLoaded)		Msg		("* Unloaded: %s",cName.c_str());
@@ -306,9 +313,6 @@ void CTexture::Unload()
     }
     flags.MemoryUsage = 0;
 
-#ifdef DEBUG
-    _SHOW_REF(msg_buff, pSurface);
-#endif // DEBUG
 
     _RELEASE(pSurface);
 

src/Layers/xrRenderDX11/dx11HW.cpp
@@ -387,24 +387,22 @@ void CHW::DestroyDevice()
     //  Must switch to windowed mode to release swap chain
     if (!m_ChainDesc.Windowed)
         m_pSwapChain->SetFullscreenState(FALSE, NULL);
-    _SHOW_REF("refCount:m_pSwapChain", m_pSwapChain);
-    _RELEASE(m_pSwapChain);
 #ifdef HAS_DX11_2
-    _SHOW_REF("refCount:m_pSwapChain2", m_pSwapChain2);
     _RELEASE(m_pSwapChain2);
 #endif
+    _SHOW_REF("refCount:m_pSwapChain", m_pSwapChain);
+    _RELEASE(m_pSwapChain);
 
+    _RELEASE(pAnnotation);
     _RELEASE(pContext1);
+    _SHOW_REF("refCount:pContext", pContext);
     _RELEASE(pContext);
 
-    _SHOW_REF("refCount:pDevice:", pDevice);
-    _RELEASE(pDevice);
-
 #ifdef HAS_DX11_3
-    _SHOW_REF("refCount:pDevice3:", pDevice3);
     _RELEASE(pDevice3);
 #endif
-
+    _SHOW_REF("refCount:pDevice:", pDevice);
+    _RELEASE(pDevice);
     DestroyD3D();
 }
 

src/Layers/xrRenderDX11/dx11SH_RT.cpp
@@ -268,8 +268,12 @@ void CRT::reset_end() { create(*cName, dwWidth, dwHeight, fmt, sampleCount, { dw
 void CRT::resolve_into(CRT& destination) const
 {
     VERIFY(fmt == destination.fmt); // only RTs with same format supported
-    HW.pContext->ResolveSubresource(destination.pTexture->surface_get(), 0,
-        pTexture->surface_get(), 0, dx11TextureUtils::ConvertTextureFormat(fmt));
+    auto srcSurf = pTexture->surface_get();
+    auto destSurf = destination.pTexture->surface_get();
+    HW.pContext->ResolveSubresource(destSurf, 0,
+        srcSurf, 0, dx11TextureUtils::ConvertTextureFormat(fmt));
+    _RELEASE(srcSurf);
+    _RELEASE(destSurf);
 }
 
 void resptrcode_crt::create(LPCSTR Name, u32 w, u32 h, D3DFORMAT f, u32 SampleCount /*= 1*/, Flags32 flags /*= 0*/)

src/Layers/xrRenderDX11/dx11SH_Texture.cpp
@@ -42,6 +42,12 @@ CTexture::~CTexture()
 
 void CTexture::surface_set(ID3DBaseTexture* surf)
 {
+#if 0//def DEBUG
+    string_path msg_buff;
+    xr_sprintf(msg_buff, sizeof(msg_buff), "* Changing texture [%s] current pSurface RefCount =", cName.c_str());
+    _SHOW_REF(msg_buff, pSurface);
+#endif // DEBUG
+
     if (surf)
         surf->AddRef();
     _RELEASE(pSurface);
@@ -569,7 +575,8 @@ void CTexture::Unload()
 {
 #ifdef DEBUG
     string_path msg_buff;
-    xr_sprintf(msg_buff, sizeof(msg_buff), "* Unloading texture [%s] pSurface RefCount=", cName.c_str());
+    xr_sprintf(msg_buff, sizeof(msg_buff), "* Unloading texture [%s] pSurface RefCount =", cName.c_str());
+    _SHOW_REF(msg_buff, pSurface);
 #endif // DEBUG
 
     //.	if (flags.bLoaded)		Msg		("* Unloaded: %s",cName.c_str());
@@ -588,10 +595,7 @@ void CTexture::Unload()
         pSurface = 0;
         m_pSRView = 0;
     }
-
-#ifdef DEBUG
-    _SHOW_REF(msg_buff, pSurface);
-#endif // DEBUG
+    
     _RELEASE(pSurface);
     _RELEASE(m_pSRView);
 

src/xrGame/UIGameCustom.cpp
@@ -39,14 +39,24 @@ CUIGameCustom::CUIGameCustom()
     m_pMessagesWnd = nullptr;
     ShowGameIndicators(true);
     ShowCrosshair(true);
+
+    InventoryUtilities::CreateShaders();
 }
 
 bool g_b_ClearGameCaptions = false;
 
 CUIGameCustom::~CUIGameCustom()
 {
+    InventoryUtilities::DestroyShaders();
+
     delete_data(CustomStatics);
     g_b_ClearGameCaptions = false;
+}
+
+void CUIGameCustom::OnUIReset()
+{
+    InventoryUtilities::DestroyShaders();
+    InventoryUtilities::CreateShaders();
 }
 
 void CUIGameCustom::OnFrame()

src/xrGame/UIGameCustom.h
@@ -100,7 +100,7 @@ public:
     virtual void Init(int stage) {}
     virtual void Render();
     virtual void OnFrame() override;
-    virtual void OnUIReset() override {}
+    virtual void OnUIReset() override;
     IC CUIActorMenu& GetActorMenu() const { return *ActorMenu; }
     IC CUIPdaWnd& GetPdaMenu() const { return *PdaMenu; }
     bool ShowActorMenu();

src/xrGame/UIGameSP.cpp
@@ -94,6 +94,7 @@ void CUIGameSP::OnFrame()
 
 void CUIGameSP::OnUIReset()
 {
+    inherited::OnUIReset();
     ReinitDialogs();
 }
 

src/xrGame/ui/UIInventoryUtilities.cpp
@@ -44,30 +44,6 @@ CharInfoStrings* charInfoReputationStrings = NULL;
 CharInfoStrings* charInfoRankStrings = NULL;
 CharInfoStrings* charInfoGoodwillStrings = NULL;
 
-static class InventoryUtilitiesReset : public CUIResetNotifier, public pureAppStart, public pureAppEnd
-{
-public:
-    void OnAppStart() override
-    {
-        if (GEnv.isDedicatedServer)
-            return;
-        InventoryUtilities::CreateShaders();
-    }
-
-    void OnAppEnd() override
-    {
-        if (GEnv.isDedicatedServer)
-            return;
-        InventoryUtilities::DestroyShaders();
-    }
-
-    void OnUIReset() override
-    {
-        OnAppEnd();
-        OnAppStart();
-    }
-} s_inventory_utilities_reset;
-
 void InventoryUtilities::CreateShaders()
 {
     // Nothing here. All needed shaders will be created on demand
@@ -76,19 +52,10 @@ void InventoryUtilities::CreateShaders()
 void InventoryUtilities::DestroyShaders()
 {
     xr_delete(g_BuyMenuShader);
-    g_BuyMenuShader = 0;
-
     xr_delete(g_EquipmentIconsShader);
-    g_EquipmentIconsShader = 0;
-
     xr_delete(g_MPCharIconsShader);
-    g_MPCharIconsShader = 0;
-
     xr_delete(g_OutfitUpgradeIconsShader);
-    g_OutfitUpgradeIconsShader = 0;
-
     xr_delete(g_WeaponUpgradeIconsShader);
-    g_WeaponUpgradeIconsShader = 0;
 }
 
 bool InventoryUtilities::GreaterRoomInRuck(PIItem item1, PIItem item2)