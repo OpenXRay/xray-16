bfc6104073bff729303753cb81f772c2bde2ca5c
Roman <Zebs-BMK@yandex.ru>
2022-12-03 01:40:20 +0300

    Export CCustomOutfit class into lua (for CoC Compatibility) (#1003)

---
+++ src/xrGame/CustomOutfit_script.cpp
@@ -0,0 +1,14 @@
+#include "pch_script.h"
+#include "CustomOutfit.h"
+#include "ActorHelmet.h"
+#include "xrScriptEngine/ScriptExporter.hpp"
+
+using namespace luabind;
+
+SCRIPT_EXPORT(CCustomOutfit, (CGameObject), {
+    module(luaState)
+    [
+        class_<CCustomOutfit, CGameObject>("CCustomOutfit")
+            .def(constructor<>())
+    ];
+});
