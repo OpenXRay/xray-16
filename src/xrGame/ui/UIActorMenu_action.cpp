35d00ed3d5f40596927c2ce89b860bd630c8e99a
Chugunov Roman <Zebs-BMK@yandex.ru>
2022-12-09 15:40:14 +0300

    UIActorMenu_action.cpp: Refactoring drop item on Item

--- src/xrGame/ui/UIActorMenu_action.cpp
+++ src/xrGame/ui/UIActorMenu_action.cpp
@@ -92,14 +92,14 @@ bool CUIActorMenu::DropItemOnAnotherItem(EDDListType t_old, EDDListType t_new, C
 
         const PIItem _iitem = _citem ? static_cast<PIItem>(_citem->m_pData) : nullptr;
 
-        CGameObject* GO1 = smart_cast<CGameObject*>(CurrentIItem());
-        CGameObject* GO2 = _iitem ? smart_cast<CGameObject*>(_iitem) : nullptr;
-        if (funct1(GO1 ? GO1->lua_game_object() : 0,
-            GO2 ? GO2->lua_game_object() : 0, (int)t_old, (int)t_new) == false)
+        if (_iitem == nullptr)
             return false;
+
+        CGameObject* GO1 = smart_cast<CGameObject*>(CurrentIItem());
+        CGameObject* GO2 = smart_cast<CGameObject*>(_iitem);
+
+        return funct1(GO1 ? GO1->lua_game_object() : 0, GO2 ? GO2->lua_game_object() : 0, (int)t_old, (int)t_new);
     }
-    //-Alundaio
-    return false;
 }
 
 bool CUIActorMenu::OnItemDrop(CUICellItem* itm)
@@ -147,8 +147,7 @@ bool CUIActorMenu::OnItemDrop(CUICellItem* itm)
     break;
     case iActorBag:
     {
-        if (!DropItemOnAnotherItem(t_old, t_new, old_owner, new_owner))
-            return false;
+        DropItemOnAnotherItem(t_old, t_new, old_owner, new_owner);
 
         ToBag(itm, true);
     }
