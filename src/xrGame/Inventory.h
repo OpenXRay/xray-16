d0c0f2d6b66858c235bc5c8bb391e8adc913d0b6
Xottab-DUTY <xottab-duty@yandex.ru>
2022-12-14 09:19:51 +0300

    xrGame/Inventory.h|cpp: moved CInventorySlot functions to the header

--- src/xrGame/Inventory.h
+++ src/xrGame/Inventory.h
@@ -6,17 +6,14 @@ class CInventoryItem;
 class CHudItem;
 class CInventoryOwner;
 
-class CInventorySlot
+class CInventorySlot final
 {
 public:
-    CInventorySlot();
-    virtual ~CInventorySlot();
+    bool CanBeActivated() const { return m_bAct; }
 
-    bool CanBeActivated() const;
-
-    PIItem m_pIItem;
-    bool m_bPersistent;
-    bool m_bAct;
+    PIItem m_pIItem     = nullptr;
+    bool m_bPersistent  = false;
+    bool m_bAct         = true;
 };
 
 class priority_group
