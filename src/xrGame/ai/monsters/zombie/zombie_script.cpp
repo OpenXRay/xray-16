dee5323de20e6815250cee7da3187e6406b5891a
Xottab-DUTY <xottab-duty@yandex.ru>
2022-12-26 01:26:33 +0300

    xrGame/ai: moved 'using namespace luabind' from global TU scope to local function scope

--- src/xrGame/ai/monsters/zombie/zombie_script.cpp
+++ src/xrGame/ai/monsters/zombie/zombie_script.cpp
@@ -2,7 +2,13 @@
 #include "zombie.h"
 #include "xrScriptEngine/ScriptExporter.hpp"
 
-using namespace luabind;
+SCRIPT_EXPORT(CZombie, (CGameObject),
+{
+    using namespace luabind;
 
-SCRIPT_EXPORT(
-    CZombie, (CGameObject), { module(luaState)[class_<CZombie, CGameObject>("CZombie").def(constructor<>())]; });
+    module(luaState)
+    [
+        class_<CZombie, CGameObject>("CZombie")
+            .def(constructor<>())
+    ];
+});
