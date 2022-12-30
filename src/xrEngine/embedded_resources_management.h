0d8c5b46fcb886ecc6c765603b6274fae63fa6e1
Xottab-DUTY <xottab-duty@yandex.ru>
2022-12-30 05:06:03 +0300

    Fixed Linux build

--- src/xrEngine/embedded_resources_management.h
+++ src/xrEngine/embedded_resources_management.h
@@ -120,13 +120,13 @@ inline void ExtractAndSetWindowIcon(SDL_Window* wnd, int iconIdx)
     SDL_Surface* surface = nullptr;
     switch (iconIdx)
     {
-    case IDI_COP:
+    case IDI_ICON_COP:
         surface = SDL_LoadBMP("icon_cop.bmp");
         break;
-    case IDI_CS:
+    case IDI_ICON_CS:
         surface = SDL_LoadBMP("icon_cs.bmp");
         break;
-    case IDI_SOC:
+    case IDI_ICON_SOC:
         surface = SDL_LoadBMP("icon_soc.bmp");
         break;
     }
