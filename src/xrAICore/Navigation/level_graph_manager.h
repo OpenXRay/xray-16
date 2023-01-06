a193b681a7353de48bd1273fb35358d43b3551ae
Xottab-DUTY <xottab-duty@yandex.ru>
2023-01-07 01:40:41 +0300

    Handle xrAI version 9 correctly AI nodes from version 9 are the same as nodes from version 8
    Thanks for the builds versions:
    https://github.com/abramcumner/xray_re-tools/blob/master/sources/xray_re/xr_ai_version.h

--- src/xrAICore/Navigation/level_graph_manager.h
+++ src/xrAICore/Navigation/level_graph_manager.h
@@ -11,13 +11,18 @@ class CLevelGraphManager
     size_t m_vertex_count;
 
 public:
-    CLevelGraphManager(IReader* stream, size_t vertex_count, u32 version) : m_vertex_count(vertex_count)
+    CLevelGraphManager(IReader* stream, size_t vertex_count, xrAI_Versions version) : m_vertex_count(vertex_count)
     {
-        if (version == XRAI_VERSION_COP || version == XRAI_VERSION_CS)
+        switch (version)
         {
+        case XRAI_CURRENT_VERSION:
+            static_assert(XRAI_CURRENT_VERSION == XRAI_VERSION_CS_COP,
+                "If you have changed the xrAI version, don't forget to add back compatibility with CS/COP xrAI version.");
             m_nodes = static_cast<CLevelVertex*>(stream->pointer());
-        }
-        else if (version == XRAI_VERSION_SOC)
+            break;
+
+        case XRAI_VERSION_PRIQUEL:
+        case XRAI_VERSION_SOC:
         {
             m_compatibility_mode = true;
             m_nodes = new CLevelVertex[vertex_count + 1]; // additional one, so we don't trigger access violation
@@ -30,27 +35,29 @@ public:
                 newNode = oldNode;
             }
 
-            // Mark end node as END NODE SOC
+            // Mark end node
             // so we can spot that in debugger, if we need
             NodeCompressed& endNode = m_nodes[vertex_count + 1];
-            endNode.data[0]  = 'E';
-            endNode.data[1]  = 'N';
-            endNode.data[2]  = 'D';
+            endNode.data[0]  = 'A';
+            endNode.data[1]  = 'I';
+            endNode.data[2]  = version;
             endNode.data[3]  = ' ';
             endNode.data[4]  = 'N';
             endNode.data[5]  = 'O';
             endNode.data[6]  = 'D';
             endNode.data[7]  = 'E';
             endNode.data[8]  = ' ';
-            endNode.data[9]  = 'S';
-            endNode.data[10] = 'O';
-            endNode.data[11] = 'C';
+            endNode.data[9]  = 'E';
+            endNode.data[10] = 'N';
+            endNode.data[11] = 'D';
             static_assert(sizeof(endNode.data) == 12, "If you have changed the NodeCompressed structure, please update the code above.");
+            break;
         }
-        else
-        {
+
+        default:
             FATAL("Unsupported level graph version.");
-        }
+            NODEFAULT;
+        } // switch (version)
     }
 
     ~CLevelGraphManager()
