6610c99bb07d3d90bdbad70fe5fdb7a8af50e23f
John Curley <johndanielcurley@gmail.com>
2023-01-28 21:36:13 +1300

    added XRCORE_API back

--- src/xrCore/xr_token.h
+++ src/xrCore/xr_token.h
@@ -1,6 +1,6 @@
 #pragma once
 
-struct alignas(sizeof(pcstr)) xr_token
+struct alignas(alignof(pcstr)) xr_token
 {
     xr_token(): name(nullptr), id(-1) {}
     xr_token(const pcstr _name, const int _id) : name(_name), id(_id) {}
@@ -10,5 +10,5 @@ struct alignas(sizeof(pcstr)) xr_token
 };
 static_assert((sizeof(pcstr) != 8) || (sizeof(pcstr) != 16), "pcstr is not a valid 32 or 64 bit value");
 
-pcstr get_token_name(const xr_token* tokens, int key);
-int get_token_id(const xr_token* tokens, pcstr key);
+XRCORE_API pcstr get_token_name(const xr_token* tokens, int key);
+XRCORE_API int get_token_id(const xr_token* tokens, pcstr key);
