247524b1570b94df6f7f4fb8681edbc008ee7dc4
John <johndanielcurley@gmail.com>
2023-01-29 00:42:21 +1300

    Added static assert for pointer alignment checking, especially for RISC machines

--- src/xrCore/xr_token.h
+++ src/xrCore/xr_token.h
@@ -8,7 +8,8 @@ struct alignas(alignof(pcstr)) xr_token
     pcstr name;
     int id;
 };
-static_assert((sizeof(pcstr) != 8) || (sizeof(pcstr) != 16), "pcstr is not a valid 32 or 64 bit value");
+static_assert(sizeof(xr_token) == sizeof(pcstr) * 2, "xr_token should be aligned, otherwise it may have problems on RISC (e.g. ARM) architectures, which require aligned pointers.");
 
 XRCORE_API pcstr get_token_name(const xr_token* tokens, int key);
 XRCORE_API int get_token_id(const xr_token* tokens, pcstr key);
+
