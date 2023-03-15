188b727f8cf48dee62e1324add5a18a5b064cd71
Alex <35600636+v2v3v4@users.noreply.github.com>
2023-03-15 06:25:49 +0300

    Fixed elements moving bug fix

--- src/xrPhysics/PHElement.cpp
+++ src/xrPhysics/PHElement.cpp
@@ -381,6 +381,15 @@ void CPHElement::PhDataUpdate(dReal step)
     if (!isActive())
         return;
 
+	if (isFixed())
+	{
+		dBodySetLinearVel(m_body,0,0,0);
+		dBodySetAngularVel(m_body,0,0,0);
+		dBodySetForce(m_body,0,0,0);
+		dBodySetTorque(m_body,0,0,0);
+		return;
+	}
+
 ///////////////skip for disabled elements////////////////////////////////////////////////////////////
 // b_enabled_onstep=!!dBodyIsEnabled(m_body);
 // VERIFY_BOUNDARIES2(cast_fv(dBodyGetPosition(m_body)),phBoundaries,PhysicsRefObject(),"PhDataUpdate begin, body
@@ -1478,19 +1487,29 @@ void CPHElement::set_ApplyByGravity(bool flag)
 bool CPHElement::get_ApplyByGravity() { return (!!dBodyGetGravityMode(m_body)); }
 void CPHElement::Fix()
 {
-    m_flags.set(flFixed, TRUE);
-    FixBody(m_body);
+	if (isFixed())
+		return;
+
+	dBodySetNoUpdatePosMode(m_body, 1);
+	m_flags.set(flFixed, TRUE);
+
+	FixBody(m_body);
 }
 
 void CPHElement::SetAnimated(bool v) { m_flags.set(flAnimated, BOOL(v)); }
 void CPHElement::ReleaseFixed()
 {
-    if (!isFixed())
-        return;
-    m_flags.set(flFixed, FALSE);
-    if (!isActive())
-        return;
-    dBodySetMass(m_body, &m_mass);
+	if (!isFixed())
+		return;
+
+	dBodySetNoUpdatePosMode(m_body, 0);
+	m_flags.set(flFixed, FALSE);
+
+	if (!isActive())
+		return;
+
+	dBodySetMass(m_body, &m_mass);
+	dBodySetGravityMode(m_body, 1);
 }
 void CPHElement::applyGravityAccel(const Fvector& accel)
 {
