a94e2fea8532874143d2ba12afde61eb9c0cc8fb
Xottab-DUTY <xottab-duty@yandex.ru>
2023-04-22 17:03:58 +0300

    Fixed possible bug with lights not being updated OpenGL renderer (thanks @GermanAizek, #1148)

--- src/Layers/xrRender/light.cpp
+++ src/Layers/xrRender/light.cpp
@@ -207,11 +207,11 @@ void light::spatial_move()
     // update spatial DB
     SpatialBase::spatial_move();
 
-#if (RENDER == R_R2) || (RENDER == R_R3) || (RENDER == R_R4)
+#if (RENDER == R_R2) || (RENDER == R_R3) || (RENDER == R_R4) || (RENDER == R_GL)
     if (flags.bActive)
         gi_generate();
     svis.invalidate();
-#endif // (RENDER==R_R2) || (RENDER==R_R3) || (RENDER==R_R4)
+#endif // (RENDER==R_R2) || (RENDER==R_R3) || (RENDER==R_R4) || (RENDER == R_GL)
 }
 
 vis_data& light::get_homdata()
