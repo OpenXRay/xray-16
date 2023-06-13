219c01d7d0fcec1993a0894ce2a260ea503af35b
vTurbine <vtrubiloff@gmail.com>
2023-06-13 20:43:15 +0200

    xrRender: Details: option to process all cache entries in one frame

--- src/Layers/xrRender/DetailManager_CACHE.cpp
+++ src/Layers/xrRender/DetailManager_CACHE.cpp
@@ -100,66 +100,75 @@ void CDetailManager::cache_Update(int v_x, int v_z, Fvector& view, int limit)
 {
     bool bNeedMegaUpdate = (cache_cx != v_x) || (cache_cz != v_z);
     // ***** Cache shift
-    while (cache_cx != v_x)
     {
         if (v_x > cache_cx)
         {
-            // shift matrix to left
-            cache_cx++;
-            for (u32 z = 0; z < dm_cache_line; z++)
+            while (cache_cx != v_x)
             {
-                Slot* S = cache[z][0];
-                for (u32 x = 1; x < dm_cache_line; x++)
-                    cache[z][x - 1] = cache[z][x];
-                cache[z][dm_cache_line - 1] = S;
-                cache_Task(dm_cache_line - 1, z, S);
+                // shift matrix to left
+                cache_cx++;
+                for (u32 z = 0; z < dm_cache_line; z++)
+                {
+                    Slot* S = cache[z][0];
+                    for (u32 x = 1; x < dm_cache_line; x++)
+                        cache[z][x - 1] = cache[z][x];
+                    cache[z][dm_cache_line - 1] = S;
+                    cache_Task(dm_cache_line - 1, z, S);
+                }
+                //R_ASSERT(cache_Validate());
             }
-            //R_ASSERT(cache_Validate());
         }
         else
         {
-            // shift matrix to right
-            cache_cx--;
-            for (u32 z = 0; z < dm_cache_line; z++)
+            while (cache_cx != v_x)
             {
-                Slot* S = cache[z][dm_cache_line - 1];
-                for (u32 x = dm_cache_line - 1; x > 0; x--)
-                    cache[z][x] = cache[z][x - 1];
-                cache[z][0] = S;
-                cache_Task(0, z, S);
+                // shift matrix to right
+                cache_cx--;
+                for (u32 z = 0; z < dm_cache_line; z++)
+                {
+                    Slot* S = cache[z][dm_cache_line - 1];
+                    for (u32 x = dm_cache_line - 1; x > 0; x--)
+                        cache[z][x] = cache[z][x - 1];
+                    cache[z][0] = S;
+                    cache_Task(0, z, S);
+                }
+                //R_ASSERT(cache_Validate());
             }
-            //R_ASSERT(cache_Validate());
         }
-    }
-    while (cache_cz != v_z)
-    {
+
         if (v_z > cache_cz)
         {
-            // shift matrix down a bit
-            cache_cz++;
-            for (u32 x = 0; x < dm_cache_line; x++)
+            while (cache_cz != v_z)
             {
-                Slot* S = cache[dm_cache_line - 1][x];
-                for (u32 z = dm_cache_line - 1; z > 0; z--)
-                    cache[z][x] = cache[z - 1][x];
-                cache[0][x] = S;
-                cache_Task(x, 0, S);
+                // shift matrix down a bit
+                cache_cz++;
+                for (u32 x = 0; x < dm_cache_line; x++)
+                {
+                    Slot* S = cache[dm_cache_line - 1][x];
+                    for (u32 z = dm_cache_line - 1; z > 0; z--)
+                        cache[z][x] = cache[z - 1][x];
+                    cache[0][x] = S;
+                    cache_Task(x, 0, S);
+                }
+                //R_ASSERT(cache_Validate());
             }
-            //R_ASSERT(cache_Validate());
         }
         else
         {
-            // shift matrix up
-            cache_cz--;
-            for (u32 x = 0; x < dm_cache_line; x++)
+            while (cache_cz != v_z)
             {
-                Slot* S = cache[0][x];
-                for (u32 z = 1; z < dm_cache_line; z++)
-                    cache[z - 1][x] = cache[z][x];
-                cache[dm_cache_line - 1][x] = S;
-                cache_Task(x, dm_cache_line - 1, S);
+                // shift matrix up
+                cache_cz--;
+                for (u32 x = 0; x < dm_cache_line; x++)
+                {
+                    Slot* S = cache[0][x];
+                    for (u32 z = 1; z < dm_cache_line; z++)
+                        cache[z - 1][x] = cache[z][x];
+                    cache[dm_cache_line - 1][x] = S;
+                    cache_Task(x, dm_cache_line - 1, S);
+                }
+                // R_ASSERT (cache_Validate());
             }
-            // R_ASSERT (cache_Validate());
         }
     }
 
@@ -170,6 +179,9 @@ void CDetailManager::cache_Update(int v_x, int v_z, Fvector& view, int limit)
         limit = dm_cache_size;
         bFullUnpack = TRUE;
     }
+
+    extern int exp_dm_full_unpack;
+    bFullUnpack |= exp_dm_full_unpack > 0;
 
     for (int iteration = 0; cache_task.size() && (iteration < limit); iteration++)
     {
