633841566db10d928e49fd168806a77cf6c5089b
Xottab-DUTY <xottab-duty@yandex.ru>
2023-08-03 01:56:35 +0500

    xrEngine/editor_base.h: replaced final with override keyword. We use final specifier on entire class, so there's no need to use it on every function. But we still need to specify that it's overridden functions.

--- src/xrEngine/editor_base.h
+++ src/xrEngine/editor_base.h
@@ -30,7 +30,7 @@ public:
 
 public:
     ide();
-    ~ide();
+    ~ide() override;
 
     [[nodiscard]]
     bool is_shown() const;
@@ -48,34 +48,34 @@ public:
 
 public:
     // Interface implementations
-    void OnFrame() final;
-    void OnRender() final;
+    void OnFrame() override;
+    void OnRender() override;
 
-    void OnAppActivate() final;
-    void OnAppDeactivate() final;
+    void OnAppActivate() override;
+    void OnAppDeactivate() override;
 
-    void OnAppStart() final;
-    void OnAppEnd() final;
+    void OnAppStart() override;
+    void OnAppEnd() override;
 
-    void IR_Capture() final;
-    void IR_Release() final;
+    void IR_Capture() override;
+    void IR_Release() override;
 
-    void IR_OnMousePress(int key) final;
-    void IR_OnMouseRelease(int key) final;
-    void IR_OnMouseHold(int key) final;
-    void IR_OnMouseWheel(int x, int y) final;
-    void IR_OnMouseMove(int x, int y) final;
+    void IR_OnMousePress(int key) override;
+    void IR_OnMouseRelease(int key) override;
+    void IR_OnMouseHold(int key) override;
+    void IR_OnMouseWheel(int x, int y) override;
+    void IR_OnMouseMove(int x, int y) override;
 
-    void IR_OnKeyboardPress(int key) final;
-    void IR_OnKeyboardRelease(int key) final;
-    void IR_OnKeyboardHold(int key) final;
-    void IR_OnTextInput(pcstr text) final;
+    void IR_OnKeyboardPress(int key) override;
+    void IR_OnKeyboardRelease(int key) override;
+    void IR_OnKeyboardHold(int key) override;
+    void IR_OnTextInput(pcstr text) override;
 
-    void IR_OnControllerPress(int key, float x, float y) final;
-    void IR_OnControllerRelease(int key, float x, float y) final;
-    void IR_OnControllerHold(int key, float x, float y) final;
+    void IR_OnControllerPress(int key, float x, float y) override;
+    void IR_OnControllerRelease(int key, float x, float y) override;
+    void IR_OnControllerHold(int key, float x, float y) override;
 
-    void IR_OnControllerAttitudeChange(Fvector change) final;
+    void IR_OnControllerAttitudeChange(Fvector change) override;
 
 private:
     ImGuiWindowFlags get_default_window_flags() const;
