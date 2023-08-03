49a19703d67faab1626184c51e8f245e8dad6261
Xottab-DUTY <xottab-duty@yandex.ru>
2023-08-03 15:39:46 +0500

    Moved uiscriptwnd_script.h contents to UIScriptWnd_script.cpp Also refactored CUIDialogWndExWrapperBase

--- src/xrGame/ui/UIScriptWnd_script.cpp
+++ src/xrGame/ui/UIScriptWnd_script.cpp
@@ -1,22 +1,82 @@
 #include "pch_script.h"
+
 #include "UIScriptWnd.h"
-#include "xrUICore/ListWnd/UIListWnd.h" // Don't remove
-#include "xrUICore/TabControl/UITabControl.h" // Don't remove
-#include "uiscriptwnd_script.h"
+
+#include "xrUICore/ListWnd/UIListWnd.h"
+#include "xrUICore/TabControl/UITabControl.h"
+
 #include "xrScriptEngine/ScriptExporter.hpp"
-#include "xrScriptEngine/Functor.hpp"
+
+//UI-controls
+class CUIStatic;
+class CUIEditBox;
+class CUIDialogWnd;
+class CUIFrameWindow;
+class CUIFrameLineWnd;
+class CUIProgressBar;
+class CUITabControl;
+class CUIListWnd;
+class CUIListBox;
+
+template <typename T>
+T* CUIDialogWndEx::GetControl(pcstr name)
+{
+    shared_str n = name;
+    CUIWindow* pWnd = FindChild(n);
+    if (pWnd == nullptr)
+        return nullptr;
+
+    return smart_cast<T*>(pWnd);
+}
+
+struct CUIDialogWndExWrapperBase final : public CUIDialogWndEx, public luabind::wrap_base
+{
+    typedef CUIDialogWndEx inherited;
+    typedef CUIDialogWndExWrapperBase self_type;
+
+    bool OnKeyboardAction(int dik, EUIMessages keyboard_action) override
+    {
+        return luabind::call_member<bool>(this, "OnKeyboard", dik, keyboard_action);
+    }
+
+    static bool OnKeyboard_static(inherited* ptr, int dik, EUIMessages keyboard_action)
+    {
+        return ptr->self_type::inherited::OnKeyboardAction(dik, keyboard_action);
+    }
+
+    void Update() override
+    {
+        luabind::call_member<void>(this, "Update");
+    }
+
+    static void Update_static(inherited* ptr)
+    {
+        ptr->self_type::inherited::Update();
+    }
+
+    bool Dispatch(int cmd, int param) override
+    {
+        return luabind::call_member<bool>(this, "Dispatch", cmd, param);
+    }
+
+    static bool Dispatch_static(inherited* ptr, int cmd, int param)
+    {
+        return ptr->self_type::inherited::Dispatch(cmd, param);
+    }
+};
 
 // clang-format off
 SCRIPT_EXPORT(CUIDialogWndEx, (CUIDialogWnd, IFactoryObject),
 {
     using namespace luabind;
+    using BaseType = CUIDialogWndEx;
+    using WrapType = CUIDialogWndExWrapperBase;
 
     module(luaState)
     [
         class_<CUIDialogWndEx, luabind::bases<CUIDialogWnd, IFactoryObject>, luabind::default_holder, WrapType>("CUIScriptWnd")
             .def(constructor<>())
-            .def("AddCallback", (void (BaseType::*)(pcstr, s16, const luabind::functor<void>&, const luabind::object&)) &
-                BaseType::AddCallback)
+            .def("AddCallback", &BaseType::AddCallback)
             .def("Register", (void (BaseType::*)(CUIWindow*, pcstr)) & BaseType::Register)
             .def("OnKeyboard", &BaseType::OnKeyboardAction, &WrapType::OnKeyboard_static)
             .def("Update", &BaseType::Update, &WrapType::Update_static)
