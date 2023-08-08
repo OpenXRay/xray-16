bbdf63253571c087a0fdd67dd82473b9b3f8486b
Xottab-DUTY <xottab-duty@yandex.ru>
2023-08-08 22:00:00 +0500

    UI Debugger now supports UI declared from Lua

--- src/xrGame/ui/UIScriptWnd_script.cpp
+++ src/xrGame/ui/UIScriptWnd_script.cpp
@@ -7,6 +7,11 @@
 
 #include "xrScriptEngine/ScriptExporter.hpp"
 
+#if LUA_VERSION_NUM < 502
+#   define lua_getuservalue lua_getfenv
+#   define lua_setuservalue lua_setfenv
+#endif
+
 //UI-controls
 class CUIStatic;
 class CUIEditBox;
@@ -33,11 +38,6 @@ struct CUIDialogWndExWrapperBase final : public CUIDialogWndEx, public luabind::
 {
     typedef CUIDialogWndEx inherited;
     typedef CUIDialogWndExWrapperBase self_type;
-
-    CUIDialogWndExWrapperBase()
-    {
-        SetWindowName(GetDebugType());
-    }
 
     bool OnKeyboardAction(int dik, EUIMessages keyboard_action) override
     {
@@ -74,14 +74,124 @@ struct CUIDialogWndExWrapperBase final : public CUIDialogWndEx, public luabind::
     bool FillDebugTree(const CUIDebugState& debugState) override
     {
 #ifndef MASTER_GOLD
-        if (!inherited::FillDebugTree(debugState))
-            return false;
+        ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_OpenOnArrow;
+        if (debugState.selected == this)
+            flags |= ImGuiTreeNodeFlags_Selected;
+
+        inherited::FillDebugTree(debugState);
+
+        const auto& wrap_ref = luabind::detail::wrap_access::ref(*this);
+
+        lua_State* ls = wrap_ref.state();
+        const int prev = lua_gettop(ls);
+        {
+            wrap_ref.get(ls);
+            luabind::detail::stack_pop pop{ ls, 1 };
+
+            const auto* obj = static_cast<luabind::detail::object_rep*>(lua_touserdata(ls, -1));
+
+            ImGui::PushID(obj->crep()->name());
+            const bool open = ImGui::TreeNodeEx(this, flags, "%s * LUA", obj->crep()->name());
+            if (ImGui::IsItemClicked())
+                debugState.select(this);
+
+            if (open)
+            {
+                lua_getuservalue(ls, -1);
+                luabind::detail::stack_pop pop2{ ls, 1 };
+
+                luabind::table members(luabind::from_stack(ls, -1));
+                for (luabind::iterator it(members), end; it != end; ++it)
+                {
+                    auto proxy = *it;
+                    proxy.push(ls);
+                    luabind::detail::stack_pop pop3{ ls, 1 };
+
+                    if (luabind::type(proxy) == LUA_TUSERDATA)
+                    {
+                        const auto ptr = luabind::detail::get_instance(ls, -1);
+                        if (!ptr)
+                            continue;
+
+                        const auto inst = ptr->get_instance(luabind::detail::registered_class<CUIWindow>::id).first;
+                        if (!inst)
+                            continue;
+
+                        ((CUIWindow*)inst)->FillDebugTree(debugState);
+                    }
+                }
+
+                ImGui::TreePop();
+            } // if (open)
+            ImGui::PopID();
+        }
+        R_ASSERT(lua_gettop(ls) == prev);
 
         return true;
 #else
         UNUSED(debugState);
         return false;
 #endif
+    }
+
+    void FillDebugInfo() override
+    {
+        inherited::FillDebugInfo();
+
+        const auto& wrap_ref = luabind::detail::wrap_access::ref(*this);
+
+        lua_State* ls = wrap_ref.state();
+        const int prev = lua_gettop(ls);
+        {
+            wrap_ref.get(ls);
+            luabind::detail::stack_pop pop{ ls, 1 };
+            const auto* obj = static_cast<luabind::detail::object_rep*>(lua_touserdata(ls, -1));
+
+            if (ImGui::CollapsingHeader(obj->crep()->name()))
+            {
+                ImGui::BeginDisabled();
+
+                lua_getuservalue(ls, -1);
+                luabind::detail::stack_pop pop2{ ls, 1 };
+
+                luabind::table members(luabind::from_stack(ls, -1));
+                for (luabind::iterator it(members), end; it != end; ++it)
+                {
+                    auto proxy = *it;
+                    proxy.push(ls);
+                    luabind::detail::stack_pop pop3{ ls, 1 };
+
+                    auto str = luabind::detail::stack_content_by_name(ls, prev);
+                    cpcstr name = lua_tostring(ls, -2);
+                    if (!name)
+                        continue;
+
+                    switch (luabind::type(proxy))
+                    {
+                    case LUA_TBOOLEAN:
+                    {
+                        bool boolean = lua_toboolean(ls, -1);
+                        ImGui::Checkbox(name, &boolean);
+                        break;
+                    }
+                    case LUA_TNUMBER:
+                    {
+                        lua_Number number = lua_tonumber(ls, -1);
+                        ImGui::DragScalar(name, ImGuiDataType_Double, &number);
+                        break;
+                    }
+                    case LUA_TSTRING:
+                    {
+                        cpcstr string = lua_tostring(ls, -1);
+                        ImGui::Text("%s", string);
+                        break;
+                    }
+                    } // switch (luabind::type(proxy))
+                }
+                ImGui::EndDisabled();
+            }
+        }
+        R_ASSERT(lua_gettop(ls) == prev);
     }
 };
 
