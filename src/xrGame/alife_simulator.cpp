28c66db0960be69ca52695493e89d0c3a54871fa
Syrotenko Igor <Neloreck@gmail.com>
2023-10-18 17:16:13 +0300

    [Feature] Adding `isNewGame` parameter for start game callback (#1462)

--- src/xrGame/alife_simulator.cpp
+++ src/xrGame/alife_simulator.cpp
@@ -52,10 +52,12 @@ CALifeSimulator::CALifeSimulator(IPureServer* server, shared_str* command_line)
     xr_strcat(temp, p.m_alife);
     *command_line = temp;
 
+    const bool isNewGame = xr_strcmp(p.m_new_or_load, "new") != -1;
+
     LPCSTR start_game_callback = pSettings->r_string(alife_section, "start_game_callback");
     luabind::functor<void> functor;
     R_ASSERT2(GEnv.ScriptEngine->functor(start_game_callback, functor), "failed to get start game callback");
-    functor();
+    functor(isNewGame);
 
     load(p.m_game_or_spawn, !xr_strcmp(p.m_new_or_load, "load") ? false : true, !xr_strcmp(p.m_new_or_load, "new"));
 }
