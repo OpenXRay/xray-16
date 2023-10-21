c9e8981a737f4aef5fb6e53e7e6b0cf403ae2dad
Xottab-DUTY <xottab-duty@yandex.ru>
2023-10-21 16:23:39 +0500

    xrRender/r__screenshot.cpp: removed unused code

--- src/Layers/xrRender/r__screenshot.cpp
+++ src/Layers/xrRender/r__screenshot.cpp
@@ -1,33 +1,5 @@
 #include "stdafx.h"
 
-IC u32 convert(float c)
-{
-    u32 C = iFloor(c);
-    if (C > 255)
-        C = 255;
-    return C;
-}
-
-IC void MouseRayFromPoint(Fvector& direction, int x, int y, Fmatrix& m_CamMat)
-{
-    int halfwidth = Device.dwWidth / 2;
-    int halfheight = Device.dwHeight / 2;
-
-    Ivector2 point2;
-    point2.set(x - halfwidth, halfheight - y);
-
-    float size_y = VIEWPORT_NEAR * tanf(deg2rad(60.f) * 0.5f);
-    float size_x = size_y / (Device.fHeight_2 / Device.fWidth_2);
-
-    float r_pt = float(point2.x) * size_x / (float)halfwidth;
-    float u_pt = float(point2.y) * size_y / (float)halfheight;
-
-    direction.mul(m_CamMat.k, VIEWPORT_NEAR);
-    direction.mad(direction, m_CamMat.j, u_pt);
-    direction.mad(direction, m_CamMat.i, r_pt);
-    direction.normalize();
-}
-
 void CRender::ScreenshotAsyncBegin()
 {
     VERIFY(!m_bMakeAsyncSS);
