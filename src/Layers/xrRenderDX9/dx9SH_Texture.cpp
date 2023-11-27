3d851f5bb5082794ad734fa40933d9d9c398b68d
vTurbine <vtrubiloff@gmail.com>
2023-11-27 22:31:04 +0100

    DX9 specifics moved into appropriate folder

--- src/Layers/xrRenderDX9/dx9SH_Texture.cpp
+++ src/Layers/xrRenderDX9/dx9SH_Texture.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #pragma hdrstop
 
-#include "ResourceManager.h"
+#include <Layers/xrRender/ResourceManager.h>
 
 #include "xrEngine/tntQAVI.h"
 #include "xrEngine/xrTheora_Surface.h"
