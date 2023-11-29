74fc4322a0197a1b36434089856145dc7dd8bdee
Xottab-DUTY <xottab-duty@yandex.ru>
2023-11-29 07:49:30 +0500

    Common/Common.hpp: move submodule check

--- src/Common/Common.hpp
+++ src/Common/Common.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#if _MSC_VER
-#include "submodule_check.hpp"
-#endif
-
 #include "Common/Config.hpp"
 #include "Common/Platform.hpp"
 #include "Common/FSMacros.hpp"
 #include "Include/xrAPI/xrAPI.h"
+
+#ifdef XR_COMPILER_MSVC
+#   include "submodule_check.hpp"
+#endif
