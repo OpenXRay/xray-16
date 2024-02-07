d7461308e3b7491e8c59e400e2cb055459e4777a
nouverbe <nouverbe@NOUVERBE-PC>
2024-02-07 18:06:38 +0200

    fixing errors related to changing settings

--- src/Layers/xrRenderPC_R5/DX12/Device/CCryDX12DeviceContext.cpp
+++ src/Layers/xrRenderPC_R5/DX12/Device/CCryDX12DeviceContext.cpp
@@ -2516,8 +2516,6 @@ void STDMETHODCALLTYPE CCryDX12DeviceContext::ClearState()
  
    IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_UNDEFINED);
    
-   ResetCachedState();
-
    CSSetShader(NULL, NULL, 0);
    DSSetShader(NULL, NULL, 0);
    GSSetShader(NULL, NULL, 0); 
@@ -2532,9 +2530,8 @@ void STDMETHODCALLTYPE CCryDX12DeviceContext::ClearState()
 
    OMSetBlendState(NULL, NULL, 0);
    OMSetDepthStencilState(NULL, 0);
- 
-   SubmitAllCommands(true);
-   WaitForIdle();
+
+   SubmitAllCommands(DX12_SUBMISSION_MODE == DX12_SUBMISSION_SYNC, m_CmdFenceSet.GetCurrentValues());
 }
 
 void STDMETHODCALLTYPE CCryDX12DeviceContext::Flush()
@@ -2561,7 +2558,8 @@ void STDMETHODCALLTYPE CCryDX12DeviceContext::ExecuteCommandList(
     _In_  BOOL RestoreContextState)
 {
     DX12_FUNC_LOG
-        DX12_NOT_IMPLEMENTED
+    //R_ASSERT(!IsDeferred(), "Only immediate context can execute command list");
+    DX12_NOT_IMPLEMENTED
 }
 
 HRESULT STDMETHODCALLTYPE CCryDX12DeviceContext::FinishCommandList(
@@ -2569,7 +2567,8 @@ HRESULT STDMETHODCALLTYPE CCryDX12DeviceContext::FinishCommandList(
     _Out_opt_  ID3D11CommandList** ppCommandList)
 {
     DX12_FUNC_LOG
-        DX12_NOT_IMPLEMENTED
+    DX12_NOT_IMPLEMENTED
+    //R_ASSERT(IsDeferred(), "Only deferred context can record command list");
     return E_FAIL;
 }
 
