OpenXRay/xray-16

commit 55677278859941f703c1ddee1f5f9715631f5f63

xrSound: encapsulate all CSoundRender_Source member variables

src/xrSound/SoundRender_Source.h
@@ -11,7 +11,7 @@ enum class SoundFormat
     Float32,
 };
 
-struct SoundSourceInfo
+struct SoundDataInfo
 {
     SoundFormat format{};
     u16         channels{};       // number of channels (i.e. mono, stereo...)
@@ -22,27 +22,30 @@ struct SoundSourceInfo
     u32         bytesPerBuffer{}; // target buffer size
 };
 
+struct SoundSourceInfo
+{
+    float baseVolume;
+    float minDist;
+    float maxDist;
+    float maxAIDist;
+    u32   gameType;
+};
+
 class XRSOUND_API CSoundRender_Source final : public CSound_source
 {
-public:
-    shared_str pname;
-    shared_str fname;
-
     OggVorbis_File ovf{};
     IReader* wave{};
     int refs{};
     std::mutex read_lock;
 
-    float fTimeTotal;
-    u32 dwBytesTotal;
+    shared_str pname;
+    shared_str fname;
 
+    float fTimeTotal{};
+    u32 dwBytesTotal{};
+
+    SoundDataInfo m_data_info{};
     SoundSourceInfo m_info{};
-
-    float m_fBaseVolume;
-    float m_fMinDist;
-    float m_fMaxDist;
-    float m_fMaxAIDist;
-    u32 m_uGameType;
 
 private:
     void i_decompress(char* dest, u32 size);
@@ -51,7 +54,7 @@ private:
     bool LoadWave(pcstr name, bool crashOnError);
 
 public:
-    CSoundRender_Source();
+    CSoundRender_Source() = default;
     ~CSoundRender_Source() override;
 
     bool load(pcstr name, bool replaceWithNoSound = true, bool crashOnError = true);
@@ -62,10 +65,14 @@ public:
 
     void decompress(void* dest, u32 byte_offset, u32 size);
 
+    [[nodiscard]] pcstr file_name() const override { return fname.c_str(); }
+
     [[nodiscard]] float length_sec() const override { return fTimeTotal; }
-    [[nodiscard]] u32 game_type() const override { return m_uGameType; }
-    [[nodiscard]] pcstr file_name() const override { return *fname; }
-    [[nodiscard]] float base_volume() const { return m_fBaseVolume; }
-    [[nodiscard]] u16 channels_num() const override { return m_info.channels; }
     [[nodiscard]] u32 bytes_total() const override { return dwBytesTotal; }
+
+    [[nodiscard]] u16 channels_num() const override { return data_info().channels; }
+    [[nodiscard]] u32 game_type() const override { return info().gameType; }
+
+    [[nodiscard]] const auto& data_info() const { return m_data_info; }
+    [[nodiscard]] const auto&      info() const { return m_info; }
 };

src/xrSound/SoundRender_Core.cpp
@@ -222,6 +222,6 @@ void CSoundRender_Core::refresh_sources()
     {
         CSoundRender_Source* s = kv.second;
         s->unload();
-        s->load(*s->fname);
+        s->load(s->file_name());
     }
 }

src/xrSound/SoundRender_Core_Processor.cpp
@@ -143,7 +143,7 @@ void CSoundRender_Core::statistic(CSound_stats* dest, CSound_stats_ext* ext)
                 item.volume = emitter->smooth_volume;
                 if (emitter->owner_data)
                 {
-                    item.name = emitter->source()->fname;
+                    item.name = emitter->source()->file_name();
                     item.game_object = emitter->owner_data->g_object;
                     item.game_type = emitter->owner_data->g_type;
                     item.type = emitter->owner_data->s_type;

src/xrSound/SoundRender_Emitter.cpp
@@ -115,7 +115,7 @@ void CSoundRender_Emitter::set_cursor(u32 p)
 
     if (owner_data._get() && owner_data->fn_attached[0].size())
     {
-        u32 bt = ((CSoundRender_Source*)owner_data->handle)->dwBytesTotal;
+        u32 bt = ((CSoundRender_Source*)owner_data->handle)->bytes_total();
         if (m_stream_cursor >= m_cur_handle_cursor + bt)
         {
             SoundRender->i_destroy_source((CSoundRender_Source*)owner_data->handle);
@@ -194,7 +194,7 @@ void CSoundRender_Emitter::fill_block(void* ptr, u32 size)
     }
     else
     {
-        const u32 bt_handle = ((CSoundRender_Source*)owner_data->handle)->dwBytesTotal;
+        const u32 bt_handle = ((CSoundRender_Source*)owner_data->handle)->bytes_total();
         if (get_cursor(true) + size > m_cur_handle_cursor + bt_handle)
         {
             R_ASSERT(owner_data->fn_attached[0].size());

src/xrSound/SoundRender_Emitter_FSM.cpp
@@ -8,7 +8,7 @@
 
 XRSOUND_API extern float psSoundCull;
 
-inline u32 calc_cursor(const float& fTimeStarted, float& fTime, const float& fTimeTotal, const float& fFreq, const SoundSourceInfo& info) //--#SM+#--
+inline u32 calc_cursor(const float& fTimeStarted, float& fTime, const float& fTimeTotal, const float& fFreq, const SoundDataInfo& info) //--#SM+#--
 {
     if (fTime < fTimeStarted)
         fTime = fTimeStarted; // Андрюха посоветовал, ассерт что ниже вылетел из за паузы как то хитро
@@ -151,7 +151,7 @@ void CSoundRender_Emitter::update(float fTime, float dt)
         }
         else
         {
-            const u32 ptr = calc_cursor(fTimeStarted, fTime, get_length_sec(), p_source.freq, source()->m_info); //--#SM+#--
+            const u32 ptr = calc_cursor(fTimeStarted, fTime, get_length_sec(), p_source.freq, source()->data_info()); //--#SM+#--
             set_cursor(ptr);
 
             if (update_culling(dt))
@@ -162,7 +162,7 @@ void CSoundRender_Emitter::update(float fTime, float dt)
                                 u32 ptr						= calc_cursor(	fTimeStarted,
                                                                             fTime,
                                                                             get_length_sec(),
-                                                                            source()->m_info);
+                                                                            source()->data_info());
                                 set_cursor					(ptr);
                 */
                 SoundRender->i_start(this);
@@ -204,7 +204,7 @@ void CSoundRender_Emitter::update(float fTime, float dt)
         {
             // switch to: PLAY
             m_current_state = stPlayingLooped; // switch state
-            const u32 ptr = calc_cursor(fTimeStarted, fTime, get_length_sec(), p_source.freq, source()->m_info); //--#SM+#--
+            const u32 ptr = calc_cursor(fTimeStarted, fTime, get_length_sec(), p_source.freq, source()->data_info()); //--#SM+#--
             set_cursor(ptr);
 
             SoundRender->i_start(this);
@@ -253,7 +253,7 @@ void CSoundRender_Emitter::update(float fTime, float dt)
                 fTimeToStop = fTime + fRemainingTime;
             }
 
-            const u32 ptr = calc_cursor(fTimeStarted, fTime, fLength, p_source.freq, source()->m_info);
+            const u32 ptr = calc_cursor(fTimeStarted, fTime, fLength, p_source.freq, source()->data_info());
             set_cursor(ptr);
 
             fTimeToRewind = 0.0f;

src/xrSound/SoundRender_Emitter_StartStop.cpp
@@ -14,14 +14,15 @@ void CSoundRender_Emitter::start(const ref_sound& _owner, u32 flags, float delay
     VERIFY(_owner);
     owner_data = _owner;
     VERIFY(owner_data);
-    //	source					= (CSoundRender_Source*)owner_data->handle;
     p_source.position.set(0, 0, 0);
-    p_source.min_distance = source()->m_fMinDist; // DS3D_DEFAULTMINDISTANCE;
-    p_source.max_distance = source()->m_fMaxDist; // 300.f;
-    p_source.base_volume = source()->m_fBaseVolume; // 1.f
-    p_source.volume = 1.f; // 1.f
+
+    const auto info = source()->info();
+    p_source.min_distance = info.minDist;
+    p_source.max_distance = info.maxDist;
+    p_source.base_volume = info.baseVolume;
+    p_source.volume = 1.f;
     p_source.freq = 1.f;
-    p_source.max_ai_distance = source()->m_fMaxAIDist; // 300.f;
+    p_source.max_ai_distance = info.maxAIDist;
 
     if (fis_zero(delay, EPS_L))
     {

src/xrSound/SoundRender_Source.cpp
@@ -3,15 +3,6 @@
 #include "SoundRender_Core.h"
 #include "SoundRender_Source.h"
 
-CSoundRender_Source::CSoundRender_Source()
-{
-    m_fMinDist = 1.f;
-    m_fMaxDist = 300.f;
-    m_fMaxAIDist = 300.f;
-    m_fBaseVolume = 1.f;
-    m_uGameType = 0;
-}
-
 CSoundRender_Source::~CSoundRender_Source() { unload(); }
 
 namespace
@@ -57,13 +48,13 @@ void CSoundRender_Source::decompress(void* dest, u32 byte_offset, u32 size)
     std::lock_guard guard{ read_lock };
 
     // seek
-    const auto sample_offset = ogg_int64_t(byte_offset / m_info.blockAlign);
+    const auto sample_offset = ogg_int64_t(byte_offset / m_data_info.blockAlign);
     const u32 cur_pos = u32(ov_pcm_tell(&ovf));
     if (cur_pos != sample_offset)
         ov_pcm_seek(&ovf, sample_offset);
 
     // decompress
-    if (m_info.format == SoundFormat::Float32)
+    if (m_data_info.format == SoundFormat::Float32)
         i_decompress(static_cast<float*>(dest), size);
     else
         i_decompress(static_cast<char*>(dest), size);
@@ -85,7 +76,7 @@ void CSoundRender_Source::i_decompress(char* _dest, u32 size)
 
 void CSoundRender_Source::i_decompress(float* _dest, u32 size)
 {
-    s32 left = s32(size / m_info.blockAlign);
+    s32 left = s32(size / m_data_info.blockAlign);
     while (left)
     {
         float** pcm;
@@ -98,7 +89,7 @@ void CSoundRender_Source::i_decompress(float* _dest, u32 size)
             samples = left;
 
         for (long j = 0; j < samples; j++)
-            for (long i = 0; i < m_info.channels; i++)
+            for (long i = 0; i < m_data_info.channels; i++)
                 *_dest++ = pcm[i][j];
 
         left -= samples;
@@ -184,29 +175,31 @@ bool CSoundRender_Source::LoadWave(pcstr pName, bool crashOnError)
         return false;
     });
 
-    m_info = {};
+    m_data_info = {};
 
-    m_info.samplesPerSec = ovi->rate;
-    m_info.channels = u16(ovi->channels);
+    m_data_info.samplesPerSec = ovi->rate;
+    m_data_info.channels = u16(ovi->channels);
 
     if (SoundRender->supports_float_pcm)
     {
-        m_info.format = SoundFormat::Float32;
-        m_info.bitsPerSample = 32;
+        m_data_info.format = SoundFormat::Float32;
+        m_data_info.bitsPerSample = 32;
     }
     else
     {
-        m_info.format = SoundFormat::PCM;
-        m_info.bitsPerSample = 16;
+        m_data_info.format = SoundFormat::PCM;
+        m_data_info.bitsPerSample = 16;
     }
 
-    m_info.blockAlign = m_info.bitsPerSample / 8 * m_info.channels;
-    m_info.avgBytesPerSec = m_info.samplesPerSec * m_info.blockAlign;
-    m_info.bytesPerBuffer = sdef_target_block * m_info.avgBytesPerSec / 1000;
+    m_data_info.blockAlign = m_data_info.bitsPerSample / 8 * m_data_info.channels;
+    m_data_info.avgBytesPerSec = m_data_info.samplesPerSec * m_data_info.blockAlign;
+    m_data_info.bytesPerBuffer = sdef_target_block * m_data_info.avgBytesPerSec / 1000;
 
     const s64 pcm_total = ov_pcm_total(&ovf, -1);
-    dwBytesTotal = u32(pcm_total * m_info.blockAlign);
-    fTimeTotal = dwBytesTotal / float(m_info.avgBytesPerSec);
+    dwBytesTotal = u32(pcm_total * m_data_info.blockAlign);
+    fTimeTotal = dwBytesTotal / float(m_data_info.avgBytesPerSec);
+
+    m_info = {};
 
     const vorbis_comment* ovm = ov_comment(&ovf, -1);
     if (ovm->comments)
@@ -215,27 +208,27 @@ bool CSoundRender_Source::LoadWave(pcstr pName, bool crashOnError)
         const u32 vers = F.r_u32();
         if (vers == 0x0001)
         {
-            m_fMinDist = F.r_float();
-            m_fMaxDist = F.r_float();
-            m_fBaseVolume = 1.f;
-            m_uGameType = F.r_u32();
-            m_fMaxAIDist = m_fMaxDist;
+            m_info.minDist = F.r_float();
+            m_info.maxDist = F.r_float();
+            m_info.baseVolume = 1.f;
+            m_info.gameType = F.r_u32();
+            m_info.maxAIDist = m_info.maxDist;
         }
         else if (vers == 0x0002)
         {
-            m_fMinDist = F.r_float();
-            m_fMaxDist = F.r_float();
-            m_fBaseVolume = F.r_float();
-            m_uGameType = F.r_u32();
-            m_fMaxAIDist = m_fMaxDist;
+            m_info.minDist = F.r_float();
+            m_info.maxDist = F.r_float();
+            m_info.baseVolume = F.r_float();
+            m_info.gameType = F.r_u32();
+            m_info.maxAIDist = m_info.maxDist;
         }
         else if (vers == OGG_COMMENT_VERSION)
         {
-            m_fMinDist = F.r_float();
-            m_fMaxDist = F.r_float();
-            m_fBaseVolume = F.r_float();
-            m_uGameType = F.r_u32();
-            m_fMaxAIDist = F.r_float();
+            m_info.minDist = F.r_float();
+            m_info.maxDist = F.r_float();
+            m_info.baseVolume = F.r_float();
+            m_info.gameType = F.r_u32();
+            m_info.maxAIDist = F.r_float();
         }
         else
         {
@@ -251,7 +244,7 @@ bool CSoundRender_Source::LoadWave(pcstr pName, bool crashOnError)
 #endif
     }
 
-    R_ASSERT3_CURE(m_fMaxAIDist >= 0.1f && m_fMaxDist >= 0.1f, "Invalid max distance.", pName, !crashOnError,
+    R_ASSERT3_CURE(m_info.maxAIDist >= 0.1f && m_info.maxDist >= 0.1f, "Invalid max distance.", pName, !crashOnError,
     {
         detach();
         return false;

src/xrSound/SoundRender_Target.cpp
@@ -30,7 +30,7 @@ void CSoundRender_Target::start(CSoundRender_Emitter* E)
 
     // Calc storage
     for (auto& buf : temp_buf)
-        buf.resize(E->source()->m_info.bytesPerBuffer);
+        buf.resize(E->source()->data_info().bytesPerBuffer);
 
     dispatch_prefill_all();
 }

commit 736466258127d71caf4985bc03148115714b3edf

Fix Invalid max distance crash (fixes #1654)
I forgot to add default values when I was moving the variables to a
SoundSourceInfo structure.

This fixes a regression introduced in
55677278859941f703c1ddee1f5f9715631f5f63.

src/xrSound/SoundRender_Source.h
@@ -24,11 +24,11 @@ struct SoundDataInfo
 
 struct SoundSourceInfo
 {
-    float baseVolume;
-    float minDist;
-    float maxDist;
-    float maxAIDist;
-    u32   gameType;
+    float baseVolume{ 1.0f };
+    float minDist   { 1.0f };
+    float maxDist   { 300.0f };
+    float maxAIDist { 300.0f };
+    u32   gameType  {};
 };
 
 class XRSOUND_API CSoundRender_Source final : public CSound_source