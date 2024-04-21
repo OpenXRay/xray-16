701e195bcaf0a0318817ba988301d6f838ba304d
Xottab-DUTY <xottab-duty@yandex.ru>
2024-04-22 04:55:49 +0500

    xrGame/level_changer.cpp: add XXX

--- src/xrGame/level_changer.cpp
+++ src/xrGame/level_changer.cpp
@@ -55,7 +55,7 @@ bool CLevelChanger::net_Spawn(CSE_Abstract* DC)
     m_bSilentMode = !!l_tpALifeLevelChanger->m_bSilentMode;
     if (ai().get_level_graph())
     {
-        //. this information should be computed in xrAI
+        // XXX: this information should be computed in xrAI
         ai_location().level_vertex(ai().level_graph().vertex(u32(-1), Position()));
         ai_location().game_vertex(ai().cross_table().vertex(ai_location().level_vertex_id()).game_vertex_id());
     }
