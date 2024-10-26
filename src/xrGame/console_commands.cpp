1421af50aa511af7e7c2cec0ee6f635579f0ac8f
Xottab-DUTY <xottab-duty@yandex.ru>
2024-10-26 08:17:56 +0300

    xrGame/console_commands.cpp: make memory log consistent

--- src/xrGame/console_commands.cpp
+++ src/xrGame/console_commands.cpp
@@ -158,7 +158,7 @@ static void full_memory_stats()
     size_t _process_heap = ::Memory.mem_usage();
     int _eco_strings = (int)g_pStringContainer->stat_economy();
     int _eco_smem = (int)g_pSharedMemoryContainer->stat_economy();
-    Msg("* [ Render ]: textures[%d K]", (m_base + m_lmaps) / 1024);
+    Msg("* [ render ]: textures[%d K]", (m_base + m_lmaps) / 1024);
     Msg("* [ x-ray  ]: process heap[%u K]", _process_heap / 1024);
     Msg("* [ x-ray  ]: economy: strings[%d K], smem[%d K]", _eco_strings / 1024, _eco_smem);
 #ifdef FS_DEBUG
