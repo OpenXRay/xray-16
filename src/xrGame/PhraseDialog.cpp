e01d122b8e6522c9e74b783d85b70b223b298d74
Roman <Zebs-BMK@yandex.ru>
2024-12-30 20:58:51 +0300

    Added a checking bad phrase structure (#1555)

--- src/xrGame/PhraseDialog.cpp
+++ src/xrGame/PhraseDialog.cpp
@@ -254,6 +254,8 @@ CPhrase* CPhraseDialog::AddPhrase(
 {
     CPhrase* phrase = NULL;
     CPhraseGraph::CVertex* _vertex = data()->m_PhraseGraph.vertex(phrase_id);
+    VERIFY2(!_vertex, make_string("Duplicate phrase ID: [%s] for phrase: [%s]. Existed phrase by this ID: [%s]", phrase_id.c_str(), text, _vertex->data()->GetText()).c_str());
+
     if (!_vertex)
     {
         phrase = xr_new<CPhrase>();
