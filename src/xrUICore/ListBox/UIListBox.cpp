66ea6e4c97946c04536042d3baae24479b57bc6d
yohjimane <nguyen.h.alexander@gmail.com>
2025-07-03 00:57:43 -0700

    [xrUICore] UIListBox - removed StringTable().translate() in AddTextItem
    Ported from OMP commit 5803358d8beae8511593ae307573fe983069bf09

--- src/xrUICore/ListBox/UIListBox.cpp
+++ src/xrUICore/ListBox/UIListBox.cpp
@@ -42,7 +42,7 @@ CUIListBoxItem* CUIListBox::AddTextItem(LPCSTR text)
 
     pItem->SetWndSize(Fvector2().set(GetDesiredChildWidth(), m_def_item_height));
     pItem->SetTextColor(m_text_color);
-    pItem->SetText(StringTable().translate(text).c_str());
+    pItem->SetText(text);
     pItem->GetTextItem()->SetWidth(GetDesiredChildWidth());
     return pItem;
 }
