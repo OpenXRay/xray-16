be8e5dec870ca74af8891ff901bb1a6f52498476
yohjimane <nguyen.h.alexander@gmail.com>
2025-07-03 03:39:44 -0700

    [xrGame] Added microphone icon and distance text in UIMainIngameWnd
    Added voice chat UI elements for multiplayer:
    - Microphone icon with active/inactive states
    - Voice distance text display
    - SetActiveVoiceIcon and SetVoiceDistance methods
    
    Port of OMP commit af9b7c6ed86da3722a2dea7c1ba2abe6106712bb

--- src/xrGame/ui/UIMainIngameWnd.h
+++ src/xrGame/ui/UIMainIngameWnd.h
@@ -42,6 +42,9 @@ protected:
     CUIStatic* m_ind_helmet_broken{};
     CUIStatic* m_ind_outfit_broken{};
     CUIStatic* m_ind_overweight{};
+    
+    CUIStatic* m_icon_microphone = nullptr;
+    CUITextWnd* m_voice_distance = nullptr;
 
 public:
     CUIStatic* m_ind_boost_psy{};
@@ -101,6 +104,9 @@ public:
         ewiInvincible,
         ewiArtefact,
     };
+    
+    void SetActiveVoiceIcon(bool active);
+    void SetVoiceDistance(u8 distance);
 
     void SetMPChatLog(CUIWindow* pChat, CUIWindow* pLog);
 
