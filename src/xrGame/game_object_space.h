cf947d51c152667a0e0c33f913ec86eb0da49a0e
Huawei bootloader research <werasik2aa@yandex.ru>
2025-07-16 00:35:39 +0300

    Format game_object_space.h

--- src/xrGame/game_object_space.h
+++ src/xrGame/game_object_space.h
@@ -2,100 +2,92 @@
 
 namespace GameObject
 {
-	enum ECallbackType
-	{
-		eTradeStart = u32(0),
-		eTradeStop,
-		eTradeSellBuyItem,
-		eTradePerformTradeOperation,
+    enum ECallbackType
+    {
+        eTradeStart = u32(0),
+        eTradeStop,
+        eTradeSellBuyItem,
+        eTradePerformTradeOperation,
 
-		eZoneEnter,
-		eZoneExit,
-		eExitLevelBorder,
-		eEnterLevelBorder,
-		eDeath,
+        eZoneEnter,
+        eZoneExit,
+        eExitLevelBorder,
+        eEnterLevelBorder,
+        eDeath,
 
-		ePatrolPathInPoint,
+        ePatrolPathInPoint,
 
-		eInventoryPda,
-		eInventoryInfo,
-		eArticleInfo,
-		eTaskStateChange,
-		eMapLocationAdded,
+        eInventoryPda,
+        eInventoryInfo,
+        eArticleInfo,
+        eTaskStateChange,
+        eMapLocationAdded,
 
-		eUseObject,
+        eUseObject,
 
-		eHit,
+        eHit,
 
-		eSound,
+        eSound,
 
-		eActionTypeMovement,
-		eActionTypeWatch,
-		eActionTypeRemoved,
-		eActionTypeAnimation,
-		eActionTypeSound,
-		eActionTypeParticle,
-		eActionTypeObject,
+        eActionTypeMovement,
+        eActionTypeWatch,
+        eActionTypeRemoved,
+        eActionTypeAnimation,
+        eActionTypeSound,
+        eActionTypeParticle,
+        eActionTypeObject,
 
-		eActorSleep,
+        eActorSleep,
 
-		eHelicopterOnPoint,
-		eHelicopterOnHit,
+        eHelicopterOnPoint,
+        eHelicopterOnHit,
 
-		eOnItemTake,
-		eOnItemDrop,
+        eOnItemTake,
+        eOnItemDrop,
 
-		eScriptAnimation,
+        eScriptAnimation,
 
-		eTraderGlobalAnimationRequest,
-		eTraderHeadAnimationRequest,
-		eTraderSoundEnd,
+        eTraderGlobalAnimationRequest,
+        eTraderHeadAnimationRequest,
+        eTraderSoundEnd,
 
-		eInvBoxItemTake,
-		eWeaponNoAmmoAvailable,
-		//Alundaio: added defines
-		eActorHudAnimationEnd,
-		//AVO: custom callbacks
-#ifdef INPUT_CALLBACKS
-		// input
-		eKeyPress,
-		eKeyRelease,
-		eKeyHold,
-#endif
-#ifdef MOUSE_INPUT_CALLBACKS
+        eInvBoxItemTake,
+        eWeaponNoAmmoAvailable,
+
+        // Alundaio: added defines
+        eActorHudAnimationEnd,
+
+        // AVO: custom callbacks
+        // Input
+        eKeyPress,
+        eKeyRelease,
+        eKeyHold,
         eMouseMove,
         eMouseWheel,
-#endif
-#ifdef EXTENDED_ITEM_CALLBACKS
-		// inventory
-		eItemToBelt,
-		eItemToSlot,
-		eItemToRuck,
-#endif
-#ifdef	EXTENDED_WEAPON_CALLBACKS
-		// weapon
-		eOnWeaponZoomIn,
-		eOnWeaponZoomOut,
-		eOnWeaponJammed,
-		eOnWeaponFired,
-		eOnWeaponMagazineEmpty,
-#endif
-		// actor
-#ifdef ACTOR_BEFORE_DEATH_CALLBACK
-		eActorBeforeDeath,
-#endif
-#ifdef ENABLE_CAR
-		// vehicle
-		eAttachVehicle,
-		eDetachVehicle,
-		eUseVehicle,
-#endif
-		eOnFootStep,
-		//-AVO
 
-		eOnWeaponLowered,
-		eOnWeaponRaised,
+        // Inventory
+        eItemToBelt,
+        eItemToSlot,
+        eItemToRuck,
 
-		eDummy = u32(-1),
-	};
+        // Weapon
+        eOnWeaponZoomIn,
+        eOnWeaponZoomOut,
+        eOnWeaponJammed,
+        eOnWeaponFired,
+        eOnWeaponMagazineEmpty,
+        eActorBeforeDeath,
+
+        // Vehicle
+        eAttachVehicle,
+        eDetachVehicle,
+        eUseVehicle,
+        eOnFootStep,
+        // -AVO
+
+        eOnWeaponLowered,
+        eOnWeaponRaised,
+
+        eDummy = u32(-1),
+    };
 };
