665a1a7f29d8ddc3546c70e49e0d999063939004
yohjimane <nguyen.h.alexander@gmail.com>
2025-10-26 23:44:48 -0700

    Revert "Add shader params used by Beef's nvg shaders"
    This reverts commit 9ecc2afa8b88f1bc3708bc3f074d9cec74c0684a.

--- src/Layers/xrRender/xrRender_console.cpp
+++ src/Layers/xrRender/xrRender_console.cpp
@@ -1023,20 +1023,11 @@ void xrRender_initconsole()
     CMD4(CCC_Integer, "r2_mt_render",       &ps_r2_mt_render,    0, 1);
 #endif
 
-    // Anomaly
-    Fvector4 tw2_min = { -100.f, -100.f, -100.f, -100.f };
-    Fvector4 tw2_max = { 100.f, 100.f, 100.f, 100.f };
     CMD4(CCC_Integer, "r__nightvision", &ps_r2_nightvision, 0, 3); //For beef's nightvision shader or other stuff
-    CMD4(CCC_Vector4, "shader_param_1", &ps_dev_param_1, tw2_min, tw2_max);
-    CMD4(CCC_Vector4, "shader_param_2", &ps_dev_param_2, tw2_min, tw2_max);
-    CMD4(CCC_Vector4, "shader_param_3", &ps_dev_param_3, tw2_min, tw2_max);
-    CMD4(CCC_Vector4, "shader_param_4", &ps_dev_param_4, tw2_min, tw2_max);
-    CMD4(CCC_Vector4, "shader_param_5", &ps_dev_param_5, tw2_min, tw2_max);
-    CMD4(CCC_Vector4, "shader_param_6", &ps_dev_param_6, tw2_min, tw2_max);
-    CMD4(CCC_Vector4, "shader_param_7", &ps_dev_param_7, tw2_min, tw2_max);
-    CMD4(CCC_Vector4, "shader_param_8", &ps_dev_param_8, tw2_min, tw2_max);
 
     // Screen Space Shaders
+    Fvector4 tw2_min = { -100.f, -100.f, -100.f, -100.f };
+    Fvector4 tw2_max = { 100.f, 100.f, 100.f, 100.f };
     CMD4(CCC_Integer, "r__screen_space_shaders", &ps_screen_space_shaders, 0, 1);
     CMD4(CCC_Vector4, "ssfx_grass_shadows", &ps_ssfx_grass_shadows, Fvector4().set(0, 0, 0, 0), Fvector4().set(3, 1, 100, 100));
     CMD4(CCC_ssfx_cascades, "ssfx_shadow_cascades", &ps_ssfx_shadow_cascades, Fvector3().set(1.0f, 1.0f, 1.0f), Fvector3().set(300, 300, 300));
