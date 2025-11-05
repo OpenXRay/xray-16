e3855b8b12b0d66a150ce48ba068225ec05ccc9c
AMS21 <ams21.github@gmail.com>
2025-11-05 22:26:27 +0100

    fix: use-after-free in `CStateMonsterAttackOnRun::update_aim_side`

--- src/xrGame/ai/monsters/states/monster_state_attack_on_run_inline.h
+++ src/xrGame/ai/monsters/states/monster_state_attack_on_run_inline.h
@@ -54,6 +54,7 @@ void ATTACK_ON_RUN_STATE::initialize()
     m_is_jumping = this->object->is_jumping();
     m_reach_old_target = false;
     m_attack_side_chosen_time = 0;
+    m_enemy_to_attack = nullptr;
 
     choose_next_atack_animation();
 }
@@ -178,6 +179,9 @@ TEMPLATE_SIGNATURE
 void ATTACK_ON_RUN_STATE::update_aim_side()
 {
     CEntityAlive const* const enemy = m_attacking ? m_enemy_to_attack : this->object->EnemyMan.get_enemy();
+
+    if (!enemy)
+        return;
 
     Fvector const self_dir = this->object->Direction();
     Fvector const self_to_enemy = enemy->Position() - this->object->Position();
@@ -619,6 +623,15 @@ bool ATTACK_ON_RUN_STATE::check_completion()
     // if (!object->control().path_builder().is_moving_on_path() ||
     //	(object->m_time_last_attack_success != 0)) return true;
     return false;
+}
+
+TEMPLATE_SIGNATURE
+void ATTACK_ON_RUN_STATE::remove_links(IGameObject* object)
+{
+    inherited::remove_links(object);
+
+    if (m_enemy_to_attack == object)
+        m_enemy_to_attack = nullptr;
 }
 
 #undef DEBUG_STATE
