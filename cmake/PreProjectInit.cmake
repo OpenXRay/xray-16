79288b70b151716565c35f8c9b18056a4064db7f
r-a-sattarov <r.a.sattarov@yandex.ru>
2025-12-23 17:23:50 +0300

    Fixed configuration on CMake < 3.24

--- cmake/PreProjectInit.cmake
+++ cmake/PreProjectInit.cmake
@@ -2,10 +2,20 @@ include_guard()
 
 message(VERBOSE "CMAKE_VERSION: ${CMAKE_VERSION}")
 
-cmake_policy(SET CMP0138 NEW) # https://cmake.org/cmake/help/latest/policy/CMP0138.html
-cmake_policy(SET CMP0149 NEW) # https://cmake.org/cmake/help/latest/policy/CMP0149.html
-cmake_policy(SET CMP0162 NEW) # https://cmake.org/cmake/help/latest/policy/CMP0162.html
-cmake_policy(SET CMP0164 NEW) # https://cmake.org/cmake/help/latest/policy/CMP0164.html
+cmake_policy(SET CMP0129 NEW) # https://cmake.org/cmake/help/latest/policy/CMP0129.html
+
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24")
+    cmake_policy(SET CMP0138 NEW) # https://cmake.org/cmake/help/latest/policy/CMP0138.html
+endif()
+
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.27")
+    cmake_policy(SET CMP0149 NEW) # https://cmake.org/cmake/help/latest/policy/CMP0149.html
+endif()
+
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.30")
+    cmake_policy(SET CMP0162 NEW) # https://cmake.org/cmake/help/latest/policy/CMP0162.html
+    cmake_policy(SET CMP0164 NEW) # https://cmake.org/cmake/help/latest/policy/CMP0164.html
+endif()
 
 include(XRay.Configurations)
 
