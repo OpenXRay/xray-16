chronic8000/OpenXRay-Pi5

commit bc4ae7055b98cd5285426d49ed82fb237b0253db

Talk dialog now properly works in SOC (#392)

src/xrGame/ui/UITalkDialogWnd.cpp
@@ -44,7 +44,8 @@ void CUITalkDialogWnd::InitTalkDialogWnd()
     m_uiXml = xr_new<CUIXml>();
     m_uiXml->Load(CONFIG_PATH, UI_PATH, UI_PATH_DEFAULT, TALK_XML);
 
-    CUIXmlInit::InitWindow(*m_uiXml, "main", 0, this, false);
+    if (!CUIXmlInit::InitWindow(*m_uiXml, "main", 0, this, false))
+        SetWndRect({ 0, 0, UI_BASE_WIDTH, UI_BASE_HEIGHT });
 
     UIStaticTop = UIHelper::CreateStatic(*m_uiXml, "top_background", this, false);
     UIStaticBottom = UIHelper::CreateStatic(*m_uiXml, "bottom_background", this, false);

src/xrGame/ui/UITalkWnd.cpp
@@ -34,7 +34,7 @@ CUITalkWnd::CUITalkWnd() : CUIDialogWnd(CUITalkWnd::GetDebugType())
 
 void CUITalkWnd::InitTalkWnd()
 {
-    inherited::SetWndRect(Frect().set(0, 0, UI_BASE_WIDTH, UI_BASE_HEIGHT));
+    inherited::SetWndRect({ 0, 0, UI_BASE_WIDTH, UI_BASE_HEIGHT });
 
     UITalkDialogWnd = xr_new<CUITalkDialogWnd>();
     UITalkDialogWnd->SetAutoDelete(true);
@@ -59,8 +59,10 @@ void CUITalkWnd::InitTalkDialog()
     UITalkDialogWnd->UICharacterInfoLeft.InitCharacter(m_pOurInvOwner->object_id());
     UITalkDialogWnd->UICharacterInfoRight.InitCharacter(m_pOthersInvOwner->object_id());
 
-    //.	UITalkDialogWnd->UIDialogFrame.UITitleText.SetText		(m_pOthersInvOwner->Name());
-    //.	UITalkDialogWnd->UIOurPhrasesFrame.UITitleText.SetText	(m_pOurInvOwner->Name());
+    if (const auto title = UITalkDialogWnd->UIDialogFrame->GetTitleText())
+        title->SetText(m_pOthersInvOwner->Name());
+    if (const auto title = UITalkDialogWnd->UIOurPhrasesFrame->GetTitleText())
+        title->SetText(m_pOurInvOwner->Name());
 
     //очистить лог сообщений
     UITalkDialogWnd->ClearAll();

src/xrGame/ui/UICharacterInfo.cpp
@@ -42,7 +42,11 @@ void CUICharacterInfo::InitCharacterInfo(Fvector2 pos, Fvector2 size, CUIXml* xm
 
     Init_IconInfoItem(*xml_doc, "icon", eIcon);
     if (!m_icons[eIcon])
-        Init_IconInfoItem(*xml_doc, "icon_static", eIcon);
+    {
+        Init_IconInfoItem(*xml_doc, "icon_static", eIcon); // SOC
+        if (m_icons[eIcon])
+            m_icons[eIcon]->SetStretchTexture(true);
+    }
 
     m_original_color = m_icons[eIcon] ? m_icons[eIcon]->GetTextureColor() : color_xrgb(255, 255, 255);
 

OpenXRay/xray-16

commit 1a2550d0c2a180d6e973f07b755aa6725739146e

Proper talk dialog look in SOC (#392) Fixed crash in CS/COP introduced in bc4ae7055b98cd5285426d49ed82fb237b0253db

src/xrGame/ui/UIHelper.cpp
@@ -142,12 +142,17 @@ CUIProgressShape* UIHelper::CreateProgressShape(CUIXml& xml, LPCSTR ui_path, CUI
 
 CUIFrameLineWnd* UIHelper::CreateFrameLine(CUIXml& xml, LPCSTR ui_path, CUIWindow* parent, bool critical)
 {
+    return CreateFrameLine(xml, ui_path, 0, parent, critical);
+}
+
+CUIFrameLineWnd* UIHelper::CreateFrameLine(CUIXml& xml, LPCSTR ui_path, int index, CUIWindow* parent, bool critical)
+{
     // If it's not critical element, then don't crash if it doesn't exist
-    if (!critical && !xml.NavigateToNode(ui_path, 0))
+    if (!critical && !xml.NavigateToNode(ui_path, index))
         return nullptr;
 
     auto ui = xr_new<CUIFrameLineWnd>(ui_path);
-    if (!CUIXmlInit::InitFrameLine(xml, ui_path, 0, ui, critical) && !critical)
+    if (!CUIXmlInit::InitFrameLine(xml, ui_path, index, ui, critical) && !critical)
     {
         xr_delete(ui);
     }

src/xrGame/ui/UIHelper.h
@@ -36,6 +36,7 @@ public:
     static CUIProgressBar* CreateProgressBar(CUIXml& xml, LPCSTR ui_path, CUIWindow* parent, bool critical = true);
     static CUIProgressShape* CreateProgressShape(CUIXml& xml, LPCSTR ui_path, CUIWindow* parent, bool critical = true);
     static CUIFrameLineWnd* CreateFrameLine(CUIXml& xml, LPCSTR ui_path, CUIWindow* parent, bool critical = true);
+    static CUIFrameLineWnd* CreateFrameLine(CUIXml& xml, LPCSTR ui_path, int index, CUIWindow* parent, bool critical = true);
     static CUIFrameWindow* CreateFrameWindow(CUIXml& xml, LPCSTR ui_path, CUIWindow* parent, bool critical = true);
     static CUI3tButton* Create3tButton(CUIXml& xml, LPCSTR ui_path, CUIWindow* parent, bool critical = true);
     static CUI3tButton* Create3tButton(CUIXml& xml, LPCSTR ui_path, int index, CUIWindow* parent, bool critical = true);

src/xrGame/ui/UITalkDialogWnd.cpp
@@ -21,8 +21,6 @@ CUITalkDialogWnd::CUITalkDialogWnd()
       m_pParent(nullptr),
       mechanic_mode(false),
       m_ClickedQuestionID(""),
-      UIDialogFrameTop(nullptr),
-      UIDialogFrameBottom(nullptr),
       m_btn_pos(),
       UIToExitButton(nullptr),
       UIOurIcon(nullptr),
@@ -47,8 +45,8 @@ void CUITalkDialogWnd::InitTalkDialogWnd()
     if (!CUIXmlInit::InitWindow(*m_uiXml, "main", 0, this, false))
         SetWndRect({ 0, 0, UI_BASE_WIDTH, UI_BASE_HEIGHT });
 
-    UIStaticTop = UIHelper::CreateStatic(*m_uiXml, "top_background", this, false);
-    UIStaticBottom = UIHelper::CreateStatic(*m_uiXml, "bottom_background", this, false);
+    std::ignore = UIHelper::CreateStatic(*m_uiXml, "top_background", this, false);
+    std::ignore = UIHelper::CreateStatic(*m_uiXml, "bottom_background", this, false);
 
     pcstr ourTag = "right_character_icon";
     pcstr othersTag = "left_character_icon";
@@ -71,43 +69,26 @@ void CUITalkDialogWnd::InitTalkDialogWnd()
             TALK_CHARACTER_XML, TRADE_CHARACTER_XML);
     }
 
-    CUIWindow* answersParent = this;
-    CUIWindow* questionsParent = this;
-
-    // Our phrases frame
+    // CS/COP order:
+    // 1. Our phrases frame
+    // 2. Main dialog frame
     UIDialogFrameBottom = UIHelper::CreateStatic(*m_uiXml, "frame_bottom", this, false);
-    if (UIDialogFrameBottom)
-        questionsParent = UIDialogFrameBottom;
-    else
-    {
-        if (m_uiXml->NavigateToNode("frame_line_window", 1))
-        {
-            // XXX: Don't replace this with UI helper, until it's missing needed functionality to select the index
-            UIOurPhrasesFrame = xr_new<CUIFrameLineWnd>("Our phrases frame");
-            UIOurPhrasesFrame->SetAutoDelete(true);
-            AttachChild(UIOurPhrasesFrame);
-            CUIXmlInitBase::InitFrameLine(*m_uiXml, "frame_line_window", 1, UIOurPhrasesFrame); // index for field is 1 (one) !!!
-            questionsParent = UIOurPhrasesFrame;
-        }
-    }
-
-    // Main dialog frame
     UIDialogFrameTop = UIHelper::CreateStatic(*m_uiXml, "frame_top", this, false);
-    if (UIDialogFrameTop)
-        answersParent = UIDialogFrameTop;
-    else
-    {
-        UIDialogFrame = UIHelper::CreateFrameLine(*m_uiXml, "frame_line_window", this, false);
-        if (UIDialogFrame)
-            answersParent = UIDialogFrame;
-    }
+
+    // SOC order:
+    // 1. Main dialog frame
+    // 2. Our phrases frame
+    if (!UIDialogFrameTop)
+        UIDialogFrameTop = UIHelper::CreateFrameLine(*m_uiXml, "frame_line_window", 0, this, false);
+    if (!UIDialogFrameBottom)
+        UIDialogFrameBottom = UIHelper::CreateFrameLine(*m_uiXml, "frame_line_window", 1, this, false);
 
     // Answers
-    UIAnswersList = UIHelper::CreateScrollView(*m_uiXml, "answers_list", answersParent);
+    UIAnswersList = UIHelper::CreateScrollView(*m_uiXml, "answers_list", UIDialogFrameTop ? UIDialogFrameTop : this);
     UIAnswersList->SetWindowName("---UIAnswersList");
 
     // Questions
-    UIQuestionsList = UIHelper::CreateScrollView(*m_uiXml, "questions_list", questionsParent);
+    UIQuestionsList = UIHelper::CreateScrollView(*m_uiXml, "questions_list", UIDialogFrameBottom ? UIDialogFrameBottom : this);
     UIQuestionsList->SetWindowName("---UIQuestionsList");
 
     //кнопка перехода в режим торговли
@@ -204,6 +185,25 @@ void CUITalkDialogWnd::ClearAll()
 }
 
 void CUITalkDialogWnd::ClearQuestions() { UIQuestionsList->Clear(); }
+
+void CUITalkDialogWnd::SetOurName(pcstr name)
+{
+    const auto frameline = smart_cast<CUIFrameLineWnd*>(UIDialogFrameBottom);
+    if (!frameline)
+        return;
+    if (const auto title = frameline->GetTitleText())
+        title->SetText(name);
+}
+
+void CUITalkDialogWnd::SetOthersName(pcstr name)
+{
+    const auto frameline = smart_cast<CUIFrameLineWnd*>(UIDialogFrameTop);
+    if (!frameline)
+        return;
+    if (const auto title = frameline->GetTitleText())
+        title->SetText(name);
+}
+
 void CUITalkDialogWnd::AddQuestion(LPCSTR str, LPCSTR value, int number, bool b_finalizer)
 {
     CUIQuestionItem* itm = xr_new<CUIQuestionItem>(m_uiXml, "question_item");
@@ -301,8 +301,6 @@ void CUITalkDialogWnd::SetOsoznanieMode(bool b)
 
     if (UIDialogFrameTop)
         UIDialogFrameTop->Show(!b);
-    else if (UIDialogFrame)
-        UIDialogFrame->Show(!b);
 
     UIToTradeButton.Show(!b);
     if (mechanic_mode)

src/xrGame/ui/UITalkDialogWnd.h
@@ -38,14 +38,8 @@ public:
     shared_str m_ClickedQuestionID;
 
     // Dialog interface elements
-    CUIStatic* UIStaticTop;
-    CUIStatic* UIStaticBottom;
-
-    CUIFrameLineWnd* UIDialogFrame;
-    CUIFrameLineWnd* UIOurPhrasesFrame;
-
-    CUIStatic* UIDialogFrameTop;
-    CUIStatic* UIDialogFrameBottom;
+    CUIWindow* UIDialogFrameTop{};
+    CUIWindow* UIDialogFrameBottom{};
 
     Fvector2 m_btn_pos[3];
     CUI3tButton UIToTradeButton;
@@ -64,6 +58,8 @@ public:
     void ClearAll();
     void ClearQuestions();
 
+    void SetOurName(pcstr name);
+    void SetOthersName(pcstr name);
     void SetOsoznanieMode(bool b);
     void SetTradeMode();
     void UpdateButtonsLayout(bool b_disable_break, bool trade_enabled);

src/xrGame/ui/UITalkWnd.cpp
@@ -59,10 +59,8 @@ void CUITalkWnd::InitTalkDialog()
     UITalkDialogWnd->UICharacterInfoLeft.InitCharacter(m_pOurInvOwner->object_id());
     UITalkDialogWnd->UICharacterInfoRight.InitCharacter(m_pOthersInvOwner->object_id());
 
-    if (const auto title = UITalkDialogWnd->UIDialogFrame->GetTitleText())
-        title->SetText(m_pOthersInvOwner->Name());
-    if (const auto title = UITalkDialogWnd->UIOurPhrasesFrame->GetTitleText())
-        title->SetText(m_pOurInvOwner->Name());
+    UITalkDialogWnd->SetOurName(m_pOurInvOwner->Name());
+    UITalkDialogWnd->SetOthersName(m_pOthersInvOwner->Name());
 
     //очистить лог сообщений
     UITalkDialogWnd->ClearAll();