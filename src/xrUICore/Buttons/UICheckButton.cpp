6125dfc0a1caa7d410295229e53d984844afe95c
Xottab-DUTY <xottab-duty@yandex.ru>
2026-02-11 18:34:19 +0300

    xrUICore/Buttons/UICheckButton.h: default initialize m_opt_backup_value directly in the class

--- src/xrUICore/Buttons/UICheckButton.cpp
+++ src/xrUICore/Buttons/UICheckButton.cpp
@@ -12,7 +12,6 @@ CUICheckButton::CUICheckButton()
     TextItemControl()->SetTextAlignment(CGameFont::alLeft);
     SetButtonAsSwitch(true);
     m_pDependControl = NULL;
-    m_opt_backup_value = false;
 }
 
 void CUICheckButton::SetDependControl(CUIWindow* pWnd) { m_pDependControl = pWnd; }
