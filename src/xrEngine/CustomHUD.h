540ca45debb3831e874b083e67a0e638745e239f
stfulldev <snjtolibjonov@gmail.com>
2026-08-28 09:26:26 +0500

    wip marker binoklya

--- src/xrEngine/CustomHUD.h
+++ src/xrEngine/CustomHUD.h
@@ -12,6 +12,7 @@ ENGINE_API extern Flags32 psHUD_Flags;
 #define HUD_CROSSHAIR_RT (1 << 5)
 #define HUD_WEAPON_RT (1 << 6)
 #define HUD_CROSSHAIR_DYNAMIC (1 << 7)
+#define HUD_BINOCULAR_VISION (1 << 8)
 #define HUD_CROSSHAIR_RT2 (1 << 9)
 #define HUD_DRAW_RT (1 << 10)
 #define HUD_WEAPON_RT2 (1 << 11)
