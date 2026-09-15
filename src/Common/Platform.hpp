0adb74f1db9566569e580fa99b727d3a0a470e45
q09009 <q09009@naver.com>
2026-09-15 17:36:29 +0900

    Add Android libc compatibility paths

--- src/Common/Platform.hpp
+++ src/Common/Platform.hpp
@@ -3,6 +3,11 @@
 #if defined(_WIN32)
 #   define XR_PLATFORM_WINDOWS
 #   define _XRAY_PLATFORM_MARKER "Windows"
+#elif defined(__ANDROID__)
+#   define XR_PLATFORM_ANDROID
+#   define XR_PLATFORM_LINUX
+#   define XR_PLATFORM_POSIX
+#   define _XRAY_PLATFORM_MARKER "Android"
 #elif defined(__linux__)
 #   define XR_PLATFORM_LINUX
 #   define XR_PLATFORM_POSIX
